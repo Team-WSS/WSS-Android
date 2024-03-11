d58abd16b9c8eb474318d55620307decdc0a5159
junseo511 <prime54@g.skku.edu>
2024-03-11 16:14:35 +0900

    refactor: App.kt 이름 수정

--- app/src/main/java/com/teamwss/websoso/WebsosoApp.kt
+++ app/src/main/java/com/teamwss/websoso/WebsosoApp.kt
@@ -3,7 +3,7 @@ package com.teamwss.websoso
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
 
-class App : Application() {
+class WebsosoApp : Application() {
     override fun onCreate() {
         super.onCreate()
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
