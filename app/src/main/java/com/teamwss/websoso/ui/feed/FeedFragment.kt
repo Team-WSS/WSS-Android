15e6b26701fe9fd60b863f25e1f273140cac1d30
s9hn <tnghqn@naver.com>
2024-03-19 20:26:27 +0900

    feat: base class, 프레임 프래그먼트 생성 및 적용

---
+++ app/src/main/java/com/teamwss/websoso/ui/feed/FeedFragment.kt
@@ -0,0 +1,7 @@
+package com.teamwss.websoso.ui.feed
+
+import com.created.team201.presentation.common.BindingFragment
+import com.teamwss.websoso.R
+import com.teamwss.websoso.databinding.FragmentFeedBinding
+
+class FeedFragment : BindingFragment<FragmentFeedBinding>(R.layout.fragment_feed)
