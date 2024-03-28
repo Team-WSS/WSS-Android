558ab9925ea95db94e6c72ea25459ed1d9bb8049
s9hn <tnghqn@naver.com>
2024-03-28 18:09:10 +0900

    feat: BaseDiffUtil 생성
    - ListAdapter에서 사용되는 디프유틸 추상화

--- app/src/main/java/com/teamwss/websoso/ui/feed/adapter/FeedAdapter.kt
+++ app/src/main/java/com/teamwss/websoso/ui/feed/adapter/FeedAdapter.kt
@@ -1,11 +1,10 @@
 package com.teamwss.websoso.ui.feed.adapter
 
 import android.view.ViewGroup
-import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import com.teamwss.websoso.domain.model.Feed
+import com.teamwss.websoso.ui.common.base.BaseDiffUtil
 import com.teamwss.websoso.ui.feed.FeedItemClickListener
-
 
 class FeedAdapter(
     private val onClick: FeedItemClickListener
@@ -15,9 +14,7 @@ class FeedAdapter(
         setHasStableIds(true)
     }
 
-    override fun getItemId(position: Int): Long {
-        return getItem(position).id.toLong()
-    }
+    override fun getItemId(position: Int): Long = getItem(position).id.toLong()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FeedViewHolder =
         FeedViewHolder.from(parent, onClick)
@@ -27,14 +24,6 @@ class FeedAdapter(
     }
 
     companion object {
-        private val diffCallBack = object : DiffUtil.ItemCallback<Feed>() {
-            override fun areItemsTheSame(oldItem: Feed, newItem: Feed): Boolean {
-                return oldItem.id == newItem.id
-            }
-
-            override fun areContentsTheSame(oldItem: Feed, newItem: Feed): Boolean {
-                return oldItem == newItem
-            }
-        }
+        private val diffCallBack = object : BaseDiffUtil<Feed>() {}
     }
 }
