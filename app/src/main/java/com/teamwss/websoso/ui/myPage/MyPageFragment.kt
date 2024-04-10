eec09fc5a05a8ea4b95bc6ef98836b223543e22e
yeonjeen <jeendev2019@gmail.com>
2024-04-10 11:00:53 +0900

    feat: tablayout+viewpager 연결하기

--- app/src/main/java/com/teamwss/websoso/ui/myPage/MyPageFragment.kt
+++ app/src/main/java/com/teamwss/websoso/ui/myPage/MyPageFragment.kt
@@ -1,7 +1,42 @@
 package com.teamwss.websoso.ui.myPage
 
-import com.teamwss.websoso.R
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayoutMediator
 import com.teamwss.websoso.databinding.FragmentMyPageBinding
-import com.teamwss.websoso.ui.common.base.BindingFragment
 
-class MyPageFragment : BindingFragment<FragmentMyPageBinding>(R.layout.fragment_my_page)
+class MyPageFragment : Fragment() {
+    private var _binding: FragmentMyPageBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMyPageBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupViewPagerAndTabs()
+    }
+
+    private fun setupViewPagerAndTabs() {
+        val pagerAdapter = MyViewPagerAdapter(childFragmentManager, lifecycle)
+        binding.vpMyPage.adapter = pagerAdapter
+
+        TabLayoutMediator(binding.tbMyPage, binding.vpMyPage) { tab, position ->
+            tab.text = "Tab ${position + 1}"
+        }.attach()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
