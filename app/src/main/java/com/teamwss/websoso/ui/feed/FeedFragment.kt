e776eb8ac51bc555671b8430363cbc7e27b99aa5
s9hn <tnghqn@naver.com>
2024-04-10 22:24:32 +0900

    feat: 커스텀 칩 디자인 및 기능 적용

--- app/src/main/java/com/teamwss/websoso/ui/feed/FeedFragment.kt
+++ app/src/main/java/com/teamwss/websoso/ui/feed/FeedFragment.kt
@@ -9,7 +9,10 @@ import androidx.fragment.app.viewModels
 import com.teamwss.websoso.R
 import com.teamwss.websoso.databinding.FragmentFeedBinding
 import com.teamwss.websoso.ui.common.base.BindingFragment
+import com.teamwss.websoso.ui.common.customView.WebsosoChip
 import com.teamwss.websoso.ui.feed.adapter.FeedAdapter
+import com.teamwss.websoso.ui.feed.model.Category
+import com.teamwss.websoso.ui.feed.model.Category.Companion.toWrappedCategories
 import com.teamwss.websoso.ui.feedDetail.FeedDetailActivity
 
 class FeedFragment : BindingFragment<FragmentFeedBinding>(R.layout.fragment_feed) {
@@ -42,39 +45,8 @@ class FeedFragment : BindingFragment<FragmentFeedBinding>(R.layout.fragment_feed
         }
     }
 
-    private fun navigateToFeedDetail(id: Int) {
-        startActivity(
-            FeedDetailActivity.from(
-                id, context ?: throw IllegalArgumentException("추후 에러 헨들링 요망")
-            )
-        )
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        setupBinding()
-        setupAdapter()
-        observeFeeds()
-    }
-
-    private fun setupBinding() {
-        binding.lifecycleOwner = viewLifecycleOwner
-    }
-
-    private fun setupAdapter() {
-        binding.rvFeed.adapter = feedAdapter
-        binding.rvFeed.setHasFixedSize(true)
-    }
-
-    private fun observeFeeds() {
-        feedViewModel.uiState.observe(viewLifecycleOwner) {
-            feedAdapter.submitList(it)
-        }
-    }
-
     @SuppressLint("InflateParams")
-    fun showMenu(view: View) {
+    private fun showMenu(view: View) {
         val popupView = layoutInflater.inflate(R.layout.menu_feed_popup, null)
         val popupWindow = PopupWindow(
             popupView,
@@ -84,5 +56,69 @@ class FeedFragment : BindingFragment<FragmentFeedBinding>(R.layout.fragment_feed
         )
 
         popupWindow.showAsDropDown(view)
+    }
+
+    private fun navigateToFeedDetail(id: Int) {
+        startActivity(
+            FeedDetailActivity.from(
+                id, requireContext()
+            )
+        )
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupBinding()
+        initView()
+    }
+
+    private fun setupBinding() {
+        binding.lifecycleOwner = viewLifecycleOwner
+    }
+
+    private fun initView() {
+        setupCategory()
+        setupFeeds()
+    }
+
+    private fun setupCategory() {
+        when (feedViewModel.gender) {
+            "MALE" -> getString(R.string.feed_category_male).toWrappedCategories()
+            "FEMALE" -> getString(R.string.feed_category_female).toWrappedCategories()
+            else -> throw IllegalStateException()
+        }.apply { setUpChips() }
+    }
+
+    private fun List<Category>.setUpChips() {
+        forEach { category ->
+            WebsosoChip(requireContext()).apply {
+                setWebsosoChipText(category.title)
+                setWebsosoChipTextAppearance(R.style.title3)
+                setWebsosoChipTextColor(R.color.bg_feed_chip_text_selector)
+                setWebsosoChipStrokeColor(R.color.bg_feed_chip_stroke_selector)
+                setWebsosoChipBackgroundColor(R.color.bg_feed_chip_background_selector)
+                setWebsosoChipPaddingVertical(20f)
+                setWebsosoChipPaddingHorizontal(12f)
+                setWebsosoChipRadius(30f)
+                setOnWebsosoChipClick { feedViewModel.updateFeedsByCategory(category) }
+            }.also { websosoChip -> binding.wcgFeed.addChip(websosoChip) }
+        }
+    }
+
+    private fun setupFeeds() {
+        setupAdapter()
+        observeFeeds()
+    }
+
+    private fun setupAdapter() {
+        binding.rvFeed.adapter = feedAdapter
+        binding.rvFeed.setHasFixedSize(true)
+    }
+
+    private fun observeFeeds() {
+        feedViewModel.uiState.observe(viewLifecycleOwner) {
+            feedAdapter.submitList(it.feeds.feeds)
+        }
     }
 }
