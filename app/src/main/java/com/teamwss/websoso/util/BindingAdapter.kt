66780aee8a0336474fb6076e9eeebe3280b0a534
librarywon <librarywon@naver.com>
2024-04-11 16:34:57 +0900

    feat: cornerRadius 바인딩 어뎁터, 디폴트 placeholder 로직 추가

--- app/src/main/java/com/teamwss/websoso/util/BindingAdapter.kt
+++ app/src/main/java/com/teamwss/websoso/util/BindingAdapter.kt
@@ -3,12 +3,35 @@ package com.teamwss.websoso.util
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import coil.load
+import coil.transform.RoundedCornersTransformation
+import coil.transform.Transformation
+import com.teamwss.websoso.R
 
 object BindingAdapter {
 
     @JvmStatic
     @BindingAdapter("loadImageUrl")
     fun loadImage(view: ImageView, imageUrl: String?) {
-        view.load(imageUrl)
+        loadImageWithTransformation(view, imageUrl, null)
+    }
+
+    @JvmStatic
+    @BindingAdapter("loadImageUrl", "cornerRadius")
+    fun loadRoundedCornerImage(view: ImageView, imageUrl: String?, cornerRadius: Float?) {
+        val transformation = cornerRadius?.let { RoundedCornersTransformation(it) }
+        loadImageWithTransformation(view, imageUrl, transformation)
+    }
+
+    private fun loadImageWithTransformation(
+        view: ImageView,
+        imageUrl: String?,
+        transformation: Transformation?,
+        placeholder: Int = R.drawable.img_loading_thumbnail
+    ) {
+        view.load(imageUrl ?: placeholder) {
+            crossfade(true)
+            transformation?.let { transformations(it) }
+            listener(onError = { _, _ -> view.load(placeholder) })
+        }
     }
 }
