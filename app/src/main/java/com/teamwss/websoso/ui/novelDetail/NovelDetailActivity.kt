0ca4526388dcb08cacb4c9d2782f719fdc904045
junseo511 <prime54@g.skku.edu>
2024-04-12 00:41:59 +0900

    refactor: const val 이동

--- app/src/main/java/com/teamwss/websoso/ui/novelDetail/NovelDetailActivity.kt
+++ app/src/main/java/com/teamwss/websoso/ui/novelDetail/NovelDetailActivity.kt
@@ -7,7 +7,8 @@ import com.teamwss.websoso.databinding.ActivityNovelDetailBinding
 import com.teamwss.websoso.ui.common.base.BindingActivity
 import com.teamwss.websoso.ui.novelDetail.adapter.NovelDetailPagerAdapter
 
-class NovelDetailActivity: BindingActivity<ActivityNovelDetailBinding>(R.layout.activity_novel_detail) {
+class NovelDetailActivity :
+    BindingActivity<ActivityNovelDetailBinding>(R.layout.activity_novel_detail) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -18,10 +19,15 @@ class NovelDetailActivity: BindingActivity<ActivityNovelDetailBinding>(R.layout.
         binding.vpNovelDetail.adapter = NovelDetailPagerAdapter(this)
         TabLayoutMediator(binding.tlNovelDetail, binding.vpNovelDetail) { tab, position ->
             tab.text = when (position) {
-                NovelDetailPagerAdapter.INFO_FRAGMENT_PAGE -> getString(R.string.novel_detail_info)
-                NovelDetailPagerAdapter.FEED_FRAGMENT_PAGE -> getString(R.string.novel_detail_feed)
+                INFO_FRAGMENT_PAGE -> getString(R.string.novel_detail_info)
+                FEED_FRAGMENT_PAGE -> getString(R.string.novel_detail_feed)
                 else -> throw IllegalArgumentException("Invalid position")
             }
         }.attach()
+    }
+
+    companion object {
+        const val INFO_FRAGMENT_PAGE = 0
+        const val FEED_FRAGMENT_PAGE = 1
     }
 }
--- app/src/main/java/com/teamwss/websoso/ui/novelDetail/adapter/NovelDetailPagerAdapter.kt
+++ app/src/main/java/com/teamwss/websoso/ui/novelDetail/adapter/NovelDetailPagerAdapter.kt
@@ -6,17 +6,13 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.teamwss.websoso.ui.novelDetail.NovelFeedFragment
 import com.teamwss.websoso.ui.novelDetail.NovelInfoFragment
 
-class NovelDetailPagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
+class NovelDetailPagerAdapter(fragmentActivity: FragmentActivity) :
+    FragmentStateAdapter(fragmentActivity) {
     private val fragments = arrayOf(NovelInfoFragment(), NovelFeedFragment())
 
     override fun getItemCount(): Int = fragments.size
 
     override fun createFragment(position: Int): Fragment {
         return fragments[position]
-    }
-
-    companion object {
-        const val INFO_FRAGMENT_PAGE = 0
-        const val FEED_FRAGMENT_PAGE = 1
     }
 }
