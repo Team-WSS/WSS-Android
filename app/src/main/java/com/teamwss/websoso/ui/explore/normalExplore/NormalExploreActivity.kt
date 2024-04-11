ea4055c15268a6b0705509a5f058f4fa6ffb8b78
Son Myeongji <sonmyungj1zz@gmail.com>
2024-04-11 17:26:18 +0900

    feat: 기본 UI 세팅 및 뷰모델 추가

--- app/src/main/java/com/teamwss/websoso/ui/explore/normalExplore/NormalExploreActivity.kt
+++ app/src/main/java/com/teamwss/websoso/ui/explore/normalExplore/NormalExploreActivity.kt
@@ -1,6 +1,7 @@
 package com.teamwss.websoso.ui.explore.normalExplore
 
 import android.os.Bundle
+import android.view.WindowManager
 import com.teamwss.websoso.R
 import com.teamwss.websoso.databinding.ActivityNormalExploreBinding
 import com.teamwss.websoso.ui.common.base.BindingActivity
@@ -11,5 +12,26 @@ class NormalExploreActivity :
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_normal_explore)
+
+        setupUI()
     }
+
+    private fun setupUI() {
+        setTranslucentOnStatusBar()
+        setFocusSearchBar()
+        setRecyclerViewAdapter()
+    }
+
+    private fun setTranslucentOnStatusBar() {
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+    }
+
+    private fun setFocusSearchBar() {
+        binding.etNormalExploreSearchContent.requestFocus()
+    }
+
+    private fun setRecyclerViewAdapter() = Unit
 }
---
+++ app/src/main/java/com/teamwss/websoso/ui/explore/normalExplore/NormalExploreViewModel.kt
@@ -0,0 +1,13 @@
+package com.teamwss.websoso.ui.explore.normalExplore
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NormalExploreViewModel : ViewModel() {
+    private val _searchContent: MutableLiveData<String> = MutableLiveData()
+    val searchContent: MutableLiveData<String> get() = _searchContent
+
+    private val _cancelButtonVisibility: MutableLiveData<Boolean> = MutableLiveData()
+    val cancelButtonVisibility: LiveData<Boolean> get() = _cancelButtonVisibility
+}
