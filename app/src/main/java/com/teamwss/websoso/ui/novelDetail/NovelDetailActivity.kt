24fea161470126efddd1bf5ba83d56241ffaf582
junseo511 <prime54@g.skku.edu>
2024-04-14 22:28:37 +0900

    feat: 작품상세 뷰 툴바 색상 변화

--- app/src/main/java/com/teamwss/websoso/ui/novelDetail/NovelDetailActivity.kt
+++ app/src/main/java/com/teamwss/websoso/ui/novelDetail/NovelDetailActivity.kt
@@ -1,11 +1,15 @@
 package com.teamwss.websoso.ui.novelDetail
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import com.google.android.material.tabs.TabLayoutMediator
 import com.teamwss.websoso.R
 import com.teamwss.websoso.databinding.ActivityNovelDetailBinding
 import com.teamwss.websoso.ui.common.base.BindingActivity
 import com.teamwss.websoso.ui.novelDetail.adapter.NovelDetailPagerAdapter
+import kotlin.math.pow
 
 class NovelDetailActivity :
     BindingActivity<ActivityNovelDetailBinding>(R.layout.activity_novel_detail) {
@@ -25,9 +29,13 @@ class NovelDetailActivity :
             tab.text = when (position) {
                 INFO_FRAGMENT_PAGE -> getString(R.string.novel_detail_info)
                 FEED_FRAGMENT_PAGE -> getString(R.string.novel_detail_feed)
-                else -> throw IllegalArgumentException("Invalid position")
+                else -> throw IllegalArgumentException()
             }
         }.attach()
+    }
+
+    private fun Int.changeAlpha(newAlpha: Int): Int {
+        return Color.argb(newAlpha, Color.red(this), Color.green(this), Color.blue(this))
     }
 
     companion object {
