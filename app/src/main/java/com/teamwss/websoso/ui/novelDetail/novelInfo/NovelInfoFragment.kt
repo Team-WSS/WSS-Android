2413cf1d74ca46e35300bea5fb58ae435bd0fdb1
junseo511 <prime54@g.skku.edu>
2024-04-15 14:41:26 +0900

    refactor: 더보기 버튼 변수 위치 수정

--- app/src/main/java/com/teamwss/websoso/ui/novelDetail/novelInfo/NovelInfoFragment.kt
+++ app/src/main/java/com/teamwss/websoso/ui/novelDetail/novelInfo/NovelInfoFragment.kt
@@ -1,7 +1,6 @@
 package com.teamwss.websoso.ui.novelDetail.novelInfo
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.TextView
 import com.teamwss.websoso.R
@@ -12,7 +11,7 @@ class NovelInfoFragment : BindingFragment<FragmentNovelInfoBinding>(R.layout.fra
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setupViewMoreListener(binding.tvNovelInfoIntroBody, binding.tvNovelInfoIntroMore)
+        setupViewMoreListener()
     }
 
     override fun onResume() {
@@ -21,26 +20,31 @@ class NovelInfoFragment : BindingFragment<FragmentNovelInfoBinding>(R.layout.fra
         scrollToTop()
     }
 
-    private fun setupViewMoreListener(contentTextView: TextView, viewMoreTextView: TextView) {
-        contentTextView.post {
-            val lineCount = contentTextView.layout.lineCount
-            if (lineCount < 3) {
-                return@post
-            }
-            if (contentTextView.layout.getEllipsisCount(lineCount - 1) > 0) {
-                viewMoreTextView.visibility = View.VISIBLE
-                contentTextView.ellipsize = null
+    private fun setupViewMoreListener() {
+        val targetText = binding.tvNovelInfoIntroBody
+        val viewMore = binding.tvNovelInfoIntroMore
+
+        targetText.post {
+            val lineCount = targetText.layout.lineCount
+            if (lineCount < 3) return@post
+            if (targetText.layout.getEllipsisCount(lineCount - 1) > 0) {
+                viewMore.visibility = View.VISIBLE
+                targetText.ellipsize = null
 
                 binding.llNovelInfoIntro.setOnClickListener {
-                    contentTextView.maxLines = Int.MAX_VALUE
-                    viewMoreTextView.visibility = View.GONE
+                    targetText.maxLines = Int.MAX_VALUE
+                    viewMore.visibility = View.GONE
                 }
             }
         }
     }
 
     private fun scrollToTop() {
-        binding.nsvNovelInfo.smoothScrollTo(PRIMATE_SCROLL_POSITION, PRIMATE_SCROLL_POSITION, SCROLL_TO_TOP_DURATION)
+        binding.nsvNovelInfo.smoothScrollTo(
+            PRIMATE_SCROLL_POSITION,
+            PRIMATE_SCROLL_POSITION,
+            SCROLL_TO_TOP_DURATION
+        )
     }
 
     companion object {
