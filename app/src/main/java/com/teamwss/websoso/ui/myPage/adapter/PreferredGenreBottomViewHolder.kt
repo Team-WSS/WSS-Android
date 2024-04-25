eb7fb4ac975e7f16b8e70a550abb8098b0d38e0e
yeonjeen <jeendev2019@gmail.com>
2024-04-25 22:21:41 +0900

    feat: 선호장르 상/하 뷰홀더 구현

--- app/src/main/java/com/teamwss/websoso/ui/myPage/adapter/PreferredGenreBottomViewHolder.kt
+++ app/src/main/java/com/teamwss/websoso/ui/myPage/adapter/PreferredGenreBottomViewHolder.kt
@@ -1,4 +1,23 @@
 package com.teamwss.websoso.ui.myPage.adapter
 
-class PreferredGenreBottomViewHolder {
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.teamwss.websoso.data.model.PreferredGenreData
+import com.teamwss.websoso.databinding.ItemPreferredGenreBottomBinding
+import com.teamwss.websoso.databinding.ItemPreferredGenreTopBinding
+
+class PreferredGenreBottomViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private val binding: ItemPreferredGenreBottomBinding = ItemPreferredGenreBottomBinding.bind(itemView)
+    private val genreIcon: ImageView = binding.ivPreferredGenreBottom
+    private val genreName: TextView = binding.tvPreferredGenreBottomName
+    private val genreCount: TextView = binding.tvPreferredGenreBottomCount
+
+    fun bindPreferredGenreBottom(preferredGenreBottom: PreferredGenreData.GenreBottom) {
+        genreIcon.load(preferredGenreBottom.genreIcon)
+        genreName.text = preferredGenreBottom.genreName
+        genreCount.text = preferredGenreBottom.genreCount.toString()
+    }
 }
--- app/src/main/java/com/teamwss/websoso/ui/myPage/adapter/PreferredGenreTopViewHolder.kt
+++ app/src/main/java/com/teamwss/websoso/ui/myPage/adapter/PreferredGenreTopViewHolder.kt
@@ -1,4 +1,23 @@
 package com.teamwss.websoso.ui.myPage.adapter
 
-class PreferredGenreTopViewHolder {
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.teamwss.websoso.data.model.PreferredGenreData
+import com.teamwss.websoso.databinding.ItemPreferredGenreTopBinding
+
+class PreferredGenreTopViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private val binding: ItemPreferredGenreTopBinding = ItemPreferredGenreTopBinding.bind(itemView)
+    private val genreIcon: ImageView = binding.ivPreferredGenreTop
+    private val genreName: TextView = binding.tvPreferredGenreTopName
+    private val genreCount: TextView = binding.tvPreferredGenreTopCount
+
+    fun bindPreferredGenreTop(preferredGenreTop: PreferredGenreData.GenreTop) {
+        genreIcon.load(preferredGenreTop.genreIcon)
+        genreName.text = preferredGenreTop.genreName
+        genreCount.text = preferredGenreTop.genreCount.toString()
+    }
 }
+
