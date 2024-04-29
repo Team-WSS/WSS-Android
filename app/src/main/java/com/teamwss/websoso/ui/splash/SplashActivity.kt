d41f44fd548a9e05760da212ed84c90c90de5e49
librarywon <librarywon@naver.com>
2024-04-29 14:33:04 +0900

    feat: splash activity 기능 구현

--- app/src/main/java/com/teamwss/websoso/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/teamwss/websoso/ui/splash/SplashActivity.kt
@@ -1,12 +1,27 @@
 package com.teamwss.websoso.ui.splash
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
 import com.teamwss.websoso.R
+import com.teamwss.websoso.databinding.ActivityLoginBinding
+import com.teamwss.websoso.ui.common.base.BindingActivity
+import com.teamwss.websoso.ui.login.LoginActivity
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : BindingActivity<ActivityLoginBinding>(R.layout.activity_splash) {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+
+        lifecycleScope.launch {
+            delay(1000L)
+            navigateToLoginActivity()
+        }
+    }
+
+    private fun navigateToLoginActivity() {
+        startActivity(LoginActivity.from(this))
+        finish()
     }
 }
