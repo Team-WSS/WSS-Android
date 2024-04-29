806855dea3d89c049951f9beced669b0e4adb0c9
s9hn <tnghqn@naver.com>
2024-04-29 22:16:48 +0900

    refactor: 카테고리 별 피드업데이트 및 캐싱 전략 리팩터링

--- app/src/main/java/com/teamwss/websoso/data/repository/DefaultFeedRepository.kt
+++ app/src/main/java/com/teamwss/websoso/data/repository/DefaultFeedRepository.kt
@@ -2,22 +2,84 @@ package com.teamwss.websoso.data.repository
 
 import com.teamwss.websoso.data.mapper.FeedMapper.toData
 import com.teamwss.websoso.data.model.FeedEntity
-import com.teamwss.websoso.data.remote.api.FeedApi
-import com.teamwss.websoso.data.remote.request.FeedsRequestDto
+import com.teamwss.websoso.data.remote.response.FeedResponseDto
+import com.teamwss.websoso.data.remote.response.FeedsResponseDto
 
-class DefaultFeedRepository(
-    private val feedApi: FeedApi,
-) {
+class DefaultFeedRepository {
+    private val _cachedFeeds: MutableList<FeedEntity> = mutableListOf()
+    val cachedFeeds: List<FeedEntity> get() = _cachedFeeds.toList()
 
-    suspend fun getFeeds(category: String, lastFeedId: Long, size: Int): List<FeedEntity> {
-        val feeds = feedApi.getFeed(
-            category = category, feedsRequestDto = FeedsRequestDto(
-                lastFeedId = lastFeedId,
-                size = size,
+    suspend fun fetchFeedsByCategory(category: String?, lastFeedId: Long, size: Int) {
+//        val requestBody = FeedsRequestDto(lastFeedId = lastFeedId, size = size)
+//        val result = when (category == null) {
+//            true -> ServiceModule.feedApi.getFeeds(feedsRequestDto = requestBody)
+//            false -> ServiceModule.feedApi.getFeedsByCategory(
+//                category = category,
+//                feedsRequestDto = requestBody
+//            )
+//        }
+//
+//        _cachedFeeds.addAll(result.feedsResponseDto.map { it.toData() })
+
+        _cachedFeeds.addAll(DUMMY.feedsResponseDto.map { it.toData() })
+    }
+
+    suspend fun fetchFeedsByUserId(userId: Long, lastFeedId: Long, size: Int): List<FeedEntity>? {
+        return null
+    }
+
+    suspend fun fetchFeedsByNovelId(novelId: Long, lastFeedId: Long, size: Int): List<FeedEntity>? {
+        return null
+    }
+
+    fun clearCachedFeeds() {
+        _cachedFeeds.clear()
+    }
+
+    companion object {
+        private val DUMMY = FeedsResponseDto(
+            category = "all", feedsResponseDto = listOf(
+                FeedResponseDto(
+                    avatarImage = "https://page-images.kakaoentcdn.com/download/resource?kid=LWSLy/hzN2my4ybO/6AFasaBRdiBRR4RRswKqU1&filename=o1",
+                    commentCount = 22,
+                    createdDate = "3월 21일",
+                    feedContent = "판소추천해요!\n" +
+                            "완결난지는 좀 되었는데 추천합니다. scp 같은 이상현상 물품을 모아놓은 창고를 관리하는 주인공입니다. 배경은 현대아니라 판타지세계라 더 독특해요. 성좌채팅이 있어서 호불호 갈릴 수 있지만 이것도 스토리성이 있는 부분이라 후 잘 모르겠쒀요",
+                    feedId = 0L,
+                    isLiked = false,
+                    isModified = false,
+                    isMyFeed = false,
+                    isSpoiler = false,
+                    likeCount = 10,
+                    nickname = "로판처돌이",
+                    novelId = 0L,
+                    novelRating = 3.7f,
+                    novelRatingCount = 10,
+                    relevantCategories = listOf("로코", "로맨스"),
+                    title = "눈물의 여왕",
+                    userId = 0L
+                ),
+                FeedResponseDto(
+                    avatarImage = "https://page-images.kakaoentcdn.com/download/resource?kid=LWSLy/hzN2my4ybO/6AFasaBRdiBRR4RRswKqU1&filename=o1",
+                    commentCount = 21,
+                    createdDate = "3월 22일",
+                    feedContent = "여주가 세계를 구함\n" +
+                            "이름이 나여주입니다ㅋㅋㅋ읽던 소설이 세계멸망엔딩나서 댓글달았다가 그 세계의 본인에게 빙의하게 되었는데 S급 힐러에 세계관 관련 중요스킬까지 얻고 시작하는 스토리. 121화 최신화 기준 큰 고구마없고 남주가 질서선 댕댕이 기여워요",
+                    feedId = 1L,
+                    isLiked = true,
+                    isModified = false,
+                    isMyFeed = false,
+                    isSpoiler = false,
+                    likeCount = 30,
+                    nickname = "죽은군",
+                    novelId = 1L,
+                    novelRating = 4.0f,
+                    novelRatingCount = 40,
+                    relevantCategories = listOf("무협", "로맨스"),
+                    title = "화산귀환",
+                    userId = 1L
+                )
             )
         )
-
-        return if (feeds.category == category) feeds.feedsResponseDto.map { it.toData() }
-        else throw IllegalArgumentException()
     }
 }
--- app/src/main/java/com/teamwss/websoso/domain/usecase/GetFeedsUseCase.kt
+++ app/src/main/java/com/teamwss/websoso/domain/usecase/GetFeedsUseCase.kt
@@ -11,34 +11,60 @@ import com.teamwss.websoso.domain.model.FeedType.NovelFeed
 class GetFeedsUseCase(
     private val defaultFeedRepository: DefaultFeedRepository,
 ) {
-    private var lastFeedId: Long = 0
+    private var lastFeedId: Long = DEFAULT_ID
+    private var selectedCategory: String? = NOTHING
 
-    suspend operator fun invoke(category: String = DEFAULT_VALUE, feedType: FeedType): List<Feed> {
+    suspend operator fun invoke(
+        category: String = DEFAULT_CATEGORY,
+        feedType: FeedType
+    ): List<Feed> {
+        fetchFeedsByFeedType(feedType, if (category == DEFAULT_CATEGORY) null else category)
+
+        defaultFeedRepository.cachedFeeds.apply {
+            lastFeedId = if (isNotEmpty()) last().id else DEFAULT_ID // 리스트가 빈 경우, 본 로직이 없으면 터지나?
+            return map { it.toDomain() }
+        }
+    }
+
+    private suspend fun fetchFeedsByFeedType(feedType: FeedType, category: String?) {
         when (feedType) {
-            is MainFeed -> defaultFeedRepository.getFeeds(
-                category = category, lastFeedId = lastFeedId, size = FEEDS_SIZE,
+            is MainFeed -> {
+                checkCategory(category)
+                defaultFeedRepository.fetchFeedsByCategory(
+                    category = category,
+                    lastFeedId = lastFeedId,
+                    size = if (lastFeedId == DEFAULT_ID) 30 else 15,
+                )
+            }
+
+            is MyFeed -> defaultFeedRepository.fetchFeedsByUserId(
+                userId = feedType.userId, lastFeedId = lastFeedId, size = 10,
             )
 
-            is MyFeed -> defaultFeedRepository.getFeedsByUserId(
-                userId = feedType.userId, lastFeedId = lastFeedId, size = FEEDS_SIZE,
-            )
 
-            is NovelFeed -> defaultFeedRepository.getFeedsByNovelId(
-                novelId = feedType.novelId, lastFeedId = lastFeedId, size = FEEDS_SIZE,
+            is NovelFeed -> defaultFeedRepository.fetchFeedsByNovelId(
+                novelId = feedType.novelId, lastFeedId = lastFeedId, size = 10,
             )
         }
+    }
 
-        val feeds = defaultFeedRepository.getFeeds(
-            category = category, lastFeedId = lastFeedId, size = FEEDS_SIZE,
-        )
+    private fun checkCategory(category: String?) {
+        if (selectedCategory != category) {
+            selectedCategory = category
+            clear()
+        }
+    }
 
-        lastFeedId = feeds.last().id
-
-        return feeds.map { it.toDomain() }
+    private fun clear() {
+        if (defaultFeedRepository.cachedFeeds.isNotEmpty()) {
+            defaultFeedRepository.clearCachedFeeds()
+            lastFeedId = DEFAULT_ID
+        }
     }
 
     companion object {
-        private const val DEFAULT_VALUE = "all"
-        private const val FEEDS_SIZE = 20
+        private const val NOTHING: String = ""
+        private const val DEFAULT_CATEGORY: String = "전체"
+        private const val DEFAULT_ID: Long = 0
     }
 }
