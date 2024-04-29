36ccdd0dd03bc353afcac4515843255c88ffdf26
librarywon <librarywon@naver.com>
2024-04-29 14:07:26 +0900

    feat: splash activity, xml 최초 생성

---
+++ app/src/main/java/com/teamwss/websoso/ui/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.teamwss.websoso.ui.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.teamwss.websoso.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
