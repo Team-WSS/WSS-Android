4d819c01d76cac162f72a7420f1adc4b45a2b4cd
s9hn <tnghqn@naver.com>
2024-04-30 14:29:03 +0900

    refactor: 카테고리 state 분리 및 모델 생성

--- app/src/main/java/com/teamwss/websoso/ui/feed/model/FeedUiState.kt
+++ app/src/main/java/com/teamwss/websoso/ui/feed/model/FeedUiState.kt
@@ -3,6 +3,12 @@ package com.teamwss.websoso.ui.feed.model
 data class FeedUiState(
     val loading: Boolean = true,
     val error: Boolean = false,
-    val categories: String = "",
+    val categories: List<CategoryUiState>,
     val feeds: List<FeedModel> = emptyList(),
-)
+) {
+
+    data class CategoryUiState(
+        val category: Category,
+        val isSelected: Boolean,
+    )
+}
