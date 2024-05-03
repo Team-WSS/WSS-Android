194663437638fee0f155554169a71fed8748fbda
s9hn <tnghqn@naver.com>
2024-05-03 12:56:52 +0900

    refactor: 어댑터 가독성 개선

--- app/src/main/java/com/teamwss/websoso/ui/feed/adapter/FeedAdapter.kt
+++ app/src/main/java/com/teamwss/websoso/ui/feed/adapter/FeedAdapter.kt
@@ -5,45 +5,51 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.teamwss.websoso.ui.feed.FeedItemClickListener
+import com.teamwss.websoso.ui.feed.adapter.FeedType.Feed
+import com.teamwss.websoso.ui.feed.adapter.FeedType.Loading
+import com.teamwss.websoso.ui.feed.adapter.FeedType.ItemType.*
 
 class FeedAdapter(
-    private val feedItemClickListener: FeedItemClickListener
-) : ListAdapter<FeedItemType, RecyclerView.ViewHolder>(diffCallBack) {
+    private val feedItemClickListener: FeedItemClickListener,
+) : ListAdapter<FeedType, RecyclerView.ViewHolder>(diffCallBack) {
 
     init {
         setHasStableIds(true)
     }
 
-    override fun getItemViewType(position: Int): Int = getItem(position).viewType
+    override fun getItemViewType(position: Int): Int = when (getItem(position)) {
+        is Feed -> FEED.ordinal
+        is Loading -> LOADING.ordinal
+    }
 
     override fun getItemId(position: Int): Long = when (getItem(position)) {
-        is FeedItemType.Feed -> (getItem(position) as FeedItemType.Feed).feed.id
-        is FeedItemType.Loading -> super.getItemId(position)
+        is Feed -> (getItem(position) as Feed).feed.id
+        is Loading -> super.getItemId(position)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
-        when (viewType) {
-            FeedItemType.FEED -> FeedViewHolder.of(parent, feedItemClickListener)
-            FeedItemType.LOADING -> FeedLoadingViewHolder.from(parent)
-            else -> throw IllegalArgumentException()
+        when (FeedType.ItemType.valueOf(viewType)) {
+            FEED -> FeedViewHolder.of(parent, feedItemClickListener)
+            LOADING -> FeedLoadingViewHolder.from(parent)
         }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         when (holder) {
-            is FeedViewHolder -> holder.bind((getItem(position) as FeedItemType.Feed).feed)
+            is FeedViewHolder -> holder.bind((getItem(position) as Feed).feed)
             is FeedLoadingViewHolder -> return
         }
     }
 
     companion object {
-        private val diffCallBack = object : DiffUtil.ItemCallback<FeedItemType>() {
-            override fun areItemsTheSame(oldItem: FeedItemType, newItem: FeedItemType): Boolean {
-                return oldItem == newItem
-            }
+        private val diffCallBack = object : DiffUtil.ItemCallback<FeedType>() {
+            override fun areItemsTheSame(oldItem: FeedType, newItem: FeedType): Boolean =
+                when ((oldItem is Feed) and (newItem is Feed)) {
+                    true -> (oldItem as Feed).feed.id == (newItem as Feed).feed.id
+                    false -> oldItem == newItem
+                }
 
-            override fun areContentsTheSame(oldItem: FeedItemType, newItem: FeedItemType): Boolean {
-                return oldItem == newItem
-            }
+            override fun areContentsTheSame(oldItem: FeedType, newItem: FeedType): Boolean =
+                oldItem == newItem
         }
     }
 }
---
+++ app/src/main/java/com/teamwss/websoso/ui/feed/adapter/FeedType.kt
@@ -0,0 +1,21 @@
+package com.teamwss.websoso.ui.feed.adapter
+
+import com.teamwss.websoso.ui.feed.model.FeedModel
+
+sealed class FeedType {
+    data class Feed(
+        val feed: FeedModel,
+    ) : FeedType()
+
+    data object Loading : FeedType()
+
+    enum class ItemType {
+        FEED, LOADING;
+
+        companion object {
+
+            fun valueOf(ordinal: Int): ItemType =
+                values().find { it.ordinal == ordinal } ?: throw IllegalArgumentException()
+        }
+    }
+}
