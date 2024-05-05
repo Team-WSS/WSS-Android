3bab273be5f075f3e1526288eb600ed648da85e4
s9hn <tnghqn@naver.com>
2024-05-05 23:54:19 +0900

    refactor: 피드 API 중복호출 방지

--- app/src/main/java/com/teamwss/websoso/ui/feed/FeedScrollListener.kt
+++ app/src/main/java/com/teamwss/websoso/ui/feed/FeedScrollListener.kt
@@ -6,29 +6,27 @@ import androidx.recyclerview.widget.RecyclerView
 class FeedScrollListener private constructor(
     private val event: () -> Unit,
 ) : RecyclerView.OnScrollListener() {
+    private var label: Int = 0
 
     override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
         super.onScrollStateChanged(recyclerView, newState)
-        // 보여지는 뷰에 한해서 마지막 아이템의 포지션을 나타낸다.
-        val observedLastPosition: Int =
+        val visibleLastItemPosition: Int =
             (recyclerView.layoutManager as LinearLayoutManager).findLastCompletelyVisibleItemPosition()
-        // 현재 어댑터가 가지고 있는 아이템의 총 갯수이다.
         val totalItemCount: Int =
-            (recyclerView.adapter?.itemCount ?: throw IllegalArgumentException()) - 1
+            recyclerView.adapter?.itemCount ?: throw IllegalArgumentException()
 
-        if (observedLastPosition == totalItemCount) {
-            // 로딩뷰 적
-            event()
+        if (visibleLastItemPosition in totalItemCount - 2..totalItemCount) {
+            when (label) {
+                0 -> return
+                1 -> event()
+            }
         }
-
-        // 디바운스 필요
-        // 아예 아이템이 안오면 더 이상 로드할 데이터가 없다는 문구도 띄워야한다.
     }
 
     companion object {
+        private const val READY = 0
+        private const val PROGRESS = 1
 
-        fun from(
-            event: () -> Unit,
-        ): FeedScrollListener = FeedScrollListener(event)
+        fun from(event: () -> Unit): FeedScrollListener = FeedScrollListener(event)
     }
 }
