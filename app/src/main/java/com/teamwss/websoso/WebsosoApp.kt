c6c861caed9ae611b02c573763d87b605f0dd441
s9hn <tnghqn@naver.com>
2024-05-06 23:50:36 +0900

    refactor: 의존성 주입로직 추가

--- app/src/main/java/com/teamwss/websoso/WebsosoApp.kt
+++ app/src/main/java/com/teamwss/websoso/WebsosoApp.kt
@@ -2,8 +2,10 @@ package com.teamwss.websoso
 
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
-import com.teamwss.websoso.data.repository.FakeFeedRepository
+import com.teamwss.websoso.data.FakeApi
+import com.teamwss.websoso.data.repository.DefaultFeedRepository
 import com.teamwss.websoso.data.repository.FakeUserRepository
+import com.teamwss.websoso.domain.usecase.GetCategoryByUserGenderUseCase
 import com.teamwss.websoso.domain.usecase.GetFeedsUseCase
 
 class WebsosoApp : Application() {
@@ -16,6 +18,9 @@ class WebsosoApp : Application() {
 
         fun getUserRepository(): FakeUserRepository = FakeUserRepository()
 
-        fun getFeedsUseCase(): GetFeedsUseCase = GetFeedsUseCase(FakeFeedRepository())
+        fun getFeedsUseCase(): GetFeedsUseCase = GetFeedsUseCase(DefaultFeedRepository(FakeApi))
+
+        fun getCategoryByUserGenderUseCase(): GetCategoryByUserGenderUseCase =
+            GetCategoryByUserGenderUseCase(getUserRepository())
     }
 }
