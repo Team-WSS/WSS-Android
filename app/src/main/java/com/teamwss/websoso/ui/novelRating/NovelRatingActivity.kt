4b2f1398ce07a0f2713bb10b7bebb7064e691541
junseo511 <prime54@g.skku.edu>
2024-05-28 09:24:50 +0900

    feat: 작품 평가 액티비티 및 뷰모델 추가

---
+++ app/src/main/java/com/teamwss/websoso/ui/novelRating/NovelRatingActivity.kt
@@ -0,0 +1,9 @@
+package com.teamwss.websoso.ui.novelRating
+
+import com.teamwss.websoso.R
+import com.teamwss.websoso.databinding.ActivityNovelRatingBinding
+import com.teamwss.websoso.ui.common.base.BindingActivity
+
+class NovelRatingActivity :
+    BindingActivity<ActivityNovelRatingBinding>(R.layout.activity_novel_rating) {
+}
---
+++ app/src/main/java/com/teamwss/websoso/ui/novelRating/NovelRatingViewModel.kt
@@ -0,0 +1,5 @@
+package com.teamwss.websoso.ui.novelRating
+
+import androidx.lifecycle.ViewModel
+
+class NovelRatingViewModel : ViewModel()
