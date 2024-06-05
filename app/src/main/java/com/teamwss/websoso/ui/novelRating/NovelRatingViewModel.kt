c60a655e4ae5bef566c2c064b8180ed0d3a8882f
junseo511 <prime54@g.skku.edu>
2024-06-05 16:09:37 +0900

    refactor: 함수 순서 변경

--- app/src/main/java/com/teamwss/websoso/ui/novelRating/NovelRatingViewModel.kt
+++ app/src/main/java/com/teamwss/websoso/ui/novelRating/NovelRatingViewModel.kt
@@ -21,79 +21,6 @@ class NovelRatingViewModel : ViewModel() {
     private val _isEditingStartDate = MutableLiveData<Boolean>()
     val isEditingStartDate: LiveData<Boolean> get() = _isEditingStartDate
 
-    fun getDummy() {
-        _uiState.value = NovelRatingUiState(
-            novelRatingModel = NovelRatingModel(
-                userNovelId = 1,
-                novelTitle = "철혈검가 사냥개의 회귀",
-                userNovelRating = 4.0f,
-                readStatus = "WATCHED",
-                startDate = "2023-02-28",
-                endDate = "2024-05-11"
-            ),
-            keywordModel = KeywordModel(
-                categories = listOf(
-                    KeywordModel.Category(
-                        categoryName = "세계관",
-                        keywords = listOf(
-                            KeywordModel.Category.Keyword(keywordId = 1, keywordName = "이세계"),
-                            KeywordModel.Category.Keyword(keywordId = 2, keywordName = "현대"),
-                            KeywordModel.Category.Keyword(keywordId = 3, keywordName = "서양풍/중세시대"),
-                            KeywordModel.Category.Keyword(keywordId = 4, keywordName = "SF"),
-                            KeywordModel.Category.Keyword(keywordId = 5, keywordName = "동양풍/사극"),
-                            KeywordModel.Category.Keyword(keywordId = 6, keywordName = "학원/아카데미"),
-                            KeywordModel.Category.Keyword(keywordId = 7, keywordName = "실존역사"),
-                            KeywordModel.Category.Keyword(keywordId = 16, keywordName = "전투"),
-                            KeywordModel.Category.Keyword(keywordId = 17, keywordName = "로맨스"),
-                            KeywordModel.Category.Keyword(keywordId = 18, keywordName = "판타지"),
-                            KeywordModel.Category.Keyword(keywordId = 19, keywordName = "드라마"),
-                            KeywordModel.Category.Keyword(keywordId = 20, keywordName = "스릴러"),
-                        )
-                    ),
-                    KeywordModel.Category(
-                        categoryName = "소재",
-                        keywords = listOf(
-                            KeywordModel.Category.Keyword(keywordId = 8, keywordName = "웹툰화"),
-                            KeywordModel.Category.Keyword(keywordId = 9, keywordName = "드라마화")
-                        )
-                    ),
-                    KeywordModel.Category(
-                        categoryName = "캐릭터",
-                        keywords = listOf(
-                            KeywordModel.Category.Keyword(keywordId = 10, keywordName = "영웅"),
-                            KeywordModel.Category.Keyword(keywordId = 11, keywordName = "악당/빌런")
-                        )
-                    ),
-                    KeywordModel.Category(
-                        categoryName = "관계",
-                        keywords = listOf(
-                            KeywordModel.Category.Keyword(keywordId = 12, keywordName = "친구"),
-                            KeywordModel.Category.Keyword(keywordId = 13, keywordName = "동료")
-                        )
-                    ),
-                    KeywordModel.Category(
-                        categoryName = "분위기",
-                        keywords = listOf(
-                            KeywordModel.Category.Keyword(keywordId = 14, keywordName = "뻔한"),
-                            KeywordModel.Category.Keyword(keywordId = 15, keywordName = "반전있는")
-                        )
-                    )
-                )
-            ),
-        )
-        dummyInit()
-    }
-
-    private fun dummyInit() {
-        val uiState = uiState.value ?: return
-        _isEditingStartDate.value =
-            RatingDateManager().updateIsEditingStartDate(uiState.novelRatingModel.uiReadStatus)
-        _maxDayValue.value = RatingDateManager().updateDayMaxValue(
-            uiState.novelRatingModel.ratingDateModel,
-            isEditingStartDate.value!!
-        )
-    }
-
     fun updatePastDate() {
         val uiState = uiState.value ?: return
         val ratingDateModel = uiState.novelRatingModel.ratingDateModel
@@ -285,6 +212,80 @@ class NovelRatingViewModel : ViewModel() {
                 categories = updatedCategories,
                 currentSelectedKeywords = uiState.keywordModel.pastSelectedKeywords
             )
+        )
+    }
+
+    // 날릴예정
+    fun getDummy() {
+        _uiState.value = NovelRatingUiState(
+            novelRatingModel = NovelRatingModel(
+                userNovelId = 1,
+                novelTitle = "철혈검가 사냥개의 회귀",
+                userNovelRating = 4.0f,
+                readStatus = "WATCHED",
+                startDate = "2023-02-28",
+                endDate = "2024-05-11"
+            ),
+            keywordModel = KeywordModel(
+                categories = listOf(
+                    KeywordModel.Category(
+                        categoryName = "세계관",
+                        keywords = listOf(
+                            KeywordModel.Category.Keyword(keywordId = 1, keywordName = "이세계"),
+                            KeywordModel.Category.Keyword(keywordId = 2, keywordName = "현대"),
+                            KeywordModel.Category.Keyword(keywordId = 3, keywordName = "서양풍/중세시대"),
+                            KeywordModel.Category.Keyword(keywordId = 4, keywordName = "SF"),
+                            KeywordModel.Category.Keyword(keywordId = 5, keywordName = "동양풍/사극"),
+                            KeywordModel.Category.Keyword(keywordId = 6, keywordName = "학원/아카데미"),
+                            KeywordModel.Category.Keyword(keywordId = 7, keywordName = "실존역사"),
+                            KeywordModel.Category.Keyword(keywordId = 16, keywordName = "전투"),
+                            KeywordModel.Category.Keyword(keywordId = 17, keywordName = "로맨스"),
+                            KeywordModel.Category.Keyword(keywordId = 18, keywordName = "판타지"),
+                            KeywordModel.Category.Keyword(keywordId = 19, keywordName = "드라마"),
+                            KeywordModel.Category.Keyword(keywordId = 20, keywordName = "스릴러"),
+                        )
+                    ),
+                    KeywordModel.Category(
+                        categoryName = "소재",
+                        keywords = listOf(
+                            KeywordModel.Category.Keyword(keywordId = 8, keywordName = "웹툰화"),
+                            KeywordModel.Category.Keyword(keywordId = 9, keywordName = "드라마화")
+                        )
+                    ),
+                    KeywordModel.Category(
+                        categoryName = "캐릭터",
+                        keywords = listOf(
+                            KeywordModel.Category.Keyword(keywordId = 10, keywordName = "영웅"),
+                            KeywordModel.Category.Keyword(keywordId = 11, keywordName = "악당/빌런")
+                        )
+                    ),
+                    KeywordModel.Category(
+                        categoryName = "관계",
+                        keywords = listOf(
+                            KeywordModel.Category.Keyword(keywordId = 12, keywordName = "친구"),
+                            KeywordModel.Category.Keyword(keywordId = 13, keywordName = "동료")
+                        )
+                    ),
+                    KeywordModel.Category(
+                        categoryName = "분위기",
+                        keywords = listOf(
+                            KeywordModel.Category.Keyword(keywordId = 14, keywordName = "뻔한"),
+                            KeywordModel.Category.Keyword(keywordId = 15, keywordName = "반전있는")
+                        )
+                    )
+                )
+            ),
+        )
+        dummyInit()
+    }
+
+    private fun dummyInit() {
+        val uiState = uiState.value ?: return
+        _isEditingStartDate.value =
+            RatingDateManager().updateIsEditingStartDate(uiState.novelRatingModel.uiReadStatus)
+        _maxDayValue.value = RatingDateManager().updateDayMaxValue(
+            uiState.novelRatingModel.ratingDateModel,
+            isEditingStartDate.value!!
         )
     }
 }
