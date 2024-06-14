ffc1cd9fe34695ce96ebb270986a2873be48327c
yeonjeen <jeendev2019@gmail.com>
2024-06-15 03:29:49 +0900

    feat: 마이페이지 프로필 데이터 클래스

---
+++ app/src/main/java/com/teamwss/websoso/data/model/MyPageProfileData.kt
@@ -0,0 +1,8 @@
+package com.teamwss.websoso.data.model
+
+data class MyPageProfileData(
+    val avatarImage: String,
+    val genrePreferences: List<String>,
+    val intro: String,
+    val nickname: String
+)
