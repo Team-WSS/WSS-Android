14e1f5c79e3b124050c10b0e27f9d9d0936509ce
yeonjeen <jeendev2019@gmail.com>
2024-06-15 03:33:05 +0900

    feat: 내 서재 보관함 count 데이터 클래스

---
+++ app/src/main/java/com/teamwss/websoso/data/model/MyLibraryStorageData.kt
@@ -0,0 +1,8 @@
+package com.teamwss.websoso.data.model
+
+data class MyLibraryStorageData (
+    val interestingNovel : Int,
+    val watchingNovel : Int,
+    val watchedNovel : Int,
+    val quitNovel : Int,
+)
