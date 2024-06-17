cdbbf5a52f668a90df8c9b6ddb8e8472156060d4
yeonjeen <jeendev2019@gmail.com>
2024-06-18 01:47:52 +0900

    refactor: 마이페이지 뷰모델 프로필 타입 명시

--- app/src/main/java/com/teamwss/websoso/ui/myPage/MyPageViewModel.kt
+++ app/src/main/java/com/teamwss/websoso/ui/myPage/MyPageViewModel.kt
@@ -6,13 +6,14 @@ import androidx.lifecycle.ViewModel
 import com.teamwss.websoso.data.model.MyPageProfileData
 
 class MyPageViewModel : ViewModel() {
-    private val _myPageProfile = MutableLiveData<MyPageProfileData>().apply {
-        value = MyPageProfileData(
-            avatarImage = "https://velog.velcdn.com/images/yeonjeen/post/73663b4d-ead3-41fa-bab9-4fd1a1651f91/image.png",
-            genrePreferences = listOf("Fantasy", "Romance", "Action"),
-            intro = "안녕 나는 사실 연진이야",
-            nickname = "연진"
-        )
-    }
+    private val _myPageProfile: MutableLiveData<MyPageProfileData> =
+        MutableLiveData<MyPageProfileData>().apply {
+            value = MyPageProfileData(
+                avatarImage = "https://velog.velcdn.com/images/yeonjeen/post/73663b4d-ead3-41fa-bab9-4fd1a1651f91/image.png",
+                genrePreferences = listOf("Fantasy", "Romance", "Action"),
+                intro = "안녕 나는 사실 연진이야",
+                nickname = "연진"
+            )
+        }
     val myPageProfile: LiveData<MyPageProfileData> = _myPageProfile
 }
