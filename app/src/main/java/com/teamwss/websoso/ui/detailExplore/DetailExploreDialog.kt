231d361b7fe9113fbd7db934f678c76ea1dad67b
Son Myeongji <sonmyungj1zz@gmail.com>
2024-06-26 15:24:49 +0900

    feat: 바텀시트 모서리 둥글게 수정

---
+++ app/src/main/java/com/teamwss/websoso/ui/detailExplore/DetailExploreDialog.kt
@@ -0,0 +1,17 @@
+package com.teamwss.websoso.ui.detailExplore
+
+import android.os.Bundle
+import android.view.View
+import com.teamwss.websoso.R
+import com.teamwss.websoso.databinding.DialogDetailExploreBinding
+import com.teamwss.websoso.ui.common.base.BindingBottomSheetDialog
+
+class DetailExploreDialog :
+    BindingBottomSheetDialog<DialogDetailExploreBinding>(R.layout.dialog_detail_explore) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
