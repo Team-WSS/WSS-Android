d163d2feb4e8c0a53a9c6529cb15f71425f8d56a
junseo511 <prime54@g.skku.edu>
2024-06-27 15:16:44 +0900

    refactor: NovelDetailUiState 생성자 수정

--- app/src/main/java/com/teamwss/websoso/ui/novelDetail/model/NovelDetailUiState.kt
+++ app/src/main/java/com/teamwss/websoso/ui/novelDetail/model/NovelDetailUiState.kt
@@ -3,10 +3,31 @@ package com.teamwss.websoso.ui.novelDetail.model
 data class NovelDetailUiState(
     val loading: Boolean = true,
     val error: Boolean = false,
-    val novelDetail: NovelDetailModel =
-        NovelDetailModel(
-            NovelDetailModel.UserNovelModel(0, "", null, null, false, 0f),
-            NovelDetailModel.NovelModel("", "", emptyList(), "", false, ""),
-            NovelDetailModel.UserRatingModel(0, 0f, 0, 0),
+    val novelDetail: NovelDetailModel = NovelDetailModel(
+        NovelDetailModel.UserNovelModel(
+            userNovelId = null,
+            readStatus = null,
+            startDate = null,
+            endDate = null,
+            isUserNovelInterest = false,
+            userNovelRating = 0.0f,
+            hasUserNovelInfo = false
         ),
+        NovelDetailModel.NovelModel(
+            novelTitle = "",
+            novelImage = "",
+            novelGenres = listOf(),
+            formattedNovelGenres = "",
+            novelGenreImage = "",
+            isNovelCompleted = false,
+            author = ""
+        ),
+        NovelDetailModel.UserRatingModel(
+            interestCount = 0,
+            novelRating = 0.0f,
+            novelRatingCount = 0,
+            formattedNovelRating = "",
+            feedCount = 0
+        ),
+    ),
 )
