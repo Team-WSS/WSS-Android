7b5433ffda8fd0f02802cd8876c5f6e15063e0e5
Son Myeongji <sonmyungj1zz@gmail.com>
2024-06-27 21:13:04 +0900

    feat: 장르 웹소소 칩 구현

--- app/src/main/java/com/teamwss/websoso/ui/detailExplore/info/DetailExploreInfoFragment.kt
+++ app/src/main/java/com/teamwss/websoso/ui/detailExplore/info/DetailExploreInfoFragment.kt
@@ -5,6 +5,8 @@ import android.view.View
 import com.teamwss.websoso.R
 import com.teamwss.websoso.databinding.FragmentDetailExploreInfoBinding
 import com.teamwss.websoso.ui.common.base.BindingFragment
+import com.teamwss.websoso.ui.common.customView.WebsosoChip
+import com.teamwss.websoso.ui.detailExplore.info.model.Genre
 
 class DetailExploreInfoFragment :
     BindingFragment<FragmentDetailExploreInfoBinding>(R.layout.fragment_detail_explore_info) {
@@ -12,10 +14,23 @@ class DetailExploreInfoFragment :
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        setupUI()
+        setupGenreChips()
     }
 
-    private fun setupUI() {
-        
+    private fun setupGenreChips() {
+        val genres = Genre.values().toList()
+        genres.forEach { genre ->
+            WebsosoChip(requireContext()).apply {
+                setWebsosoChipText(genre.title)
+                setWebsosoChipTextAppearance(R.style.body2)
+                setWebsosoChipTextColor(R.color.bg_detail_explore_chip_text_selector)
+                setWebsosoChipStrokeColor(R.color.bg_detail_explore_chip_stroke_selector)
+                setWebsosoChipBackgroundColor(R.color.bg_detail_explore_chip_background_selector)
+                setWebsosoChipPaddingVertical(30f)
+                setWebsosoChipPaddingHorizontal(12f)
+                setWebsosoChipRadius(45f)
+                setOnWebsosoChipClick { } // TODO 추후 추가 예정
+            }.also { websosoChip -> binding.wcgDetailExploreInfoGenre.addChip(websosoChip) }
+        }
     }
 }
