942c35981fd1d911f4672f520084a7b5745770e9
Son Myeongji <sonmyungj1zz@gmail.com>
2024-06-27 23:49:42 +0900

    feat: 바텀시트 버튼 통해서만 닫히도록 구현

--- app/src/main/java/com/teamwss/websoso/ui/detailExplore/DetailExploreDialog.kt
+++ app/src/main/java/com/teamwss/websoso/ui/detailExplore/DetailExploreDialog.kt
@@ -2,6 +2,8 @@ package com.teamwss.websoso.ui.detailExplore
 
 import android.os.Bundle
 import android.view.View
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.teamwss.websoso.R
 import com.teamwss.websoso.databinding.DialogDetailExploreBinding
 import com.teamwss.websoso.ui.common.base.BindingBottomSheetDialog
@@ -13,7 +15,16 @@ class DetailExploreDialog :
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setupBottomSheet()
         setupFragmentContainer()
+        onBottomSheetExitButtonClick()
+    }
+
+    private fun setupBottomSheet() {
+        (dialog as BottomSheetDialog).behavior.state = BottomSheetBehavior.STATE_EXPANDED
+        (dialog as BottomSheetDialog).behavior.isDraggable = false
+        (dialog as BottomSheetDialog).behavior.isHideable = false
+        (dialog as BottomSheetDialog).setCancelable(false)
     }
 
     private fun setupFragmentContainer() {
@@ -21,5 +32,11 @@ class DetailExploreDialog :
         childFragmentManager.beginTransaction()
             .replace(R.id.fcv_detail_explore, DetailExploreInfoFragment())
             .commit()
+    }
+
+    private fun onBottomSheetExitButtonClick() {
+        binding.ivDetailExploreExitButton.setOnClickListener {
+            dismiss()
+        }
     }
 }
