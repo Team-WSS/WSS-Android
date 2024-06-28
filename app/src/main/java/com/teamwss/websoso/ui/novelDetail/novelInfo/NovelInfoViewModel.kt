d6e794126e8bb143f7937ead3edf8fbf8bb8b95d
junseo511 <prime54@g.skku.edu>
2024-06-28 13:39:52 +0900

    refactor: 함수 구조 수정 및 함수명 변경

--- app/src/main/java/com/teamwss/websoso/ui/novelDetail/novelInfo/NovelInfoViewModel.kt
+++ app/src/main/java/com/teamwss/websoso/ui/novelDetail/novelInfo/NovelInfoViewModel.kt
@@ -6,9 +6,11 @@ import androidx.lifecycle.ViewModel
 import com.teamwss.websoso.data.remote.response.NovelInfoResponseDto
 
 class NovelInfoViewModel : ViewModel() {
-    private val _expandTextToggleVisibility: MutableLiveData<Boolean> = MutableLiveData<Boolean>(false)
+    private val _expandTextToggleVisibility: MutableLiveData<Boolean> =
+        MutableLiveData<Boolean>(false)
     val expandTextToggleVisibility: LiveData<Boolean> get() = _expandTextToggleVisibility
-    private val _isExpandTextToggleEnabled: MutableLiveData<Boolean> = MutableLiveData<Boolean>(true)
+    private val _isExpandTextToggleEnabled: MutableLiveData<Boolean> =
+        MutableLiveData<Boolean>(true)
     val isExpandTextToggleEnabled: LiveData<Boolean> get() = _isExpandTextToggleEnabled
     private val _bodyMaxLines: MutableLiveData<Int> = MutableLiveData<Int>(DEFAULT_MAX_LINES)
     val bodyMaxLines: LiveData<Int> get() = _bodyMaxLines
@@ -21,43 +23,47 @@ class NovelInfoViewModel : ViewModel() {
             NovelInfoResponseDto(
                 novelDescription = "설명 어쩌구",
                 platforms =
-                    listOf(
-                        NovelInfoResponseDto.PlatformResponseDto(
-                            platformName = "네이버시리즈",
-                            platformImage = "https://ssl.pstatic.net/static/nstore/ogtag_series_v2.png",
-                            platformUrl = "https://www.naver.com",
-                        ),
+                listOf(
+                    NovelInfoResponseDto.PlatformResponseDto(
+                        platformName = "네이버시리즈",
+                        platformImage = "https://ssl.pstatic.net/static/nstore/ogtag_series_v2.png",
+                        platformUrl = "https://www.naver.com",
                     ),
+                ),
                 attractivePoints = listOf("소재", "관계", "분위기"),
                 keywords =
-                    listOf(
-                        NovelInfoResponseDto.KeywordResponseDto(
-                            keywordName = "서양풍/중세시대",
-                            keywordCount = 5,
-                        ),
-                        NovelInfoResponseDto.KeywordResponseDto(
-                            keywordName = "웹툰화",
-                            keywordCount = 4,
-                        ),
-                        NovelInfoResponseDto.KeywordResponseDto(
-                            keywordName = "동양풍/사극",
-                            keywordCount = 4,
-                        ),
+                listOf(
+                    NovelInfoResponseDto.KeywordResponseDto(
+                        keywordName = "서양풍/중세시대",
+                        keywordCount = 5,
                     ),
+                    NovelInfoResponseDto.KeywordResponseDto(
+                        keywordName = "웹툰화",
+                        keywordCount = 4,
+                    ),
+                    NovelInfoResponseDto.KeywordResponseDto(
+                        keywordName = "동양풍/사극",
+                        keywordCount = 4,
+                    ),
+                ),
                 watchingCount = 36,
                 watchedCount = 5,
                 quitCount = 2,
             )
     }
 
-    fun onViewMoreClicked() {
-        if (_isExpandTextToggleEnabled.value == true) {
-            _isExpandTextToggleEnabled.value = false
-            _bodyMaxLines.value = Int.MAX_VALUE
-            return
+    fun updateExpandTextToggle() {
+        when (_isExpandTextToggleEnabled.value == true) {
+            true -> {
+                _isExpandTextToggleEnabled.value = false
+                _bodyMaxLines.value = Int.MAX_VALUE
+            }
+
+            false -> {
+                _isExpandTextToggleEnabled.value = true
+                _bodyMaxLines.value = DEFAULT_MAX_LINES
+            }
         }
-        _isExpandTextToggleEnabled.value = true
-        _bodyMaxLines.value = DEFAULT_MAX_LINES
     }
 
     fun updateExpandTextToggleVisibility(
