6722cadb17f8f7ea7891126455559ab7eb3e31da
Son Myeongji <sonmyungj1zz@gmail.com>
2024-07-08 16:21:18 +0900

    feat: 클릭리스너 구현

--- app/src/main/java/com/teamwss/websoso/ui/detailExplore/keyword/DetailExploreKeywordViewModel.kt
+++ app/src/main/java/com/teamwss/websoso/ui/detailExplore/keyword/DetailExploreKeywordViewModel.kt
@@ -1,10 +1,24 @@
 package com.teamwss.websoso.ui.detailExplore.keyword
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class DetailExploreKeywordViewModel @Inject constructor() : ViewModel() {
+    private val _searchWord: MutableLiveData<String> = MutableLiveData()
+    val searchWord: MutableLiveData<String> get() = _searchWord
 
+    private val _isSearchCancelButtonVisibility: MutableLiveData<Boolean> = MutableLiveData(false)
+    val isSearchCancelButtonVisibility: LiveData<Boolean> get() = _isSearchCancelButtonVisibility
+
+    fun validateSearchWordClearButton() {
+        _isSearchCancelButtonVisibility.value = _searchWord.value.isNullOrEmpty().not()
+    }
+
+    fun updateSearchWordEmpty() {
+        _searchWord.value = ""
+    }
 }
