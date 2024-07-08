e876e0d830eb54fea2b3e5457c4f3b3152fbefed
Son Myeongji <sonmyungj1zz@gmail.com>
2024-07-08 15:09:51 +0900

    feat: 상세탐색 키워드 xml 구현

---
+++ app/src/main/java/com/teamwss/websoso/ui/detailExplore/keyword/DetailExploreKeywordFragment.kt
@@ -0,0 +1,15 @@
+package com.teamwss.websoso.ui.detailExplore.keyword
+
+import android.os.Bundle
+import android.view.View
+import com.teamwss.websoso.R
+import com.teamwss.websoso.databinding.FragmentDetailExploreKeywordBinding
+import com.teamwss.websoso.ui.common.base.BindingFragment
+
+class DetailExploreKeywordFragment :
+    BindingFragment<FragmentDetailExploreKeywordBinding>(R.layout.fragment_detail_explore_keyword) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
