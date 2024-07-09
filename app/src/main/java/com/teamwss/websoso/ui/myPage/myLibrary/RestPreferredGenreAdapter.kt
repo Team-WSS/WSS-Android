02ddab354d32d234c49d22e96b5f481690a42554
yeonjeen <jeendev2019@gmail.com>
2024-07-09 18:37:38 +0900

    refactor: 어댑터 수정

--- app/src/main/java/com/teamwss/websoso/ui/myPage/myLibrary/RestPreferredGenreAdapter.kt
+++ app/src/main/java/com/teamwss/websoso/ui/myPage/myLibrary/RestPreferredGenreAdapter.kt
@@ -1,36 +1,54 @@
 package com.teamwss.websoso.ui.myPage.myLibrary
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
+import android.widget.BaseAdapter
 import com.teamwss.websoso.data.model.PreferredGenreEntity
 import com.teamwss.websoso.databinding.ItemRestPreferredGenreBinding
 
 class RestPreferredGenreAdapter(
-    context: Context, resource: Int, items: List<PreferredGenreEntity>
-) : ArrayAdapter<PreferredGenreEntity>(context, resource, items) {
+    var items: List<PreferredGenreEntity>
+) : BaseAdapter() {
+
+    private var genreCount: String = ""
+
+    fun updateGenreCount(genreCount: String) {
+        this.genreCount = genreCount
+        notifyDataSetChanged()
+    }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val binding: ItemRestPreferredGenreBinding
         val view: View
 
         if (convertView == null) {
-            val layoutInflater = LayoutInflater.from(context)
-            binding = ItemRestPreferredGenreBinding.inflate(layoutInflater, parent, false)
+            binding = ItemRestPreferredGenreBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
             view = binding.root
             view.tag = binding
         } else {
+            binding = convertView.tag as ItemRestPreferredGenreBinding
             view = convertView
-            binding = view.tag as ItemRestPreferredGenreBinding
         }
 
-        val genre: PreferredGenreEntity? = getItem(position)
-        if (genre != null) {
-            binding.restPreferredGenre = genre
-            binding.executePendingBindings()
-        }
+        binding.restPreferredGenre = getItem(position)
         return view
     }
+
+    override fun getCount(): Int {
+        return items.size
+    }
+
+    override fun getItem(position: Int): PreferredGenreEntity {
+        return items[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
 }
+
