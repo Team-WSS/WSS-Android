3b82a2c43dca290a20267ea35bb244ce8d089455
s9hn <tnghqn@naver.com>
2024-07-16 21:59:20 +0900

    feat: 베이스 DialogFragment 구현

---
+++ app/src/main/java/com/teamwss/websoso/ui/common/base/BindingDialogFragment.kt
@@ -0,0 +1,32 @@
+package com.teamwss.websoso.ui.common.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.DialogFragment
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+abstract class BindingDialogFragment<B : ViewDataBinding>(
+    @LayoutRes private val layoutResId: Int,
+) : DialogFragment() {
+    private var _binding: B? = null
+    val binding get() = _binding ?: error("error: binding is null")
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = DataBindingUtil.inflate(inflater, layoutResId, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}
