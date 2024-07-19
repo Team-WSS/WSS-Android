8ea06d76d0542b92acec4f261cee962aaff5fa8a
Son Myeongji <sonmyungj1zz@gmail.com>
2024-07-19 10:55:04 +0900

    refactor: 장르 칩 로직 수정

--- app/src/main/java/com/teamwss/websoso/ui/detailExplore/DetailExploreViewModel.kt
+++ app/src/main/java/com/teamwss/websoso/ui/detailExplore/DetailExploreViewModel.kt
@@ -1,15 +1,18 @@
 package com.teamwss.websoso.ui.detailExplore
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.map
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class DetailExploreViewModel @Inject constructor() : ViewModel() {
-    private val _selectedGenres: MutableLiveData<List<String>?> = MutableLiveData()
-    val selectedGenres: LiveData<List<String>?> get() = _selectedGenres
+    private val _selectedGenres: MutableLiveData<MutableList<String>> =
+        MutableLiveData(mutableListOf())
+    val selectedGenres: LiveData<List<String>> get() = _selectedGenres.map { it.toList() }
 
     private val _selectedSeriesStatus: MutableLiveData<String?> = MutableLiveData()
     val selectedStatus: LiveData<String?> get() = _selectedSeriesStatus
@@ -18,14 +21,13 @@ class DetailExploreViewModel @Inject constructor() : ViewModel() {
     val selectedRating: LiveData<Float?> get() = _selectedRating
 
     fun updateSelectedGenres(genre: String) {
-        val currentGenres = _selectedGenres.value.orEmpty()
-        val updatedGenres = if (currentGenres.contains(genre)) {
-            currentGenres.filter { it != genre }
+        if (selectedGenres.value?.contains(genre) == true) {
+            _selectedGenres.value?.remove(genre)
         } else {
-            currentGenres + genre
+            _selectedGenres.value?.add(genre)
         }
 
-        _selectedGenres.value = updatedGenres
+        Log.d("123123", selectedGenres.value.toString())
     }
 
     fun updateSelectedSeriesStatus(status: String?) {
