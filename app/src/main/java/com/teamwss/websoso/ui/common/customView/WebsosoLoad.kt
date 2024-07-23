a186454cc3c870dfb499f39886d684401567ab79
junseo511 <prime54@g.skku.edu>
2024-07-23 13:47:04 +0900

    feat: 로딩 레이아웃 추가

---
+++ app/src/main/java/com/teamwss/websoso/ui/common/customView/WebsosoLoad.kt
@@ -0,0 +1,25 @@
+package com.teamwss.websoso.ui.common.customView
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.teamwss.websoso.databinding.LayoutLoadBinding
+
+class WebsosoLoad @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+
+    private val binding: LayoutLoadBinding = LayoutLoadBinding.inflate(LayoutInflater.from(context), this, true)
+
+    fun setErrorMessage(title: String, description: String) {
+        binding.tvLoadFailTitle.text = title
+        binding.tvLoadFailDescription.text = description
+    }
+
+    fun setReloadButtonClickListener(listener: OnClickListener) {
+        binding.tvLoadFailReload.setOnClickListener(listener)
+    }
+}
