342326f022db99c04ae5f75627c73991656b15b9
junseo511 <prime54@g.skku.edu>
2024-07-25 09:15:22 +0900

    refactor: 불필요한 의존성 제거

--- app/src/main/java/com/teamwss/websoso/ui/novelDetail/NovelDetailViewModel.kt
+++ app/src/main/java/com/teamwss/websoso/ui/novelDetail/NovelDetailViewModel.kt
@@ -1,6 +1,5 @@
 package com.teamwss.websoso.ui.novelDetail
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
