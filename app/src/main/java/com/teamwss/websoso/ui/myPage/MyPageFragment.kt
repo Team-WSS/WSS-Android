9598cc998cf450d775de2210a0c69dd90783fa91
yeonjeen <jeendev2019@gmail.com>
2024-07-29 06:30:47 +0900

    feat: 스티키헤더 구현

--- app/src/main/java/com/teamwss/websoso/ui/myPage/MyPageFragment.kt
+++ app/src/main/java/com/teamwss/websoso/ui/myPage/MyPageFragment.kt
@@ -2,6 +2,7 @@ package com.teamwss.websoso.ui.myPage
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import com.google.android.material.tabs.TabLayoutMediator
 import com.teamwss.websoso.R
 import com.teamwss.websoso.databinding.FragmentMyPageBinding
@@ -10,14 +11,15 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MyPageFragment : BindingFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
-    private val viewPagerAdapter by lazy {MyPageViewPagerAdapter(requireActivity())}
+    private val viewPagerAdapter by lazy { MyPageViewPagerAdapter(requireActivity()) }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setUpViewPager()
+        setItemVisibilityOnToolBar()
     }
 
-    private fun setUpViewPager(){
+    private fun setUpViewPager() {
         val tabTitleItems =
             listOf(getText(R.string.my_page_library), getText(R.string.my_page_activity))
         binding.vpMyPage.adapter = viewPagerAdapter
@@ -27,5 +29,39 @@ class MyPageFragment : BindingFragment<FragmentMyPageBinding>(R.layout.fragment_
         }.attach()
     }
 
+    private fun setItemVisibilityOnToolBar() {
+        binding.ablMyPage.addOnOffsetChangedListener { appBarLayout, verticalOffset ->
+            val totalScrollRange = appBarLayout.totalScrollRange
+            val percentage = (totalScrollRange.toFloat() + verticalOffset) / totalScrollRange
+            updateToolbarAppearance(percentage <= TOOLBAR_COLLAPSE_THRESHOLD)
+        }
+    }
+
+    private fun updateToolbarAppearance(isCollapsed: Boolean) {
+        with(binding) {
+            val color = if (isCollapsed) R.color.white else R.color.transparent
+            tbMyPage.setBackgroundColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    color
+                )
+            )
+
+            tvMyPageStickyTitle.visibility =
+                if (isCollapsed) View.VISIBLE else View.GONE
+
+            val window = requireActivity().window
+            window.statusBarColor = ContextCompat.getColor(
+                requireContext(),
+                if (isCollapsed) R.color.white else android.R.color.transparent
+            )
+
+            clMyPageUserProfile.visibility = if (isCollapsed) View.INVISIBLE else View.VISIBLE
+        }
+    }
+
+    companion object {
+        const val TOOLBAR_COLLAPSE_THRESHOLD = 0
+    }
 
 }
