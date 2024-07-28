7309fd21d3cc167434571ab6136c1fbd820179a3
yeonjeen <jeendev2019@gmail.com>
2024-07-29 02:13:37 +0900

    feat: 뷰페이저 어댑터 연결

--- app/src/main/java/com/teamwss/websoso/ui/myPage/MyPageFragment.kt
+++ app/src/main/java/com/teamwss/websoso/ui/myPage/MyPageFragment.kt
@@ -1,7 +1,31 @@
 package com.teamwss.websoso.ui.myPage
 
-import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.View
+import com.google.android.material.tabs.TabLayoutMediator
+import com.teamwss.websoso.R
+import com.teamwss.websoso.databinding.FragmentMyPageBinding
+import com.teamwss.websoso.ui.common.base.BindingFragment
+import dagger.hilt.android.AndroidEntryPoint
 
-class MyPageFragment : Fragment() {
+@AndroidEntryPoint
+class MyPageFragment : BindingFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
+    private val viewPagerAdapter by lazy {MyPageViewPagerAdapter(requireActivity())}
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpViewPager()
+    }
+
+    private fun setUpViewPager(){
+        val tabTitleItems =
+            listOf(getText(R.string.my_page_library), getText(R.string.my_page_activity))
+        binding.vpMyPage.adapter = viewPagerAdapter
+
+        TabLayoutMediator(binding.tlMyPage, binding.vpMyPage) { tab, position ->
+            tab.text = tabTitleItems[position]
+        }.attach()
+    }
+
 
 }
