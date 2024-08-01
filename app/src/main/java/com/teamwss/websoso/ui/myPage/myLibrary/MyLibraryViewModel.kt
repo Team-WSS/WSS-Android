7da27ad911dbd3e49124cca1b2461a91f4895c04
yeonjeen <jeendev2019@gmail.com>
2024-08-01 18:54:59 +0900

    refactor: apply() 제거

--- app/src/main/java/com/teamwss/websoso/ui/myPage/myLibrary/MyLibraryViewModel.kt
+++ app/src/main/java/com/teamwss/websoso/ui/myPage/myLibrary/MyLibraryViewModel.kt
@@ -3,15 +3,14 @@ package com.teamwss.websoso.ui.myPage.myLibrary
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.teamwss.websoso.R
 import com.teamwss.websoso.data.model.GenrePreferenceEntity
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class MyLibraryViewModel @Inject constructor() : ViewModel() {
-    private val _genres = MutableLiveData<List<GenrePreferenceEntity>>().apply {
-        value = listOf(
+    private val _genres = MutableLiveData(
+        listOf(
             GenrePreferenceEntity("https://imgur.com/bdP0jm0", "로판", 10),
             GenrePreferenceEntity("https://imgur.com/bdP0jm0", "로맨스", 8),
             GenrePreferenceEntity("https://imgur.com/bdP0jm0", "무협", 6),
@@ -19,7 +18,7 @@ class MyLibraryViewModel @Inject constructor() : ViewModel() {
             GenrePreferenceEntity("https://imgur.com/bdP0jm0", "코믹", 2),
             GenrePreferenceEntity("https://imgur.com/bdP0jm0", "무협", 1)
         )
-    }
+    )
     val genres: LiveData<List<GenrePreferenceEntity>> = _genres
 
     private val _isGenreListVisible = MutableLiveData<Boolean>().apply { value = false }
