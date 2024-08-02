5a3d9975c16300c02004a3f822ada048fb8f07fb
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-08-02 13:30:19 +0900

    build: recyclerview 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,9 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    // RecyclerView
+    implementation ("androidx.recyclerview:recyclerview:1.3.2")
+
     // Retrofit2
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
 
