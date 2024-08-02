73bf07cfe02d8fa42005d0ad6a1e8fe8876dc2a5
yeonjeen <jeendev2019@gmail.com>
2024-08-02 15:24:07 +0900

    refactor: 코드 정렬

--- app/src/main/java/com/teamwss/websoso/util/ViewExtensions.kt
+++ app/src/main/java/com/teamwss/websoso/util/ViewExtensions.kt
@@ -6,6 +6,7 @@ import android.widget.ListView
 fun ListView.setListViewHeightBasedOnChildren() {
     val listAdapter = adapter ?: return
     var totalHeight = 0
+
     for (i in 0 until listAdapter.count) {
         val listItem = listAdapter.getView(i, null, this)
         listItem.measure(
@@ -14,6 +15,7 @@ fun ListView.setListViewHeightBasedOnChildren() {
         )
         totalHeight += listItem.measuredHeight
     }
+
     val params = layoutParams
     params.height = totalHeight + (dividerHeight * (listAdapter.count - 1))
     layoutParams = params
