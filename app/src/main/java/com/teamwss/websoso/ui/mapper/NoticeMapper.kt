d3e1432547a26fe3a99fd8615aa38c4ae515f87e
librarywon <librarywon@naver.com>
2024-08-03 22:40:01 +0900

    feat: notice ui mapper 생성

---
+++ app/src/main/java/com/teamwss/websoso/ui/mapper/NoticeMapper.kt
@@ -0,0 +1,17 @@
+package com.teamwss.websoso.ui.mapper
+
+
+import com.teamwss.websoso.data.model.NoticeEntity
+import com.teamwss.websoso.ui.notice.model.Notice
+
+fun NoticeEntity.toUiModel(): Notice {
+    return Notice(
+        createDate = this.createDate,
+        noticeTitle = this.noticeTitle,
+        noticeContent = this.noticeContent,
+    )
+}
+
+fun List<NoticeEntity>.toUiModel(): List<Notice> {
+    return this.map { it.toUiModel() }
+}
