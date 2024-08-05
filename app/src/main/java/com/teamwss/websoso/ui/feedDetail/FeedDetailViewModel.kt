743b741d9ca47c0ae85ffe59721377375f28c44d
s9hn <tnghqn@naver.com>
2024-08-05 22:39:23 +0900

    feat: 뷰모델 및 리사이클러뷰 주입

---
+++ app/src/main/java/com/teamwss/websoso/ui/feedDetail/FeedDetailViewModel.kt
@@ -0,0 +1,8 @@
+package com.teamwss.websoso.ui.feedDetail
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class FeedDetailViewModel : ViewModel() {
+}
