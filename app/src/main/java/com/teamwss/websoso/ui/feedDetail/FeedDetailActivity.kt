33c1a910918d4a132d2c1119a667c30d24b25c5a
s9hn <tnghqn@naver.com>
2024-08-05 17:54:59 +0900

    feat: 피드 상세보기 뷰 xml 구현

--- app/src/main/java/com/teamwss/websoso/ui/feedDetail/FeedDetailActivity.kt
+++ app/src/main/java/com/teamwss/websoso/ui/feedDetail/FeedDetailActivity.kt
@@ -2,24 +2,21 @@ package com.teamwss.websoso.ui.feedDetail
 
 import android.content.Context
 import android.content.Intent
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.teamwss.websoso.R
+import com.teamwss.websoso.databinding.ActivityFeedDetailBinding
+import com.teamwss.websoso.ui.common.base.BindingActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class FeedDetailActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_feed_detail)
-    }
+class FeedDetailActivity :
+    BindingActivity<ActivityFeedDetailBinding>(R.layout.activity_feed_detail) {
 
     companion object {
         private const val FEED_ID = "FEED_ID"
 
         fun from(
             id: Long,
-            context: Context
+            context: Context,
         ): Intent = Intent(context, FeedDetailActivity::class.java).apply {
             putExtra(FEED_ID, id)
         }
