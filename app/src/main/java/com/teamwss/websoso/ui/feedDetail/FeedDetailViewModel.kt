aa420cb0ceb1fa3343c88ae4d25618d5e22413a3
s9hn <tnghqn@naver.com>
2024-08-07 02:45:46 +0900

    feat: feedDetail UiState 구현 및 update 함수 구현

--- app/src/main/java/com/teamwss/websoso/ui/feedDetail/FeedDetailViewModel.kt
+++ app/src/main/java/com/teamwss/websoso/ui/feedDetail/FeedDetailViewModel.kt
@@ -1,8 +1,45 @@
 package com.teamwss.websoso.ui.feedDetail
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.teamwss.websoso.data.repository.FeedRepository
+import com.teamwss.websoso.ui.feedDetail.model.FeedDetailModel
+import com.teamwss.websoso.ui.feedDetail.model.FeedDetailUiState
+import com.teamwss.websoso.ui.feedDetail.model.FeedDetailUiState.Error
+import com.teamwss.websoso.ui.feedDetail.model.FeedDetailUiState.Loading
+import com.teamwss.websoso.ui.feedDetail.model.FeedDetailUiState.Success
+import com.teamwss.websoso.ui.mapper.toUi
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
-class FeedDetailViewModel : ViewModel() {
+class FeedDetailViewModel @Inject constructor(
+    private val feedRepository: FeedRepository,
+) : ViewModel() {
+    private val _feedDetailUiState: MutableLiveData<FeedDetailUiState> = MutableLiveData(Loading)
+    val feedDetailUiState: LiveData<FeedDetailUiState> get() = _feedDetailUiState
+
+    fun updateFeedDetail(feedId: Long) {
+        viewModelScope.launch {
+            runCatching {
+                val feed = async { feedRepository.fetchFeed(feedId) }
+                val comments = async { feedRepository.fetchComments(feedId) }
+
+                Success(
+                    FeedDetailModel(
+                        feed = feed.await().toUi(),
+                        comments = comments.await().comments.map { it.toUi() },
+                    )
+                )
+            }.onSuccess {
+                _feedDetailUiState.value = it
+            }.onFailure {
+                _feedDetailUiState.value = Error
+            }
+        }
+    }
 }
--- app/src/main/java/com/teamwss/websoso/ui/mapper/FeedMapper.kt
+++ app/src/main/java/com/teamwss/websoso/ui/mapper/FeedMapper.kt
@@ -1,10 +1,15 @@
 package com.teamwss.websoso.ui.mapper
 
+import com.teamwss.websoso.data.model.CommentEntity
+import com.teamwss.websoso.data.model.FeedEntity
 import com.teamwss.websoso.domain.model.Feed
 import com.teamwss.websoso.ui.feed.model.FeedModel
+import com.teamwss.websoso.ui.feed.model.FeedModel.NovelModel
+import com.teamwss.websoso.ui.feed.model.FeedModel.UserModel
+import com.teamwss.websoso.ui.feedDetail.model.CommentModel
 
 fun Feed.toUi(): FeedModel = FeedModel(
-    user = FeedModel.UserModel(
+    user = UserModel(
         id = user.id,
         nickname = user.nickname,
         avatarImage = user.avatarImage,
@@ -19,10 +24,47 @@ fun Feed.toUi(): FeedModel = FeedModel(
     isSpoiler = isSpoiler,
     isLiked = isLiked,
     isMyFeed = isMyFeed,
-    novel = FeedModel.NovelModel(
+    novel = NovelModel(
         id = novel.id,
         title = novel.title,
         rating = novel.rating,
         ratingCount = novel.ratingCount,
     ),
+)
+
+fun FeedEntity.toUi(): FeedModel = FeedModel(
+    user = UserModel(
+        id = user.id,
+        nickname = user.nickname,
+        avatarImage = user.avatarImage,
+    ),
+    createdDate = createdDate,
+    id = id,
+    content = content,
+    relevantCategories = relevantCategories,
+    likeCount = likeCount,
+    commentCount = commentCount,
+    isModified = isModified,
+    isSpoiler = isSpoiler,
+    isLiked = isLiked,
+    isMyFeed = isMyFeed,
+    novel = NovelModel(
+        id = novel.id,
+        title = novel.title,
+        rating = novel.rating,
+        ratingCount = novel.ratingCount,
+    ),
+)
+
+fun CommentEntity.toUi(): CommentModel = CommentModel(
+    user = UserModel(
+        id = user.id,
+        nickname = user.nickname,
+        avatarImage = user.avatarImage,
+    ),
+    commentContent = commentContent,
+    commentId = commentId,
+    createdDate = createdDate,
+    isModified = isModified,
+    isMyComment = isMyComment,
 )
