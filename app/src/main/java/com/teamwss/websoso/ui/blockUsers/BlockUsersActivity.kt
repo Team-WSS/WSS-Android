4b0f96ce5609c2c7a06eb5970bd73bf57ffd1eaf
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-08-09 01:34:12 +0900

    feat: 액티비티 추가

---
+++ app/src/main/java/com/teamwss/websoso/ui/blockUsers/BlockUsersActivity.kt
@@ -0,0 +1,14 @@
+package com.teamwss.websoso.ui.blockUsers
+
+import android.os.Bundle
+import com.teamwss.websoso.R
+import com.teamwss.websoso.databinding.ActivityBlockUsersBinding
+import com.teamwss.websoso.ui.common.base.BindingActivity
+
+class BlockUsersActivity :
+    BindingActivity<ActivityBlockUsersBinding>(R.layout.activity_block_users) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
