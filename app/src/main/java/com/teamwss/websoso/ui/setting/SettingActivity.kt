34a8457e0420f8c6c8faa89e112b8168e7dc1937
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-08-08 22:47:09 +0900

    feat: 액티비티 생성

---
+++ app/src/main/java/com/teamwss/websoso/ui/setting/SettingActivity.kt
@@ -0,0 +1,13 @@
+package com.teamwss.websoso.ui.setting
+
+import android.os.Bundle
+import com.teamwss.websoso.R
+import com.teamwss.websoso.databinding.ActivitySettingBinding
+import com.teamwss.websoso.ui.common.base.BindingActivity
+
+class SettingActivity : BindingActivity<ActivitySettingBinding>(R.layout.activity_setting) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
