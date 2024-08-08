1d1398ec3bf58b0264053d8043e4a31341cacea1
yeonjeen <jeendev2019@gmail.com>
2024-08-08 16:53:07 +0900

    feat: 레포지터리 분리 및 매력포인트 텍스트 변환 구현

--- app/src/main/java/com/teamwss/websoso/ui/myPage/myLibrary/MyLibraryViewModel.kt
+++ app/src/main/java/com/teamwss/websoso/ui/myPage/myLibrary/MyLibraryViewModel.kt
@@ -3,31 +3,60 @@ package com.teamwss.websoso.ui.myPage.myLibrary
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.teamwss.websoso.data.model.AttractivePointEntity
 import com.teamwss.websoso.data.model.GenrePreferenceEntity
+import com.teamwss.websoso.data.repository.MyLibraryRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class MyLibraryViewModel @Inject constructor() : ViewModel() {
-    private val _genres = MutableLiveData(
-        listOf(
-            GenrePreferenceEntity("https://cdn-icons-png.flaticon.com/512/660/660026.png", "로판", 10),
-            GenrePreferenceEntity("https://cdn-icons-png.flaticon.com/512/660/660026.png", "로맨스", 8),
-            GenrePreferenceEntity("https://cdn-icons-png.flaticon.com/512/660/660026.png", "무협", 6),
-            GenrePreferenceEntity("https://cdn-icons-png.flaticon.com/512/660/660026.png", "판타지", 4),
-            GenrePreferenceEntity("https://cdn-icons-png.flaticon.com/512/660/660026.png", "코믹", 2),
-            GenrePreferenceEntity("https://cdn-icons-png.flaticon.com/512/660/660026.png", "무협", 1),
-            GenrePreferenceEntity("https://cdn-icons-png.flaticon.com/512/660/660026.png", "로판", 10),
-            GenrePreferenceEntity("https://cdn-icons-png.flaticon.com/512/660/660026.png", "로판", 10),
-            GenrePreferenceEntity("https://cdn-icons-png.flaticon.com/512/660/660026.png", "로판", 10)
-        )
-    )
+class MyLibraryViewModel @Inject constructor(private val myLibraryRepository: MyLibraryRepository) :
+    ViewModel() {
+    private val _genres = MutableLiveData<List<GenrePreferenceEntity>>()
     val genres: LiveData<List<GenrePreferenceEntity>> = _genres
 
     private val _isGenreListVisible = MutableLiveData<Boolean>().apply { value = false }
     val isGenreListVisible: LiveData<Boolean> = _isGenreListVisible
 
+    private val _attractivePoints = MutableLiveData<List<AttractivePointEntity>>()
+    val attractivePoints: LiveData<List<AttractivePointEntity>> = _attractivePoints
+
+    private val _attractivePointsText = MutableLiveData<String>()
+    val attractivePointsText: LiveData<String> get() = _attractivePointsText
+
+    init {
+        loadPrefrenceData()
+        setDummyData()
+    }
+
+    private fun loadPrefrenceData() {
+        _genres.value = myLibraryRepository.getGenres()
+        _attractivePoints.value = myLibraryRepository.getAttractivePoints()
+    }
+
+    private fun translateAttractivePointsText(text: String): String {
+        return text.split(", ").joinToString(", ") {
+            when (it) {
+                "character" -> "캐릭터"
+                "relationship" -> "관계"
+                "material" -> "소재"
+                else -> it
+            }
+        }
+    }
+
     fun updateToggleGenresVisibility() {
         _isGenreListVisible.value = _isGenreListVisible.value?.not() ?: false
+    }
+
+    fun setText(serverText: String, fixedText: String) {
+        val translatedText = translateAttractivePointsText(serverText)
+        _attractivePointsText.value = "$translatedText$fixedText"
+    }
+
+    private fun setDummyData() {
+        val serverText = "character, material"
+        val fixedText = "가 매력적인 작품"
+        setText(serverText, fixedText)
     }
 }
