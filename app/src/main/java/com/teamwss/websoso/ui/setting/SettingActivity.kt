38e3fba79d9e1899043726ed41fd67dbe472c0f9
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-08-08 22:47:22 +0900

    feat: 상태바 투명하게 설정

--- app/src/main/java/com/teamwss/websoso/ui/setting/SettingActivity.kt
+++ app/src/main/java/com/teamwss/websoso/ui/setting/SettingActivity.kt
@@ -1,6 +1,7 @@
 package com.teamwss.websoso.ui.setting
 
 import android.os.Bundle
+import android.view.WindowManager
 import com.teamwss.websoso.R
 import com.teamwss.websoso.databinding.ActivitySettingBinding
 import com.teamwss.websoso.ui.common.base.BindingActivity
@@ -9,5 +10,14 @@ class SettingActivity : BindingActivity<ActivitySettingBinding>(R.layout.activit
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setupTranslucentOnStatusBar()
+    }
+
+    private fun setupTranslucentOnStatusBar() {
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+        )
     }
 }
