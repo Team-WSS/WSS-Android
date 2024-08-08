2127545e7a6cd972758dfe6b595ee59e6b756143
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-08-09 00:48:10 +0900

    feat: 계정정보 액티비티 manifest 추가

---
+++ app/src/main/java/com/teamwss/websoso/ui/setting/accountInfo/AccountInfoActivity.kt
@@ -0,0 +1,14 @@
+package com.teamwss.websoso.ui.setting.accountInfo
+
+import android.os.Bundle
+import com.teamwss.websoso.R
+import com.teamwss.websoso.databinding.ActivityAccountInfoBinding
+import com.teamwss.websoso.ui.common.base.BindingActivity
+
+class AccountInfoActivity :
+    BindingActivity<ActivityAccountInfoBinding>(R.layout.activity_account_info) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
