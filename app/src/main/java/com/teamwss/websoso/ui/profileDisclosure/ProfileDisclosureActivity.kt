e8018801043d15f333421bec6a9a2b77871c0ada
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-08-09 02:01:56 +0900

    feat: 액티비티 생성

---
+++ app/src/main/java/com/teamwss/websoso/ui/profileDisclosure/ProfileDisclosureActivity.kt
@@ -0,0 +1,14 @@
+package com.teamwss.websoso.ui.profileDisclosure
+
+import android.os.Bundle
+import com.teamwss.websoso.R
+import com.teamwss.websoso.databinding.ActivityProfileDisclosureBinding
+import com.teamwss.websoso.ui.common.base.BindingActivity
+
+class ProfileDisclosureActivity :
+    BindingActivity<ActivityProfileDisclosureBinding>(R.layout.activity_profile_disclosure) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
