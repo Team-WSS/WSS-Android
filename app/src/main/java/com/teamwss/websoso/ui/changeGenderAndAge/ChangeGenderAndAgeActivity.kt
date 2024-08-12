671a092faea426c9f2166af2eb3c53f63bd73895
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-08-12 22:13:33 +0900

    feat: 액티비티 생성

---
+++ app/src/main/java/com/teamwss/websoso/ui/changeGenderAndAge/ChangeGenderAndAgeActivity.kt
@@ -0,0 +1,14 @@
+package com.teamwss.websoso.ui.changeGenderAndAge
+
+import android.os.Bundle
+import com.teamwss.websoso.R
+import com.teamwss.websoso.databinding.ActivityChangeGenderAndAgeBinding
+import com.teamwss.websoso.ui.common.base.BindingActivity
+
+class ChangeGenderAndAgeActivity :
+    BindingActivity<ActivityChangeGenderAndAgeBinding>(R.layout.activity_change_gender_and_age) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
