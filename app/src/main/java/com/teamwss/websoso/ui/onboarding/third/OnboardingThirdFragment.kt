dc7fd78cbb8dc2a08883f32a3c32ac98844abd8b
librarywon <librarywon@naver.com>
2024-08-12 23:00:54 +0900

    feat: 장르 선택 로직 적용

--- app/src/main/java/com/teamwss/websoso/ui/onboarding/third/OnboardingThirdFragment.kt
+++ app/src/main/java/com/teamwss/websoso/ui/onboarding/third/OnboardingThirdFragment.kt
@@ -2,17 +2,42 @@ package com.teamwss.websoso.ui.onboarding.third
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.activityViewModels
 import com.teamwss.websoso.R
 import com.teamwss.websoso.databinding.FragmentOnboardingThirdBinding
 import com.teamwss.websoso.ui.common.base.BindingFragment
+import com.teamwss.websoso.ui.onboarding.OnboardingViewModel
+import com.teamwss.websoso.ui.onboarding.third.adapter.GenreAdapter
+import com.teamwss.websoso.ui.onboarding.third.model.Genre
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class OnboardingThirdFragment :
     BindingFragment<FragmentOnboardingThirdBinding>(R.layout.fragment_onboarding_third) {
+    private val onboardingViewModel: OnboardingViewModel by activityViewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupGenreAdapter()
+        setupObserver()
 
+        binding.viewModel = onboardingViewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+    }
+
+    private fun setupGenreAdapter() {
+        val genres = Genre.values().toList()
+        val adapter = GenreAdapter(
+            genres,
+            onboardingViewModel::toggleGenreSelection,
+            onboardingViewModel::isSelected
+        )
+        binding.rvOnboardingThird.adapter = adapter
+    }
+
+    private fun setupObserver() {
+        onboardingViewModel.selectedGenres.observe(viewLifecycleOwner) {
+            binding.rvOnboardingThird.adapter?.notifyDataSetChanged()
+        }
     }
 }
