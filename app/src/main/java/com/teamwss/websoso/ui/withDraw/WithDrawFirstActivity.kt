f1cc7d017631605ea114803e46548954cc238760
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-08-13 23:19:47 +0900

    feat: 액티비티 생성

---
+++ app/src/main/java/com/teamwss/websoso/ui/withDraw/WithDrawFirstActivity.kt
@@ -0,0 +1,14 @@
+package com.teamwss.websoso.ui.withDraw
+
+import android.os.Bundle
+import com.teamwss.websoso.R
+import com.teamwss.websoso.databinding.ActivityWithDrawFirstBinding
+import com.teamwss.websoso.ui.common.base.BindingActivity
+
+class WithDrawFirstActivity :
+    BindingActivity<ActivityWithDrawFirstBinding>(R.layout.activity_with_draw_first) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/teamwss/websoso/ui/withDraw/WithDrawSecondActivity.kt
@@ -0,0 +1,14 @@
+package com.teamwss.websoso.ui.withDraw
+
+import android.os.Bundle
+import com.teamwss.websoso.R
+import com.teamwss.websoso.databinding.ActivityWithDrawSecondBinding
+import com.teamwss.websoso.ui.common.base.BindingActivity
+
+class WithDrawSecondActivity :
+    BindingActivity<ActivityWithDrawSecondBinding>(R.layout.activity_with_draw_second) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
