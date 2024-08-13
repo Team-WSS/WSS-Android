9a480d1edb4fc643fb9489dc818fa74517f5428c
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-08-13 23:20:37 +0900

    feat: 상태바 투명하게 설정

--- app/src/main/java/com/teamwss/websoso/ui/withDraw/WithDrawFirstActivity.kt
+++ app/src/main/java/com/teamwss/websoso/ui/withDraw/WithDrawFirstActivity.kt
@@ -1,6 +1,7 @@
 package com.teamwss.websoso.ui.withDraw
 
 import android.os.Bundle
+import android.view.WindowManager
 import com.teamwss.websoso.R
 import com.teamwss.websoso.databinding.ActivityWithDrawFirstBinding
 import com.teamwss.websoso.ui.common.base.BindingActivity
@@ -10,5 +11,14 @@ class WithDrawFirstActivity :
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setupTranslucentOnStatusBar()
+    }
+
+    private fun setupTranslucentOnStatusBar() {
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+        )
     }
 }
