0a61864e77bbc4f006daaf87bf9bf84ad3e37de2
yeonjeen <jeendev2019@gmail.com>
2024-08-15 01:27:04 +0900

    feat: 내 활동 Entity

---
+++ app/src/main/java/com/teamwss/websoso/data/model/MyActivitiesEntity.kt
@@ -0,0 +1,25 @@
+package com.teamwss.websoso.data.model
+
+data class MyActivitiesEntity(
+    val isLoadable: Boolean,
+    val feeds: List<MyActivityEntity>
+){
+    data class MyActivityEntity(
+        val feedId: Int,
+        val userId: Int,
+        val profileImg: String,
+        val nickname: String,
+        val isSpoiler: Boolean,
+        val feedContent: String,
+        val createdDate: String,
+        val isModified: Boolean,
+        val isLiked: Boolean,
+        val likeCount: Int,
+        val commentCount: Int,
+        val novelId: Int?,
+        val title: String?,
+        val novelRatingCount: Int?,
+        val novelRating: Double?,
+        val relevantCategories: List<String>,
+    )
+}
