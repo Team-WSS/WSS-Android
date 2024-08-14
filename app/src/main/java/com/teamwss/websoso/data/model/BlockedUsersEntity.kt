59a9d8e56c62bd2308e5331018073af869adfb32
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-08-15 00:37:50 +0900

    build: 코드 포맷팅

--- app/src/main/java/com/teamwss/websoso/data/model/BlockedUsersEntity.kt
+++ app/src/main/java/com/teamwss/websoso/data/model/BlockedUsersEntity.kt
@@ -3,7 +3,6 @@ package com.teamwss.websoso.data.model
 data class BlockedUsersEntity(
     val blockedUsers: List<BlockedUserEntity>
 ) {
-
     data class BlockedUserEntity(
         val blockId: Long,
         val userId: Long,
