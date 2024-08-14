0793b403d17c66df659eba438a548b1dbab49b32
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-08-14 23:48:57 +0900

    feat: api 구현

--- app/src/main/java/com/teamwss/websoso/data/remote/api/UserApi.kt
+++ app/src/main/java/com/teamwss/websoso/data/remote/api/UserApi.kt
@@ -1,10 +1,14 @@
 package com.teamwss.websoso.data.remote.api
 
 import com.teamwss.websoso.data.remote.response.UserEmailResponseDto
+import com.teamwss.websoso.data.remote.response.UserNovelStatsResponseDto
 import retrofit2.http.GET
 
 interface UserApi {
 
     @GET("users/email")
     suspend fun getUserEmail(): UserEmailResponseDto
+
+    @GET("users/user-novel-stats")
+    suspend fun getUserNovelStats(): UserNovelStatsResponseDto
 }
