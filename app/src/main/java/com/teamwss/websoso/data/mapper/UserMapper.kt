b6779dd75b6fb42cf4e4b0613bec8c4ae75e2ac7
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-08-14 13:35:34 +0900

    feat: dto 및 entity 생성 후 매퍼 구현

---
+++ app/src/main/java/com/teamwss/websoso/data/mapper/UserMapper.kt
@@ -0,0 +1,10 @@
+package com.teamwss.websoso.data.mapper
+
+import com.teamwss.websoso.data.model.UserEmailEntity
+import com.teamwss.websoso.data.remote.response.UserEmailResponseDto
+
+fun UserEmailResponseDto.toData(): UserEmailEntity {
+    return UserEmailEntity(
+        email = email
+    )
+}
