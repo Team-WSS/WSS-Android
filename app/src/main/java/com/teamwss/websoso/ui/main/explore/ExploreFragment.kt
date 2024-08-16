ee663b83ee37336cd23a4a1b1d235e9dd53a6455
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-08-16 16:10:12 +0900

    feat: 소소픽 작품 클릭 시 작품 상세보기로 이동

--- app/src/main/java/com/teamwss/websoso/ui/main/explore/ExploreFragment.kt
+++ app/src/main/java/com/teamwss/websoso/ui/main/explore/ExploreFragment.kt
@@ -9,6 +9,7 @@ import com.teamwss.websoso.ui.common.base.BindingFragment
 import com.teamwss.websoso.ui.detailExplore.DetailExploreDialogBottomSheet
 import com.teamwss.websoso.ui.main.explore.adapter.SosoPickAdapter
 import com.teamwss.websoso.ui.normalExplore.NormalExploreActivity
+import com.teamwss.websoso.ui.novelDetail.NovelDetailActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -31,7 +32,8 @@ class ExploreFragment : BindingFragment<FragmentExploreBinding>(R.layout.fragmen
     }
 
     private fun navigateToNovelDetail(novelId: Long) {
-        // TODO 작품 정보 뷰로 이동
+        val intent = NovelDetailActivity.getIntent(requireContext(), novelId)
+        startActivity(intent)
     }
 
     private fun onNormalSearchButtonClick() {
