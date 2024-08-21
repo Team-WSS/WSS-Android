6346586ad2ddaf1583f9a2b7862487e21ac290b7
junseo511 <prime54@g.skku.edu>
2024-08-21 16:40:55 +0900

    refactor: 함수명 및 기능 수정

--- app/src/main/java/com/teamwss/websoso/data/di/DataStoreModule.kt
+++ app/src/main/java/com/teamwss/websoso/data/di/DataStoreModule.kt
@@ -3,7 +3,6 @@ package com.teamwss.websoso.data.di
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
-import androidx.datastore.preferences.SharedPreferencesMigration
 import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.emptyPreferences
@@ -24,12 +23,11 @@ object DataStoreModule {
 
     @Singleton
     @Provides
-    fun providePreferencesDataStore(@ApplicationContext context: Context): DataStore<Preferences> {
+    fun provideUserPreferencesDataStore(@ApplicationContext context: Context): DataStore<Preferences> {
         return PreferenceDataStoreFactory.create(
             corruptionHandler = ReplaceFileCorruptionHandler(
                 produceNewData = { emptyPreferences() }
             ),
-            migrations = listOf(SharedPreferencesMigration(context, USER_PREFERENCES)),
             scope = CoroutineScope(Dispatchers.IO + SupervisorJob()),
             produceFile = { context.preferencesDataStoreFile(USER_PREFERENCES) }
         )
