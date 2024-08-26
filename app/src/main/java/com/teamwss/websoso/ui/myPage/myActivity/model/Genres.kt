bfb96650f77b054c22ac989fb1026486fc9e4211
yeonjeen <jeendev2019@gmail.com>
2024-08-27 03:12:16 +0900

    fix: 장르 네이밍 수정

--- app/src/main/java/com/teamwss/websoso/ui/myPage/myActivity/model/Genres.kt
+++ app/src/main/java/com/teamwss/websoso/ui/myPage/myActivity/model/Genres.kt
@@ -3,14 +3,14 @@ package com.teamwss.websoso.ui.myPage.myActivity.model
 enum class Genres(val korean: String) {
     ALL("전체"),
     ROMANCE("로맨스"),
-    ROMANCE_FANTASY("로판"),
-    BOYS_LOVE("BL"),
+    ROMANCEFANTASY("로판"),
+    BOYSLOVE("BL"),
     FANTASY("판타지"),
     MODERN_FANTASY("현판"),
     WUXIA("무협"),
     DRAMA("드라마"),
     MYSTERY("미스터리"),
-    LIGHT_NOVEL("라노벨"),
+    LIGHTNOVEL("라노벨"),
     ETC("기타");
 
     companion object {
