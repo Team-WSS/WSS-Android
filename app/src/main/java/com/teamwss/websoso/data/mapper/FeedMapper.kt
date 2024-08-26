9e78dac89226cde11f22bb349be45b68cdab1485
librarywon <librarywon@naver.com>
2024-08-26 17:29:33 +0900

    feat: PopularFeeds data 로직 구현

--- app/src/main/java/com/teamwss/websoso/data/mapper/FeedMapper.kt
+++ app/src/main/java/com/teamwss/websoso/data/mapper/FeedMapper.kt
@@ -6,10 +6,12 @@ import com.teamwss.websoso.data.model.FeedEntity
 import com.teamwss.websoso.data.model.FeedEntity.NovelEntity
 import com.teamwss.websoso.data.model.FeedEntity.UserEntity
 import com.teamwss.websoso.data.model.FeedsEntity
+import com.teamwss.websoso.data.model.PopularFeedsEntity
 import com.teamwss.websoso.data.remote.response.CommentResponseDto
 import com.teamwss.websoso.data.remote.response.CommentsResponseDto
 import com.teamwss.websoso.data.remote.response.FeedResponseDto
 import com.teamwss.websoso.data.remote.response.FeedsResponseDto
+import com.teamwss.websoso.data.remote.response.PopularFeedsResponseDto
 
 fun FeedsResponseDto.toData(): FeedsEntity = FeedsEntity(
     category = category,
@@ -58,3 +60,17 @@ fun CommentResponseDto.toData(): CommentEntity = CommentEntity(
     isModified = isModified,
     isMyComment = isMyComment,
 )
+
+fun PopularFeedsResponseDto.toData(): PopularFeedsEntity {
+    return PopularFeedsEntity(
+        popularFeeds = popularFeeds.map { feed ->
+            PopularFeedsEntity.PopularFeedEntity(
+                feedId = feed.feedId,
+                feesContent = feed.feedContent,
+                likeCount = feed.likeCount,
+                commentCount = feed.commentCount,
+                isSpoiler = feed.isSpoiler
+            )
+        }
+    )
+}
