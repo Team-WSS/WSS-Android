62af0fe13c3ab0c2a99f684e981b5af58c746318
librarywon <librarywon@naver.com>
2024-08-26 16:23:45 +0900

    refactor: 잘못된 string 수정

--- app/src/main/java/com/teamwss/websoso/ui/main/home/HomeViewModel.kt
+++ app/src/main/java/com/teamwss/websoso/ui/main/home/HomeViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.teamwss.websoso.data.repository.FeedRepository
 import com.teamwss.websoso.data.repository.NovelRepository
 import com.teamwss.websoso.ui.main.home.model.HomeUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -13,6 +14,7 @@ import javax.inject.Inject
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     private val novelRepository: NovelRepository,
+    private val feedRepository: FeedRepository,
 ) : ViewModel() {
 
     private val _uiState: MutableLiveData<HomeUiState> = MutableLiveData(HomeUiState())
@@ -20,6 +22,7 @@ class HomeViewModel @Inject constructor(
 
     init {
         updatePopularNovels()
+        updatePopularFeeds()
     }
 
     private fun updatePopularNovels() {
@@ -27,12 +30,30 @@ class HomeViewModel @Inject constructor(
             runCatching {
                 novelRepository.fetchPopularNovels()
             }.onSuccess { popularNovels ->
-                _uiState.value = uiState.value?.copy(
+                _uiState.value = _uiState.value?.copy(
                     loading = false,
                     popularNovels = popularNovels.popularNovels
                 )
             }.onFailure {
-                _uiState.value = uiState.value?.copy(
+                _uiState.value = _uiState.value?.copy(
+                    loading = false,
+                    error = true,
+                )
+            }
+        }
+    }
+
+    private fun updatePopularFeeds() {
+        viewModelScope.launch {
+            runCatching {
+                feedRepository.fetchPopularFeeds()
+            }.onSuccess { popularFeeds ->
+                _uiState.value = _uiState.value?.copy(
+                    loading = false,
+                    popularFeeds = popularFeeds.popularFeeds.chunked(3)
+                )
+            }.onFailure {
+                _uiState.value = _uiState.value?.copy(
                     loading = false,
                     error = true,
                 )
@@ -40,3 +61,4 @@ class HomeViewModel @Inject constructor(
         }
     }
 }
+
