6c8deabcb6573cc02e46a1decafb27d34a70c0a1
yeonjeen <jeendev2019@gmail.com>
2024-08-27 07:41:11 +0900

    feat: 내 활동에서 내 활동디테일 이동 구현

--- app/src/main/java/com/teamwss/websoso/ui/myPage/myActivity/MyActivityFragment.kt
+++ app/src/main/java/com/teamwss/websoso/ui/myPage/myActivity/MyActivityFragment.kt
@@ -1,11 +1,13 @@
 package com.teamwss.websoso.ui.myPage.myActivity
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
 import com.teamwss.websoso.R
 import com.teamwss.websoso.common.ui.base.BaseFragment
 import com.teamwss.websoso.databinding.FragmentMyActivityBinding
+import com.teamwss.websoso.ui.myActivityDetail.MyActivityDetailActivity
 import com.teamwss.websoso.ui.myPage.myActivity.adapter.MyActivityAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -22,6 +24,7 @@ class MyActivityFragment :
 
         setupMyActivitiesAdapter()
         setUpObserve()
+        setupMyActivityMoreButton()
     }
 
     private fun setupMyActivitiesAdapter() {
@@ -33,6 +36,13 @@ class MyActivityFragment :
     private fun setUpObserve() {
         myActivityViewModel.myActivity.observe(viewLifecycleOwner) { activities ->
             myActivityAdapter.submitList(activities)
+        }
+    }
+
+    private fun setupMyActivityMoreButton() {
+        binding.btnMyActivityMore.setOnClickListener {
+            val intent = Intent(requireContext(), MyActivityDetailActivity::class.java)
+            startActivity(intent)
         }
     }
 }
