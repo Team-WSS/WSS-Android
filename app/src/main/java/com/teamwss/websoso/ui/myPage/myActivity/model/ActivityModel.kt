dca1cf80f754aa6c82545fe2c66044b5f66f881e
yeonjeen <jeendev2019@gmail.com>
2024-08-27 05:41:14 +0900

    feat: 내 활동 데이터 클래스 생성

---
+++ app/src/main/java/com/teamwss/websoso/ui/myPage/myActivity/model/ActivityModel.kt
@@ -0,0 +1,20 @@
+package com.teamwss.websoso.ui.myPage.myActivity.model
+
+data class ActivityModel(
+    val feedId: Int,
+    val userId: Int,
+    val profileImg: String,
+    val nickname: String,
+    val isSpoiler: Boolean,
+    val feedContent: String,
+    val createdDate: String,
+    val isModified: Boolean,
+    val isLiked: Boolean,
+    val likeCount: Int,
+    val commentCount: Int,
+    val novelId: Int,
+    val title: String,
+    val novelRatingCount: Int?,
+    val novelRating: Float?,
+    val relevantCategories:String,
+)
