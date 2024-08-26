d6d6f33176f45f31d89f786c6dd1e4ca6cf1f852
librarywon <librarywon@naver.com>
2024-08-26 17:32:49 +0900

    feat: PopularFeeds adapter, viewHolder 구현

---
+++ app/src/main/java/com/teamwss/websoso/ui/main/home/adpater/PopularFeedsAdapter.kt
@@ -0,0 +1,41 @@
+package com.teamwss.websoso.ui.main.home.adpater
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import com.teamwss.websoso.data.model.PopularFeedsEntity.PopularFeedEntity
+import com.teamwss.websoso.databinding.ItemPopularFeedBinding
+
+class PopularFeedsAdapter(
+    private val navigateToNovelDetail: (Long) -> Unit,
+) : ListAdapter<List<PopularFeedEntity>, PopularFeedsViewHolder>(diffUtil) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PopularFeedsViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemPopularFeedBinding.inflate(inflater, parent, false)
+        return PopularFeedsViewHolder(binding, navigateToNovelDetail)
+    }
+
+    override fun onBindViewHolder(holder: PopularFeedsViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<List<PopularFeedEntity>>() {
+            override fun areItemsTheSame(
+                oldItem: List<PopularFeedEntity>,
+                newItem: List<PopularFeedEntity>,
+            ): Boolean {
+                return oldItem.firstOrNull()?.feedId == newItem.firstOrNull()?.feedId
+            }
+
+            override fun areContentsTheSame(
+                oldItem: List<PopularFeedEntity>,
+                newItem: List<PopularFeedEntity>,
+            ): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/teamwss/websoso/ui/main/home/adpater/PopularFeedsViewHolder.kt
@@ -0,0 +1,32 @@
+package com.teamwss.websoso.ui.main.home.adpater
+
+import androidx.recyclerview.widget.RecyclerView
+import com.teamwss.websoso.data.model.PopularFeedsEntity.PopularFeedEntity
+import com.teamwss.websoso.databinding.ItemPopularFeedBinding
+
+class PopularFeedsViewHolder(
+    private val binding: ItemPopularFeedBinding,
+    private val navigateToNovelDetail: (Long) -> Unit,
+) : RecyclerView.ViewHolder(binding.root) {
+
+    private val slots by lazy {
+        listOf(
+            binding.itemPopularFeesSlot1,
+            binding.itemPopularFeesSlot2,
+            binding.itemPopularFeesSlot3
+        )
+    }
+
+    fun bind(feedItems: List<PopularFeedEntity>) {
+        slots.forEachIndexed { index, slotBinding ->
+            slotBinding?.apply {
+                feedItems.getOrNull(index)?.let { feed ->
+                    tvPopularFeedContent.text = feed.feesContent
+                    tvPopularFeedLikeCount.text = feed.likeCount.toString()
+                    tvPopularFeedCommentCount.text = feed.commentCount.toString()
+                    root.setOnClickListener { navigateToNovelDetail(feed.feedId) }
+                }
+            }
+        }
+    }
+}
