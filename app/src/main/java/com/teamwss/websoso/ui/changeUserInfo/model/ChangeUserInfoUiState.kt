4170bb7bbe8725a6bd96683950d7ef92be8a280b
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-08-26 21:06:40 +0900

    build: 성별/나이 기본값 수정

--- app/src/main/java/com/teamwss/websoso/ui/changeUserInfo/model/ChangeUserInfoUiState.kt
+++ app/src/main/java/com/teamwss/websoso/ui/changeUserInfo/model/ChangeUserInfoUiState.kt
@@ -1,10 +1,12 @@
 package com.teamwss.websoso.ui.changeUserInfo.model
 
+import com.teamwss.websoso.ui.changeUserInfo.model.Gender.FEMALE
+
 data class ChangeUserInfoUiState(
     val loading: Boolean = true,
     val error: Boolean = false,
-    val gender: Gender? = null,
-    val birthYear: Int? = null,
+    val gender: Gender = FEMALE,
+    val birthYear: Int = 2000,
     val isMaleButtonSelected: Boolean = false,
     val isFemaleButtonSelected: Boolean = false,
     val isCompleteButtonEnabled: Boolean = false,
