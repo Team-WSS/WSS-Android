384a154ce838d4f25d46f14adf1fb1c7be36c090
junseo511 <prime54@g.skku.edu>
2024-08-26 14:55:45 +0900

    refactor: 변수명 변경

--- app/src/main/java/com/teamwss/websoso/data/repository/UserPreferencesRepository.kt
+++ app/src/main/java/com/teamwss/websoso/data/repository/UserPreferencesRepository.kt
@@ -8,17 +8,17 @@ import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 
 class UserPreferencesRepository @Inject constructor(
-    private val localStorage: DataStore<Preferences>,
+    private val storage: DataStore<Preferences>,
 ) {
 
     suspend fun saveNovelDetailFirstLaunched(value: Boolean) {
-        localStorage.edit { preferences ->
+        storage.edit { preferences ->
             preferences[KEY_NOVEL_DETAIL_FIRST_LAUNCHED] = value
         }
     }
 
     suspend fun fetchNovelDetailFirstLaunched(): Boolean {
-        return localStorage.data.first()[KEY_NOVEL_DETAIL_FIRST_LAUNCHED] ?: true
+        return storage.data.first()[KEY_NOVEL_DETAIL_FIRST_LAUNCHED] ?: true
     }
 
     companion object {
