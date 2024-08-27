cc52bd80c0fa2db777dd3db1afc96263984fe1e2
junseo511 <prime54@g.skku.edu>
2024-08-27 15:53:33 +0900

    feat: 캐릭터 변경 바텀시트 추가

---
+++ app/src/main/java/com/teamwss/websoso/ui/profileEdit/ProfileEditCharacterBottomSheetDialog.kt
@@ -0,0 +1,8 @@
+package com.teamwss.websoso.ui.profileEdit
+
+import com.teamwss.websoso.R
+import com.teamwss.websoso.common.ui.base.BaseBottomSheetDialog
+import com.teamwss.websoso.databinding.DialogProfileEditCharacterBinding
+
+class ProfileEditCharacterBottomSheetDialog : BaseBottomSheetDialog<DialogProfileEditCharacterBinding>(R.layout.dialog_profile_edit_character) {
+}
