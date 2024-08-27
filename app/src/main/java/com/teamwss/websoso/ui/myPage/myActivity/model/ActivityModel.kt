f7ebc49384288f59feb4e387cfa6b5c1606712ee
yeonjeen <jeendev2019@gmail.com>
2024-08-28 02:39:11 +0900

    refactor: 코드리뷰 반영

--- app/src/main/java/com/teamwss/websoso/ui/myPage/myActivity/model/ActivityModel.kt
+++ app/src/main/java/com/teamwss/websoso/ui/myPage/myActivity/model/ActivityModel.kt
@@ -1,5 +1,8 @@
 package com.teamwss.websoso.ui.myPage.myActivity.model
 
+import java.text.SimpleDateFormat
+import java.util.Locale
+
 data class ActivityModel(
     val feedId: Int,
     val userId: Int,
@@ -16,5 +19,21 @@ data class ActivityModel(
     val title: String,
     val novelRatingCount: Int?,
     val novelRating: Float?,
-    val relevantCategories:String,
-)
+    val relevantCategories: String
+) {
+    companion object {
+        fun translateGenres(relevantCategories: List<String>): String {
+            return relevantCategories.joinToString(", ") { category ->
+                Genres.from(category)?.korean ?: category
+            }
+        }
+
+        fun formatDate(inputDate: String): String {
+            val inputFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+            val outputFormat = SimpleDateFormat("M월 d일", Locale.getDefault())
+
+            val date = inputFormat.parse(inputDate)
+            return outputFormat.format(date!!)
+        }
+    }
+}
