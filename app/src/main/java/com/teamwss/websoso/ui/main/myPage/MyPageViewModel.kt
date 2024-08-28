870c2115c6bc257b798658f3677063143be7b8c0
yeonjeen <jeendev2019@gmail.com>
2024-08-29 02:43:04 +0900

    feat: 내 프로필 api 연결

---
+++ app/src/main/java/com/teamwss/websoso/ui/main/myPage/MyPageViewModel.kt
@@ -0,0 +1,34 @@
+package com.teamwss.websoso.ui.main.myPage
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.teamwss.websoso.data.model.MyProfileEntity
+import com.teamwss.websoso.data.repository.UserRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import javax.inject.Inject
+
+@HiltViewModel
+class MyPageViewModel @Inject constructor(
+    private val userRepository: UserRepository
+) : ViewModel() {
+
+    private val _myProfile = MutableLiveData<MyProfileEntity>()
+    val myProfile: LiveData<MyProfileEntity> get() = _myProfile
+
+    fun fetchUserProfile() {
+        viewModelScope.launch {
+            runCatching {
+                userRepository.fetchMyProfile()
+            }.onSuccess { myProfile ->
+                _myProfile.value = myProfile
+            }.onFailure { exception ->
+                Log.e("MyPageViewModel", "Failed to load user profile", exception)
+            }
+        }
+    }
+}
