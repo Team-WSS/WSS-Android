9c0a1b5aff318b3a52ab0b51866898a218e4d40b
yeonjeen <jeendev2019@gmail.com>
2024-08-29 15:53:27 +0900

    refactor: 현판 이름 수정

--- app/src/main/java/com/teamwss/websoso/ui/main/myPage/myActivity/model/Genres.kt
+++ app/src/main/java/com/teamwss/websoso/ui/main/myPage/myActivity/model/Genres.kt
@@ -6,7 +6,7 @@ enum class Genres(val korean: String) {
     ROMANCEFANTASY("로판"),
     BOYSLOVE("BL"),
     FANTASY("판타지"),
-    MODERN_FANTASY("현판"),
+    MODERNFANTASY("현판"),
     WUXIA("무협"),
     DRAMA("드라마"),
     MYSTERY("미스터리"),
