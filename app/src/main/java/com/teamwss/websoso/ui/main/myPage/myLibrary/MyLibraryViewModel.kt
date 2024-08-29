60a836556724d8629bb27eb7ecf88d2f15fc15ef
yeonjeen <jeendev2019@gmail.com>
2024-08-29 16:08:20 +0900

    feat: 장르 및 작품 선호 업데이트

--- app/src/main/java/com/teamwss/websoso/ui/main/myPage/myLibrary/MyLibraryViewModel.kt
+++ app/src/main/java/com/teamwss/websoso/ui/main/myPage/myLibrary/MyLibraryViewModel.kt
@@ -8,7 +8,6 @@ import androidx.lifecycle.viewModelScope
 import com.teamwss.websoso.data.model.GenrePreferenceEntity
 import com.teamwss.websoso.data.model.NovelPreferenceEntity
 import com.teamwss.websoso.data.model.UserNovelStatsEntity
-import com.teamwss.websoso.data.repository.FakeUserRepository
 import com.teamwss.websoso.data.repository.UserRepository
 import com.teamwss.websoso.ui.main.myPage.myLibrary.model.AttractivePoints
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -17,12 +16,17 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MyLibraryViewModel @Inject constructor(
-    private val fakeUserRepository: FakeUserRepository,
     private val userRepository: UserRepository
 ) :
     ViewModel() {
     private val _genres = MutableLiveData<List<GenrePreferenceEntity>>()
     val genres: LiveData<List<GenrePreferenceEntity>> = _genres
+
+    private val _dominantGenres = MutableLiveData<List<GenrePreferenceEntity>>()
+    val topGenres: LiveData<List<GenrePreferenceEntity>> = _dominantGenres
+
+    private val _restGenres = MutableLiveData<List<GenrePreferenceEntity>>()
+    val restGenres: LiveData<List<GenrePreferenceEntity>> = _restGenres
 
     private val _isGenreListVisible = MutableLiveData<Boolean>(false)
     val isGenreListVisible: LiveData<Boolean> = _isGenreListVisible
@@ -31,15 +35,20 @@ class MyLibraryViewModel @Inject constructor(
     val novelPreferences: LiveData<NovelPreferenceEntity> = _novelPreferences
 
     private val _attractivePointsText = MutableLiveData<String>()
-    val attractivePointsText: LiveData<String> = _attractivePointsText
+    val attractivePointsText: LiveData<String> get() = _attractivePointsText
+
+    private val _translatedAttractivePoints = MutableLiveData<List<String>>()
+    val translatedAttractivePoints: LiveData<List<String>> get() = _translatedAttractivePoints
 
     private val _novelStats = MutableLiveData<UserNovelStatsEntity>()
     val novelStats: LiveData<UserNovelStatsEntity> get() = _novelStats
 
+    private val userId: Long = getUserId()
+
     init {
         updateNovelStats()
-        updatePreferenceData()
-        setDummyData()
+        updateGenrePreference(userId)
+        updateNovelPreferences(userId)
     }
 
     private fun updateNovelStats() {
@@ -47,7 +56,6 @@ class MyLibraryViewModel @Inject constructor(
             runCatching {
                 userRepository.fetchUserNovelStats()
             }.onSuccess { novelStats ->
-                Log.d("MyLibraryFragment", "Novel Stats: $novelStats")
                 _novelStats.value = novelStats
             }.onFailure { exception ->
                 Log.e("NovelViewModel", "Failed to load novel counts", exception)
@@ -55,32 +63,45 @@ class MyLibraryViewModel @Inject constructor(
         }
     }
 
-    private fun updatePreferenceData() {
-        _genres.value = fakeUserRepository.getGenres()
-        _novelPreferences.value = fakeUserRepository.getNovelPreferences()
+    private fun updateGenrePreference(userId: Long) {
+        viewModelScope.launch {
+            runCatching {
+                userRepository.fetchGenrePreference(userId)
+            }.onSuccess { genres ->
+                val sortedGenres = genres.sortedByDescending { it.genreCount }
+
+                _dominantGenres.value = sortedGenres.take(3)
+                _restGenres.value = sortedGenres.drop(3)
+            }.onFailure { exception ->
+                Log.e("MyLibraryViewModel", "Failed to load genre preference", exception)
+            }
+        }
+    }
+
+    private fun getUserId(): Long {
+        return 1L
     }
 
     fun updateToggleGenresVisibility() {
         _isGenreListVisible.value = _isGenreListVisible.value?.not() ?: false
     }
 
-    fun setAttractivePointText(serverText: String, fixedText: String) {
-        val translatedText = translateAttractivePointsText(serverText)
-        _attractivePointsText.value = "$translatedText$fixedText"
-    }
-
-    private fun translateAttractivePointsText(text: String): String {
-        return text.split(", ").joinToString(", ") { point ->
-            AttractivePoints.fromString(point)?.korean ?: point
+    private fun updateNovelPreferences(userId: Long) {
+        viewModelScope.launch {
+            runCatching {
+                userRepository.fetchNovelPreferences(userId)
+            }.onSuccess { novelPreferences ->
+                _novelPreferences.value = novelPreferences
+                _translatedAttractivePoints.value = translateAttractivePoints(novelPreferences.attractivePoints)
+            }.onFailure { exception ->
+                Log.e("MyLibraryViewModel", "Failed to load novel preferences", exception)
+            }
         }
     }
 
-    private fun setDummyData() {
-        val dummyPreferences = fakeUserRepository.getNovelPreferences()
-
-        val serverText = dummyPreferences.attractivePoint.joinToString(", ")
-        val fixedText = "가 매력적인 작품"
-
-        setAttractivePointText(serverText, fixedText)
+    private fun translateAttractivePoints(attractivePoints: List<String>): List<String> {
+        return attractivePoints.mapNotNull { point ->
+            AttractivePoints.fromString(point)?.korean
+        }
     }
 }
