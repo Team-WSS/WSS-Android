fd5df5dd251b09767a35264787f2415d13bf8a78
librarywon <librarywon@naver.com>
2024-08-30 02:31:57 +0900

    refactor: userInfo -> userProfile 변수명 변경

--- app/src/main/java/com/teamwss/websoso/ui/onboarding/second/OnboardingSecondFragment.kt
+++ app/src/main/java/com/teamwss/websoso/ui/onboarding/second/OnboardingSecondFragment.kt
@@ -29,7 +29,7 @@ class OnboardingSecondFragment :
     }
 
     private fun setupObserver() {
-        onboardingViewModel.userInfo.observe(viewLifecycleOwner) { userInfo ->
+        onboardingViewModel.userProfile.observe(viewLifecycleOwner) { userInfo ->
             updateBirthYearTextUi(userInfo)
         }
     }
