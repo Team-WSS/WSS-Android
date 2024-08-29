fca92b6b51fd689271f20acd2dccebaeed07da52
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-08-30 01:01:53 +0900

    feat: 뷰모델에 검색결과를 위한 필터값 가져오는 로직 추가

--- app/src/main/java/com/teamwss/websoso/ui/detailExploreResult/DetailExploreResultViewModel.kt
+++ app/src/main/java/com/teamwss/websoso/ui/detailExploreResult/DetailExploreResultViewModel.kt
@@ -1,10 +1,14 @@
 package com.teamwss.websoso.ui.detailExploreResult
 
+import android.util.Log
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.map
 import androidx.lifecycle.viewModelScope
 import com.teamwss.websoso.data.repository.FakeNovelRepository
+import com.teamwss.websoso.ui.detailExplore.info.model.Genre
 import com.teamwss.websoso.ui.detailExploreResult.model.DetailExploreResultUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -18,8 +22,44 @@ class DetailExploreResultViewModel @Inject constructor(
         MutableLiveData(DetailExploreResultUiState())
     val uiState: LiveData<DetailExploreResultUiState> get() = _uiState
 
+    private val _selectedGenres: MutableLiveData<MutableList<Genre>> =
+        MutableLiveData(mutableListOf())
+    val selectedGenres: LiveData<List<Genre>> get() = _selectedGenres.map { it.toList() }
+
+    private val _selectedSeriesStatus: MutableLiveData<Boolean?> = MutableLiveData()
+    val selectedStatus: LiveData<Boolean?> get() = _selectedSeriesStatus
+
+    private val _selectedRating: MutableLiveData<Float?> = MutableLiveData()
+    val selectedRating: LiveData<Float?> get() = _selectedRating
+
+    val ratings: List<Float> = listOf(3.5f, 4.0f, 4.5f, 4.8f)
+
+    private val _isInfoChipSelected: MediatorLiveData<Boolean> = MediatorLiveData(false)
+    val isInfoChipSelected: LiveData<Boolean> get() = _isInfoChipSelected
+
+    private val _isKeywordChipSelected: MutableLiveData<Boolean> = MutableLiveData(false)
+    val isKeywordChipSelected: LiveData<Boolean> get() = _isKeywordChipSelected
+
     private val _isNovelResultEmptyBoxVisibility: MutableLiveData<Boolean> = MutableLiveData(false)
     val isNovelResultEmptyBoxVisibility: LiveData<Boolean> get() = _isNovelResultEmptyBoxVisibility
+
+    fun updateDetailExploreFilteredValue(
+        genre: Array<Genre>?,
+        isCompleted: Boolean?,
+        novelRating: Float?,
+        keywordIds: Array<Int>?
+    ) {
+        _selectedGenres.value = genre?.toMutableList()
+        _selectedSeriesStatus.value = isCompleted
+        _selectedRating.value = novelRating
+        _uiState.value = uiState.value?.copy(
+            selectedKeywords = keywordIds?.toList() ?: emptyList(),
+        )
+        Log.d(
+            "moongchi",
+            "updateDetailExploreFilteredValue: genres=${genre?.joinToString()} isCompleted=$isCompleted novelRating=$novelRating keywordIds=${keywordIds?.joinToString()}"
+        )
+    }
 
     fun updateSearchResult() {
         viewModelScope.launch {
