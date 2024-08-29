6c6304479082cf8846385b2568eb09e85f6ccd37
yeonjeen <jeendev2019@gmail.com>
2024-08-29 11:22:41 +0900

    feat: 내 서재 보관함 카운트 api 연결

--- app/src/main/java/com/teamwss/websoso/ui/main/myPage/myLibrary/MyLibraryViewModel.kt
+++ app/src/main/java/com/teamwss/websoso/ui/main/myPage/myLibrary/MyLibraryViewModel.kt
@@ -1,17 +1,25 @@
 package com.teamwss.websoso.ui.main.myPage.myLibrary
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.teamwss.websoso.data.model.GenrePreferenceEntity
 import com.teamwss.websoso.data.model.NovelPreferenceEntity
+import com.teamwss.websoso.data.model.UserNovelStatsEntity
 import com.teamwss.websoso.data.repository.FakeUserRepository
+import com.teamwss.websoso.data.repository.UserRepository
 import com.teamwss.websoso.ui.main.myPage.myLibrary.model.AttractivePoints
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class MyLibraryViewModel @Inject constructor(private val fakeUserRepository: FakeUserRepository) :
+class MyLibraryViewModel @Inject constructor(
+    private val fakeUserRepository: FakeUserRepository,
+    private val userRepository: UserRepository
+) :
     ViewModel() {
     private val _genres = MutableLiveData<List<GenrePreferenceEntity>>()
     val genres: LiveData<List<GenrePreferenceEntity>> = _genres
@@ -25,9 +33,26 @@ class MyLibraryViewModel @Inject constructor(private val fakeUserRepository: Fak
     private val _attractivePointsText = MutableLiveData<String>()
     val attractivePointsText: LiveData<String> = _attractivePointsText
 
+    private val _novelStats = MutableLiveData<UserNovelStatsEntity>()
+    val novelStats: LiveData<UserNovelStatsEntity> get() = _novelStats
+
     init {
+        updateNovelStats()
         updatePreferenceData()
         setDummyData()
+    }
+
+    private fun updateNovelStats() {
+        viewModelScope.launch {
+            runCatching {
+                userRepository.fetchUserNovelStats()
+            }.onSuccess { novelStats ->
+                Log.d("MyLibraryFragment", "Novel Stats: $novelStats")
+                _novelStats.value = novelStats
+            }.onFailure { exception ->
+                Log.e("NovelViewModel", "Failed to load novel counts", exception)
+            }
+        }
     }
 
     private fun updatePreferenceData() {
