a0fbc103102a5856e0b682673fa6d5b0c6c97dfe
yeonjeen <jeendev2019@gmail.com>
2024-08-30 04:17:48 +0900

    feat: 다른 유저 활동 리사이클러뷰 구현

--- app/src/main/java/com/teamwss/websoso/ui/otherUserPage/otherUserActivity/OtherUserActivityFragment.kt
+++ app/src/main/java/com/teamwss/websoso/ui/otherUserPage/otherUserActivity/OtherUserActivityFragment.kt
@@ -1,10 +1,46 @@
 package com.teamwss.websoso.ui.otherUserPage.otherUserActivity
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
 import com.teamwss.websoso.R
 import com.teamwss.websoso.common.ui.base.BaseFragment
 import com.teamwss.websoso.databinding.FragmentOtherUserActivityBinding
+import com.teamwss.websoso.ui.activityDetail.ActivityDetailActivity
+import com.teamwss.websoso.ui.otherUserPage.otherUserActivity.adapter.OtherUserActivityAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class OtherUserActivityFragment :
-    BaseFragment<FragmentOtherUserActivityBinding>(R.layout.fragment_other_user_activity)
+    BaseFragment<FragmentOtherUserActivityBinding>(R.layout.fragment_other_user_activity) {
+    private val otherUserActivityViewModel: OtherUserActivityViewModel by viewModels()
+    private val otherUserActivityAdapter: OtherUserActivityAdapter by lazy {
+        OtherUserActivityAdapter()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setUpMyActivitiesAdapter()
+        setUpObserve()
+        navigateToMyActivityDetail()
+    }
+
+    private fun setUpMyActivitiesAdapter() {
+        binding.rvOtherUserActivity.adapter = otherUserActivityAdapter
+    }
+
+    private fun setUpObserve() {
+        otherUserActivityViewModel.otherUserActivity.observe(viewLifecycleOwner) { activities ->
+            otherUserActivityAdapter.submitList(activities)
+        }
+    }
+
+    private fun navigateToMyActivityDetail() {
+        binding.btnOtherUserActivityMore.setOnClickListener {
+            val intent = ActivityDetailActivity.createIntentForMyActivityDetail(requireContext())
+            startActivity(intent)
+        }
+    }
+}
+
---
+++ app/src/main/java/com/teamwss/websoso/ui/otherUserPage/otherUserActivity/OtherUserActivityViewModel.kt
@@ -0,0 +1,39 @@
+package com.teamwss.websoso.ui.otherUserPage.otherUserActivity
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.teamwss.websoso.data.repository.UserFeedRepository
+import com.teamwss.websoso.ui.main.myPage.myActivity.model.ActivityModel
+import com.teamwss.websoso.ui.mapper.toUi
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class OtherUserActivityViewModel @Inject constructor(
+    private val otherUserActivityRepository: UserFeedRepository,
+) : ViewModel() {
+    private val _otherUserActivity = MutableLiveData<List<ActivityModel>>()
+    val otherUserActivity: LiveData<List<ActivityModel>> get() = _otherUserActivity
+
+    init {
+        updateMyActivities()
+    }
+
+    private fun updateMyActivities() {
+        viewModelScope.launch {
+            runCatching {
+                otherUserActivityRepository.getUserFeed()
+            }.mapCatching { activities ->
+                activities.toUi().take(5)
+            }.onSuccess { mappedActivities ->
+                _otherUserActivity.value = mappedActivities
+            }.onFailure { exception ->
+                Log.e("MyActivityViewModel", "Failed to load activities", exception)
+            }
+        }
+    }
+}
