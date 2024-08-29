a1ddcd73034609ecd2e8e3a6bac5ad4713ba3695
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-08-30 01:01:28 +0900

    feat: 검색 결과 uiState 수정

--- app/src/main/java/com/teamwss/websoso/ui/detailExploreResult/model/DetailExploreResultUiState.kt
+++ app/src/main/java/com/teamwss/websoso/ui/detailExploreResult/model/DetailExploreResultUiState.kt
@@ -1,9 +1,15 @@
 package com.teamwss.websoso.ui.detailExploreResult.model
 
-import com.teamwss.websoso.data.model.ExploreResultEntity
+import com.teamwss.websoso.common.ui.model.CategoriesModel.CategoryModel
+import com.teamwss.websoso.data.model.ExploreResultEntity.NovelEntity
 
 data class DetailExploreResultUiState(
     val loading: Boolean = true,
     val error: Boolean = false,
-    val novels: List<ExploreResultEntity.NovelEntity> = emptyList(),
+    val novels: List<NovelEntity> = emptyList(),
+    val categories: List<CategoryModel> = emptyList(),
+    val selectedKeywords: List<Int> = emptyList(),
+    val isSearchKeywordProceeding: Boolean = false,
+    val isInitialSearchKeyword: Boolean = true,
+    val isSearchResultKeywordsEmpty: Boolean = false,
 )
