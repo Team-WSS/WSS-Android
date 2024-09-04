4085ea19570ff15663556e1029b1e0834e4124c0
yeonjeen <jeendev2019@gmail.com>
2024-09-05 01:03:17 +0900

    feat: 유저피드조회 Dto/Entity 맵핑

--- app/src/main/java/com/teamwss/websoso/data/repository/UserRepository.kt
+++ app/src/main/java/com/teamwss/websoso/data/repository/UserRepository.kt
@@ -6,6 +6,7 @@ import com.teamwss.websoso.data.model.GenrePreferenceEntity
 import com.teamwss.websoso.data.model.MyProfileEntity
 import com.teamwss.websoso.data.model.NovelPreferenceEntity
 import com.teamwss.websoso.data.model.OtherUserProfileEntity
+import com.teamwss.websoso.data.model.UserFeedsEntity
 import com.teamwss.websoso.data.model.UserInfoEntity
 import com.teamwss.websoso.data.model.UserNovelStatsEntity
 import com.teamwss.websoso.data.model.UserProfileStatusEntity
@@ -60,5 +61,9 @@ class UserRepository @Inject constructor(
 
     suspend fun fetchOtherUserProfile(userId: Long): OtherUserProfileEntity {
         return userApi.getOtherUserProfile(userId).toData()
+    }
+
+    suspend fun fetchUserFeeds(userId: Long,lastFeedId: Long, size: Int) :UserFeedsEntity{
+        return userApi.getUserFeeds(userId,lastFeedId,size).toData()
     }
 }
