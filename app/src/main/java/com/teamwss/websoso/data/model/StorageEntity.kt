15f54fe963875e026015ddfe925d732fc244c08f
yeonjeen <jeendev2019@gmail.com>
2024-09-13 10:23:05 +0900

    feat: 보관함 Entity

---
+++ app/src/main/java/com/teamwss/websoso/data/model/StorageEntity.kt
@@ -0,0 +1,17 @@
+package com.teamwss.websoso.data.model
+
+data class StorageEntity(
+    val isLoadable: Boolean,
+    val userNovelCount: Int,
+    val userNovelRating: Double,
+    val userNovels: List<UserNovel>,
+) {
+    data class UserNovel(
+        val author: String,
+        val novelId: Int,
+        val novelImage: String,
+        val novelRating: Double,
+        val title: String,
+        val userNovelId: Int,
+    )
+}
