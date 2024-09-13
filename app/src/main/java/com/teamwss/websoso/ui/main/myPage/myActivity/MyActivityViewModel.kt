53f02203c14a1c37f1abc228f582a4bb78eb40a4
yeonjeen <jeendev2019@gmail.com>
2024-09-13 11:01:31 +0900

    build: 빌드오류 주석처리

--- app/src/main/java/com/teamwss/websoso/ui/main/myPage/myActivity/MyActivityViewModel.kt
+++ app/src/main/java/com/teamwss/websoso/ui/main/myPage/myActivity/MyActivityViewModel.kt
@@ -1,15 +1,11 @@
 package com.teamwss.websoso.ui.main.myPage.myActivity
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.teamwss.websoso.data.repository.UserRepository
 import com.teamwss.websoso.ui.main.myPage.myActivity.model.ActivityModel
-import com.teamwss.websoso.ui.mapper.toUi
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -27,27 +23,27 @@ class MyActivityViewModel @Inject constructor(
 
     private val size: Int = 10
 
-    init {
-        updateMyActivities()
-    }
-
-    private fun updateMyActivities() {
-        viewModelScope.launch {
-            val result = runCatching {
-                myActivityRepository.fetchUserFeeds(userId, _lastFeedId.value ?: 0L, size)
-            }
-
-            result.onSuccess { responseDto ->
-                val mappedActivities = responseDto.feeds.map { it.toUi() }.take(5)
-                _myActivity.value = mappedActivities
-
-                val lastId = responseDto.feeds.lastOrNull()?.feedId?.toLong() ?: 0L
-                _lastFeedId.value = lastId
-            }.onFailure { exception ->
-                Log.e("MyActivityViewModel", "Failed to load activities", exception)
-            }
-        }
-    }
+//    init {
+//        updateMyActivities()
+//    }
+//
+//    private fun updateMyActivities() {
+//        viewModelScope.launch {
+//            val result = runCatching {
+//                myActivityRepository.fetchUserFeeds(userId, _lastFeedId.value ?: 0L, size)
+//            }
+//
+//            result.onSuccess { responseDto ->
+//                val mappedActivities = responseDto.feeds.map { it.toUi() }.take(5)
+//                _myActivity.value = mappedActivities
+//
+//                val lastId = responseDto.feeds.lastOrNull()?.feedId?.toLong() ?: 0L
+//                _lastFeedId.value = lastId
+//            }.onFailure { exception ->
+//                Log.e("MyActivityViewModel", "Failed to load activities", exception)
+//            }
+//        }
+//    }
 
     private fun getUserId(): Long {
         return 2L
