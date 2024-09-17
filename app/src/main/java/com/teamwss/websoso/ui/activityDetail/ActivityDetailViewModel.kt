af1bc784257a73387dbdc0f6123a8e5d4e0436ef
yeonjeen <jeendev2019@gmail.com>
2024-09-17 23:45:00 +0900

    fix: 충돌해결

--- app/src/main/java/com/teamwss/websoso/ui/activityDetail/ActivityDetailViewModel.kt
+++ app/src/main/java/com/teamwss/websoso/ui/activityDetail/ActivityDetailViewModel.kt
@@ -3,12 +3,9 @@ package com.teamwss.websoso.ui.activityDetail
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.teamwss.websoso.data.repository.UserFeedRepository
 import com.teamwss.websoso.ui.main.myPage.myActivity.model.ActivityModel
-import com.teamwss.websoso.ui.mapper.toUi
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -20,20 +17,20 @@ class ActivityDetailViewModel @Inject constructor(
     val userActivity: LiveData<List<ActivityModel>> get() = _userActivity
 
     init {
-        updateMyActivities()
+//        updateMyActivities()
     }
 
-    private fun updateMyActivities() {
-        viewModelScope.launch {
-            runCatching {
-                activityRepository.getUserFeed()
-            }.mapCatching { activities ->
-                activities.toUi().take(5)
-            }.onSuccess { mappedActivities ->
-                _userActivity.value = mappedActivities
-            }.onFailure { exception ->
-
-            }
-        }
-    }
+//    private fun updateMyActivities() {
+//        viewModelScope.launch {
+//            runCatching {
+//                activityRepository.getUserFeed()
+//            }.mapCatching { activities ->
+//                activities.toUi().take(5)
+//            }.onSuccess { mappedActivities ->
+//                _userActivity.value = mappedActivities
+//            }.onFailure { exception ->
+//
+//            }
+//        }
+//    }
 }
--- app/src/main/java/com/teamwss/websoso/ui/otherUserPage/otherUserActivity/OtherUserActivityViewModel.kt
+++ app/src/main/java/com/teamwss/websoso/ui/otherUserPage/otherUserActivity/OtherUserActivityViewModel.kt
@@ -1,15 +1,11 @@
 package com.teamwss.websoso.ui.otherUserPage.otherUserActivity
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.teamwss.websoso.data.repository.UserFeedRepository
 import com.teamwss.websoso.ui.main.myPage.myActivity.model.ActivityModel
-import com.teamwss.websoso.ui.mapper.toUi
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -20,24 +16,24 @@ class OtherUserActivityViewModel @Inject constructor(
     val otherUserActivity: LiveData<List<ActivityModel>> get() = _otherUserActivity
 
     init {
-        updateMyActivities()
+//        updateMyActivities()
     }
 
-    private fun updateMyActivities() {
-        viewModelScope.launch {
-            runCatching {
-                otherUserActivityRepository.getUserFeed()
-            }.mapCatching { activities ->
-                activities.toUi().take(ACTIVITY_COUNT)
-            }.onSuccess { mappedActivities ->
-                _otherUserActivity.value = mappedActivities
-            }.onFailure { exception ->
+//    private fun updateMyActivities() {
+//        viewModelScope.launch {
+//            runCatching {
+//                otherUserActivityRepository.getUserFeed()
+//            }.mapCatching { activities ->
+//                activities.toUi().take(ACTIVITY_COUNT)
+//            }.onSuccess { mappedActivities ->
+//                _otherUserActivity.value = mappedActivities
+//            }.onFailure { exception ->
+//
+//            }
+//        }
+//    }
 
-            }
-        }
-    }
-
-    companion object{
+    companion object {
         const val ACTIVITY_COUNT = 5
     }
 }
