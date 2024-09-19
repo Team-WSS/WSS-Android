c24a1b152084c8f6303170ce900e804db961f329
librarywon <librarywon@naver.com>
2024-09-19 20:07:14 +0900

    refactor: var -> val

--- app/src/main/java/com/teamwss/websoso/ui/main/MainViewModel.kt
+++ app/src/main/java/com/teamwss/websoso/ui/main/MainViewModel.kt
@@ -6,6 +6,6 @@ import androidx.lifecycle.ViewModel
 import com.teamwss.websoso.ui.main.model.MainUiState
 
 class MainViewModel: ViewModel() {
-    private var _mainUiState : MutableLiveData<MainUiState> = MutableLiveData(MainUiState())
+    private val _mainUiState : MutableLiveData<MainUiState> = MutableLiveData(MainUiState())
     val mainUiState : LiveData<MainUiState> get() = _mainUiState
 }
