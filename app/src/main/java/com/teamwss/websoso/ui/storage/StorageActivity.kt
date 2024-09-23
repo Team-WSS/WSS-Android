63c8dddd7322fa4c89b18ef3cdb923d343d71078
yeonjeen <jeendev2019@gmail.com>
2024-09-24 08:31:37 +0900

    feat: 보관함 액티비티 구현

--- app/src/main/java/com/teamwss/websoso/ui/storage/StorageActivity.kt
+++ app/src/main/java/com/teamwss/websoso/ui/storage/StorageActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
+import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import com.teamwss.websoso.R
 import com.teamwss.websoso.common.ui.base.BaseActivity
@@ -11,6 +12,7 @@ import com.teamwss.websoso.databinding.ActivityStorageBinding
 import com.teamwss.websoso.ui.main.MainActivity
 import com.teamwss.websoso.ui.storage.adapter.StorageViewPagerAdapter
 import com.teamwss.websoso.ui.storage.model.StorageTab
+import com.teamwss.websoso.ui.storage.model.StorageUiState
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -19,33 +21,84 @@ class StorageActivity : BaseActivity<ActivityStorageBinding>(R.layout.activity_s
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        bindViewModel()
         setupViewPagerAndTabLayout()
+        onSortButtonClick()
         onBackButtonClick()
     }
 
+    private fun bindViewModel() {
+        binding.lifecycleOwner = this
+        binding.storageViewModel = storageViewModel
+    }
+
     private fun setupViewPagerAndTabLayout() {
-        val pagerAdapter =
-            StorageViewPagerAdapter(storageViewModel.novelsMap, ::navigateToExploreFragment)
+        setupViewPager()
+        setupTabLayoutWithViewPager()
+        setupObserver()
+    }
+
+    private fun setupViewPager() {
+        val pagerAdapter = StorageViewPagerAdapter(
+            novels = emptyList(),
+            navigateToExplore = ::navigateToExploreFragment,
+        )
         binding.vpStorage.adapter = pagerAdapter
-        bindTabLayoutWithViewPager()
     }
 
-    private fun bindTabLayoutWithViewPager() {
-        TabLayoutMediator(binding.tlStorage, binding.vpStorage) { tab, position ->
-            tab.text = StorageTab.fromPosition(position).title
-        }.attach()
-    }
+    private fun setupTabLayoutWithViewPager() {
+        binding.vpStorage.adapter?.let {
+            TabLayoutMediator(binding.tlStorage, binding.vpStorage) { tab, position ->
+                tab.text = StorageTab.fromPosition(position).title
+            }.attach()
 
-    private fun onBackButtonClick() {
-        binding.ivStorageDetailBackButton.setOnClickListener {
-            finish()
+            binding.tlStorage.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+                override fun onTabSelected(tab: TabLayout.Tab?) {
+                    tab?.let {
+                        onSortTabSelected(it.position)
+                    }
+                }
+
+                override fun onTabUnselected(tab: TabLayout.Tab?) {}
+                override fun onTabReselected(tab: TabLayout.Tab?) {}
+            })
         }
+    }
+
+    private fun onSortTabSelected(position: Int) {
+        val selectedTab = StorageTab.fromPosition(position)
+        storageViewModel.updateReadStatus(
+            selectedTab.readStatus,
+            forceLoad = true,
+        )
+    }
+
+    private fun setupObserver() {
+        storageViewModel.uiState.observe(this) { uiState ->
+            updateViewPagerAdapter(uiState)
+        }
+    }
+
+    private fun updateViewPagerAdapter(uiState: StorageUiState) {
+        val adapter = binding.vpStorage.adapter as? StorageViewPagerAdapter
+        adapter?.updateNovels(uiState.storageNovels)
     }
 
     private fun navigateToExploreFragment() {
         val intent = Intent(this, MainActivity::class.java)
         intent.putExtra("navigateToExplore", true)
         startActivity(intent)
+    }
+
+    private fun onSortButtonClick() {
+        val clickHandler = SortClickHandler(storageViewModel)
+        binding.onClick = clickHandler
+    }
+
+    private fun onBackButtonClick() {
+        binding.ivStorageDetailBackButton.setOnClickListener {
+            finish()
+        }
     }
 
     companion object {
