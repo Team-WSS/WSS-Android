2615f7d2d5c0a72272f8b88a16f79841068df930
yeonjeen <jeendev2019@gmail.com>
2024-09-23 23:07:23 +0900

    feat: dto와 entity 매핑

--- app/src/main/java/com/teamwss/websoso/data/mapper/UserMapper.kt
+++ app/src/main/java/com/teamwss/websoso/data/mapper/UserMapper.kt
@@ -6,6 +6,7 @@ import com.teamwss.websoso.data.model.GenrePreferenceEntity
 import com.teamwss.websoso.data.model.MyProfileEntity
 import com.teamwss.websoso.data.model.NovelPreferenceEntity
 import com.teamwss.websoso.data.model.OtherUserProfileEntity
+import com.teamwss.websoso.data.model.StorageEntity
 import com.teamwss.websoso.data.model.UserInfoEntity
 import com.teamwss.websoso.data.model.UserNovelStatsEntity
 import com.teamwss.websoso.data.model.UserProfileStatusEntity
@@ -14,6 +15,7 @@ import com.teamwss.websoso.data.remote.response.GenrePreferenceResponseDto
 import com.teamwss.websoso.data.remote.response.MyProfileResponseDto
 import com.teamwss.websoso.data.remote.response.NovelPreferenceResponseDto
 import com.teamwss.websoso.data.remote.response.OtherUserProfileResponseDto
+import com.teamwss.websoso.data.remote.response.StorageResponseDto
 import com.teamwss.websoso.data.remote.response.UserInfoResponseDto
 import com.teamwss.websoso.data.remote.response.UserNovelStatsResponseDto
 import com.teamwss.websoso.data.remote.response.UserProfileStatusResponseDto
@@ -92,5 +94,25 @@ fun OtherUserProfileResponseDto.toData(): OtherUserProfileEntity {
         avatarImage = this.avatarImage,
         isProfilePublic = this.isProfilePublic,
         genrePreferences = this.genrePreferences,
+    )
+}
+
+fun StorageResponseDto.toData(): StorageEntity {
+    return StorageEntity(
+        isLoadable = this.isLoadable,
+        userNovelRating = this.userNovelRating,
+        userNovelCount = this.userNovelCount,
+        userNovels = this.userNovels.map { it.toData() },
+    )
+}
+
+fun StorageResponseDto.StorageNovelDto.toData(): StorageEntity.StorageNovelEntity {
+    return StorageEntity.StorageNovelEntity(
+        author = this.author,
+        userNovelId = this.userNovelId,
+        novelId = this.novelId,
+        novelRating = this.novelRating,
+        novelImage = this.novelImage,
+        title = this.title,
     )
 }
