5e8fffa8162284472fc24a4d10479c0b90f45878
yeonjeen <jeendev2019@gmail.com>
2024-09-24 08:35:53 +0900

    refactor: entity명 변경 및 span count 상수화

--- app/src/main/java/com/teamwss/websoso/ui/storage/adapter/StorageViewPagerViewHolder.kt
+++ app/src/main/java/com/teamwss/websoso/ui/storage/adapter/StorageViewPagerViewHolder.kt
@@ -7,20 +7,24 @@ import com.teamwss.websoso.databinding.ItemStorageBinding
 
 class StorageViewPagerViewHolder(
     private val binding: ItemStorageBinding,
-    private val navigateToExplore: () -> Unit,
+    private val navigateToExplore: () -> Unit
 ) : RecyclerView.ViewHolder(binding.root) {
 
-    fun bind(novels: List<StorageEntity.UserNovel>) {
+    fun bind(novels: List<StorageEntity.StorageNovelEntity>) {
         binding.hasData = novels.isNotEmpty()
 
         if (novels.isNotEmpty()) {
             val adapter = StorageItemAdapter(novels)
             binding.rvStorage.adapter = adapter
-            binding.rvStorage.layoutManager = GridLayoutManager(binding.root.context, 3)
+            binding.rvStorage.layoutManager = GridLayoutManager(binding.root.context, STORAGE_NOVEL_SPAN_COUNT)
         }
 
         binding.btnStorageGoToSearchNovel.setOnClickListener {
             navigateToExplore()
         }
+    }
+
+    companion object {
+        const val STORAGE_NOVEL_SPAN_COUNT = 3
     }
 }
