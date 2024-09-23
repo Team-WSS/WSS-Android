f3b29967625b3dd833b5f30a43ca2110a658e499
yeonjeen <jeendev2019@gmail.com>
2024-09-23 22:59:38 +0900

    refactor: 탭 한국어, 영어 설정

--- app/src/main/java/com/teamwss/websoso/ui/storage/model/StorageTabs.kt
+++ app/src/main/java/com/teamwss/websoso/ui/storage/model/StorageTabs.kt
@@ -1,10 +1,14 @@
 package com.teamwss.websoso.ui.storage.model
 
-enum class StorageTab(val position: Int, val title: String) {
-    INTEREST(0, "관심"),
-    WATCHING(1, "보는중"),
-    WATCHED(2, "봤어요"),
-    QUITTING(3, "하차");
+enum class StorageTab(
+    val position: Int,
+    val title: String,
+    val readStatus: String,
+) {
+    INTEREST(0, "관심", "INTEREST"),
+    WATCHING(1, "보는중", "WATCHING"),
+    WATCHED(2, "봤어요", "WATCHED"),
+    QUITTING(3, "하차", "QUITTING");
 
     companion object {
         fun fromPosition(position: Int): StorageTab {
