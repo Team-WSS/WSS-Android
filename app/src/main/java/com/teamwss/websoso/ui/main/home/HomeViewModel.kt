086e2d924c5a62035692c0aa0fb3116e1ab7507f
librarywon <librarywon@naver.com>
2024-09-25 16:38:33 +0900

    refactor: 비동기로 데이터를 받아올 수 있도록 개선

--- app/src/main/java/com/teamwss/websoso/ui/main/home/HomeViewModel.kt
+++ app/src/main/java/com/teamwss/websoso/ui/main/home/HomeViewModel.kt
@@ -4,12 +4,19 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.teamwss.websoso.data.model.PopularFeedsEntity
+import com.teamwss.websoso.data.model.PopularNovelsEntity
+import com.teamwss.websoso.data.model.RecommendedNovelsByUserTasteEntity
 import com.teamwss.websoso.data.model.UserInterestFeedMessage
-import com.teamwss.websoso.data.model.UserInterestFeedMessage.*
+import com.teamwss.websoso.data.model.UserInterestFeedMessage.NO_ASSOCIATED_FEEDS
+import com.teamwss.websoso.data.model.UserInterestFeedMessage.NO_INTEREST_NOVELS
+import com.teamwss.websoso.data.model.UserInterestFeedsEntity
 import com.teamwss.websoso.data.repository.FeedRepository
 import com.teamwss.websoso.data.repository.NovelRepository
 import com.teamwss.websoso.ui.main.home.model.HomeUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -27,89 +34,97 @@ class HomeViewModel @Inject constructor(
     }
 
     fun updateHomeData() {
-        updatePopularNovels()
-        updatePopularFeeds()
-        updateUserInterestFeeds()
-        updateRecommendedNovelsByUser()
-    }
-
-    private fun updatePopularNovels() {
         viewModelScope.launch {
-            runCatching {
-                novelRepository.fetchPopularNovels()
-            }.onSuccess { popularNovels ->
-                _uiState.value = uiState.value?.copy(
+            _uiState.value = _uiState.value?.copy(loading = true)
+
+            val result = runCatching {
+                listOf(
+                    async { novelRepository.fetchPopularNovels() },
+                    async { feedRepository.fetchPopularFeeds() },
+                    async { feedRepository.fetchUserInterestFeeds() },
+                    async { novelRepository.fetchRecommendedNovelsByUserTaste() }
+                ).awaitAll()
+            }
+
+            result.onSuccess { responses ->
+                val popularNovels = responses[0] as PopularNovelsEntity
+                val popularFeeds = responses[1] as PopularFeedsEntity
+                val userInterestFeeds = responses[2] as UserInterestFeedsEntity
+                val recommendedNovels = responses[3] as RecommendedNovelsByUserTasteEntity
+
+                _uiState.value = _uiState.value?.copy(
                     loading = false,
                     popularNovels = popularNovels.popularNovels,
-                )
-            }.onFailure {
-                _uiState.value = uiState.value?.copy(
-                    loading = false,
-                    error = true,
-                )
-            }
-        }
-    }
-
-    private fun updatePopularFeeds() {
-        viewModelScope.launch {
-            runCatching {
-                feedRepository.fetchPopularFeeds()
-            }.onSuccess { popularFeeds ->
-                _uiState.value = uiState.value?.copy(
-                    loading = false,
                     popularFeeds = popularFeeds.popularFeeds.chunked(3),
-                )
-            }.onFailure {
-                _uiState.value = uiState.value?.copy(
-                    loading = false,
-                    error = true,
-                )
-            }
-        }
-    }
-
-    private fun updateUserInterestFeeds() {
-        viewModelScope.launch {
-            runCatching {
-                feedRepository.fetchUserInterestFeeds()
-            }.onSuccess { userInterestFeeds ->
-                val isInterestNovel =
-                    when (UserInterestFeedMessage.fromMessage(userInterestFeeds.message)) {
-                        NO_ASSOCIATED_FEEDS -> true
-                        NO_INTEREST_NOVELS -> false
-                        else -> true
-                    }
-
-                _uiState.value = uiState.value?.copy(
-                    loading = false,
-                    isInterestNovel = isInterestNovel,
+                    isInterestNovel = isUserInterestedInNovels(userInterestFeeds.message),
                     userInterestFeeds = userInterestFeeds.userInterestFeeds,
+                    recommendedNovelsByUserTaste = recommendedNovels.tasteNovels
                 )
             }.onFailure {
-                _uiState.value = uiState.value?.copy(
+                _uiState.value = _uiState.value?.copy(
                     loading = false,
-                    error = true,
+                    error = true
                 )
             }
         }
     }
 
-    private fun updateRecommendedNovelsByUser() {
+    fun updateFeedData() {
         viewModelScope.launch {
-            runCatching {
-                novelRepository.fetchRecommendedNovelsByUserTaste()
-            }.onSuccess { recommendedNovelsByUserTaste ->
-                _uiState.value = uiState.value?.copy(
-                    loading = false,
-                    recommendedNovelsByUserTaste = recommendedNovelsByUserTaste.tasteNovels,
+            val result = runCatching {
+                listOf(
+                    async { feedRepository.fetchPopularFeeds() },
+                    async { feedRepository.fetchUserInterestFeeds() }
+                ).awaitAll()
+            }
+
+            result.onSuccess { responses ->
+                val popularFeeds = responses[0] as PopularFeedsEntity
+                val userInterestFeeds = responses[1] as UserInterestFeedsEntity
+
+                _uiState.value = _uiState.value?.copy(
+                    popularFeeds = popularFeeds.popularFeeds.chunked(3),
+                    isInterestNovel = isUserInterestedInNovels(userInterestFeeds.message),
+                    userInterestFeeds = userInterestFeeds.userInterestFeeds
                 )
             }.onFailure {
-                _uiState.value = uiState.value?.copy(
-                    loading = false,
-                    error = true,
+                _uiState.value = _uiState.value?.copy(
+                    error = true
                 )
             }
+        }
+    }
+
+    fun updateNovelData() {
+        viewModelScope.launch {
+            val result = runCatching {
+                listOf(
+                    async { novelRepository.fetchPopularNovels() },
+                    async { novelRepository.fetchRecommendedNovelsByUserTaste() }
+                ).awaitAll()
+            }
+
+            result.onSuccess { responses ->
+                val popularNovels = responses[0] as PopularNovelsEntity
+                val recommendedNovels = responses[1] as RecommendedNovelsByUserTasteEntity
+
+                _uiState.value = _uiState.value?.copy(
+                    popularNovels = popularNovels.popularNovels,
+                    recommendedNovelsByUserTaste = recommendedNovels.tasteNovels
+                )
+            }.onFailure {
+                _uiState.value = _uiState.value?.copy(
+                    error = true
+                )
+            }
+        }
+    }
+
+    private fun isUserInterestedInNovels(userInterestFeedMessage: String): Boolean {
+        return when (UserInterestFeedMessage.fromMessage(userInterestFeedMessage)) {
+            NO_ASSOCIATED_FEEDS -> true
+            NO_INTEREST_NOVELS -> false
+            else -> true
         }
     }
 }
