febb7fe166b9dcd9fd47f538e30d5d2e7222fc64
Son Myeongji <114990782+m6z1@users.noreply.github.com>
2024-09-25 17:38:51 +0900

    feat: 팝업 메뉴 구현

--- app/src/main/java/com/teamwss/websoso/ui/otherUserPage/OtherUserPageActivity.kt
+++ app/src/main/java/com/teamwss/websoso/ui/otherUserPage/OtherUserPageActivity.kt
@@ -1,34 +1,53 @@
 package com.teamwss.websoso.ui.otherUserPage
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import android.widget.PopupWindow
 import androidx.activity.viewModels
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import com.google.android.material.tabs.TabLayoutMediator
 import com.teamwss.websoso.R
 import com.teamwss.websoso.common.ui.base.BaseActivity
+import com.teamwss.websoso.common.util.SingleEventHandler
+import com.teamwss.websoso.common.util.toFloatPxFromDp
+import com.teamwss.websoso.common.util.toIntPxFromDp
 import com.teamwss.websoso.databinding.ActivityOtherUserPageBinding
+import com.teamwss.websoso.databinding.MenuOtherUserPagePopupBinding
 import com.teamwss.websoso.ui.otherUserPage.adapter.OtherUserPageViewPagerAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class OtherUserPageActivity :
     BaseActivity<ActivityOtherUserPageBinding>(R.layout.activity_other_user_page) {
+    private var _popupBinding: MenuOtherUserPagePopupBinding? = null
+    private val popupBinding: MenuOtherUserPagePopupBinding
+        get() = _popupBinding ?: error("OtherUserPageActivity")
     private val otherUserPageViewModel: OtherUserPageViewModel by viewModels()
     private val viewPagerAdapter: OtherUserPageViewPagerAdapter by lazy {
         OtherUserPageViewPagerAdapter(this)
     }
+    private val singleEventHandler: SingleEventHandler by lazy { SingleEventHandler.from() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         bindViewModel()
         setUpViewPager()
         setUpItemVisibilityOnToolBar()
+        onBackButtonClick()
+        onMoreButtonClick()
     }
 
     private fun bindViewModel() {
         binding.otherUserPageViewModel = otherUserPageViewModel
         binding.lifecycleOwner = this
+        _popupBinding = MenuOtherUserPagePopupBinding.inflate(layoutInflater)
+        popupBinding.lifecycleOwner = this
+        popupBinding.onBlockedUser = otherUserPageViewModel::updateBlockedUser
     }
 
     private fun setUpViewPager() {
@@ -65,7 +84,43 @@ class OtherUserPageActivity :
         }
     }
 
+    private fun onBackButtonClick() {
+        binding.ivOtherUserPageStickyBack.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun onMoreButtonClick() {
+        binding.ivOtherUserPageStickyKebab.setOnClickListener {
+            singleEventHandler.throttleFirst {
+                showMenu(binding.ivOtherUserPageStickyKebab)
+            }
+        }
+    }
+
+    private fun showMenu(view: View) {
+        val popupWindow: PopupWindow = PopupWindow(
+            popupBinding.root,
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            true,
+        ).apply {
+            elevation = 2f.toFloatPxFromDp()
+            showAsDropDown(
+                view,
+                POPUP_MARGIN_END.toIntPxFromDp(),
+                POPUP_MARGIN_TOP.toIntPxFromDp(),
+            )
+        }
+    }
+
     companion object {
         private const val TOOLBAR_COLLAPSE_THRESHOLD = 0f
+        private const val POPUP_MARGIN_END = -80
+        private const val POPUP_MARGIN_TOP = 0
+
+        fun getIntent(context: Context): Intent {
+            return Intent(context, OtherUserPageActivity::class.java)
+        }
     }
 }
