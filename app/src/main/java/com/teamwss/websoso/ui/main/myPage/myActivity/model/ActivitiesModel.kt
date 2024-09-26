78fc6ad5dc154319155e070672a764db31fa0852
yeonjeen <jeendev2019@gmail.com>
2024-09-27 04:52:24 +0900

    refactor: 활동모델 클래스명 변경

---
+++ app/src/main/java/com/teamwss/websoso/ui/main/myPage/myActivity/model/ActivitiesModel.kt
@@ -0,0 +1,48 @@
+package com.teamwss.websoso.ui.main.myPage.myActivity.model
+
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+data class ActivitiesModel(
+    val isLoadable: Boolean,
+    val feeds: List<ActivityModel>,
+) {
+    data class ActivityModel(
+        val feedId: Long,
+        val isSpoiler: Boolean,
+        val feedContent: String,
+        val createdDate: String,
+        val isModified: Boolean,
+        val isLiked: Boolean,
+        val likeCount: Int,
+        val commentCount: Int,
+        val novelId: Long?,
+        val title: String?,
+        val novelRatingCount: Int?,
+        val novelRating: Float?,
+        val relevantCategories: String?,
+    ) {
+        val formattedScore: String
+            get() = String.format(
+                "%s (%,d)",
+                novelRating?.takeIf { it != null } ?: 0.0f,
+                novelRatingCount?.takeIf { it != null } ?: 0
+            )
+
+        companion object {
+            fun translateGenres(relevantCategories: List<String>): String {
+                return relevantCategories.joinToString(", ") { category ->
+                    Genres.from(category)?.korean ?: category
+                }
+            }
+
+            fun formatDate(inputDate: String): String {
+                val inputFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+                val outputFormat = SimpleDateFormat("M월 d일", Locale.getDefault())
+
+                val date = inputFormat.parse(inputDate)
+                return date?.let { outputFormat.format(it) } ?: "Invalid Date"
+            }
+        }
+    }
+}
