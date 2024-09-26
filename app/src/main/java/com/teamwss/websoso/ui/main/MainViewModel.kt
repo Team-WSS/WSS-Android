710c34d85d2b6184a33e705f60c47ab5aa580e18
librarywon <librarywon@naver.com>
2024-09-27 03:30:16 +0900

    feat: userInfo api 연동 로직 구현

--- app/src/main/java/com/teamwss/websoso/ui/main/MainViewModel.kt
+++ app/src/main/java/com/teamwss/websoso/ui/main/MainViewModel.kt
@@ -3,13 +3,44 @@ package com.teamwss.websoso.ui.main
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.teamwss.websoso.data.repository.UserRepository
 import com.teamwss.websoso.ui.main.model.MainUiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class MainViewModel: ViewModel() {
-    private val _mainUiState : MutableLiveData<MainUiState> = MutableLiveData(MainUiState())
-    val mainUiState : LiveData<MainUiState> get() = _mainUiState
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val userRepository: UserRepository,
+) : ViewModel() {
+    private val _mainUiState: MutableLiveData<MainUiState> = MutableLiveData(MainUiState())
+    val mainUiState: LiveData<MainUiState> get() = _mainUiState
 
-    fun updateNickname(){
-        // TODO nickname 가져오는 서버통신
+    init {
+        updateUserInfo()
+    }
+
+    fun updateUserInfo() {
+        if (mainUiState.value?.isLogin == true) {
+            viewModelScope.launch {
+                _mainUiState.value = mainUiState.value?.copy(
+                    loading = true
+                )
+                runCatching {
+                    userRepository.fetchUserInfo()
+                }.onSuccess { userInfo ->
+                    _mainUiState.value = mainUiState.value?.copy(
+                        nickname = userInfo.nickname,
+                        loading = false
+                    )
+                }.onFailure {
+                    _mainUiState.value = mainUiState.value?.copy(
+                        error = true,
+                        loading = false
+                    )
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/teamwss/websoso/ui/main/model/MainUiState.kt
+++ app/src/main/java/com/teamwss/websoso/ui/main/model/MainUiState.kt
@@ -1,7 +1,8 @@
 package com.teamwss.websoso.ui.main.model
 
 data class MainUiState(
+    val loading: Boolean = false,
+    val error: Boolean = false,
     val isLogin: Boolean = true,
-    val userId: Long = 0,
     val nickname: String = "웹소소",
 )
