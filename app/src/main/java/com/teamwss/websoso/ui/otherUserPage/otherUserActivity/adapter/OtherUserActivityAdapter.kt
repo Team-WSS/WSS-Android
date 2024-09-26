feecab255cb387b9f55c7ee83a60e0b9077572b4
yeonjeen <jeendev2019@gmail.com>
2024-09-27 06:45:43 +0900

    refactor: 활동모델 클래스명 변경

--- app/src/main/java/com/teamwss/websoso/ui/otherUserPage/otherUserActivity/adapter/OtherUserActivityAdapter.kt
+++ app/src/main/java/com/teamwss/websoso/ui/otherUserPage/otherUserActivity/adapter/OtherUserActivityAdapter.kt
@@ -3,7 +3,7 @@ package com.teamwss.websoso.ui.otherUserPage.otherUserActivity.adapter
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
-import com.teamwss.websoso.ui.main.myPage.myActivity.model.ActivityModel
+import com.teamwss.websoso.ui.main.myPage.myActivity.model.ActivitiesModel.ActivityModel
 
 class OtherUserActivityAdapter :
     ListAdapter<ActivityModel, OtherUserActivityViewHolder>(diffCallback) {
