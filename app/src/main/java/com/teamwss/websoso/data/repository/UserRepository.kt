6e44b84e46b2111f95768a8afa601f4f5cb281a1
librarywon <librarywon@naver.com>
2024-09-28 12:07:18 +0900

    refactor: 로그 import 제거

--- app/src/main/java/com/teamwss/websoso/data/repository/UserRepository.kt
+++ app/src/main/java/com/teamwss/websoso/data/repository/UserRepository.kt
@@ -1,6 +1,5 @@
 package com.teamwss.websoso.data.repository
 
-import android.util.Log
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
