086166e22044b15fa26c1e6ba0162f45e74515f6
yeonjeen <jeendev2019@gmail.com>
2024-10-04 19:37:01 +0900

    feat: 유저피드 api 연결

--- app/src/main/java/com/teamwss/websoso/ui/activityDetail/ActivityDetailViewModel.kt
+++ app/src/main/java/com/teamwss/websoso/ui/activityDetail/ActivityDetailViewModel.kt
@@ -3,34 +3,48 @@ package com.teamwss.websoso.ui.activityDetail
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.teamwss.websoso.data.repository.UserFeedRepository
+import androidx.lifecycle.viewModelScope
+import com.teamwss.websoso.data.repository.UserRepository
 import com.teamwss.websoso.ui.main.myPage.myActivity.model.ActivitiesModel.ActivityModel
+import com.teamwss.websoso.ui.mapper.toUi
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ActivityDetailViewModel @Inject constructor(
-    private val activityRepository: UserFeedRepository,
+    private val userRepository: UserRepository,
 ) : ViewModel() {
 
     private val _userActivity = MutableLiveData<List<ActivityModel>>()
     val userActivity: LiveData<List<ActivityModel>> get() = _userActivity
 
-    init {
-//        updateMyActivities()
+    private val _userId: MutableLiveData<Long> = MutableLiveData()
+    val userId: LiveData<Long> get() = _userId
+
+    private val _lastFeedId = MutableLiveData<Long>().apply { value = 0L }
+    val lastFeedId: LiveData<Long> get() = _lastFeedId
+
+    private val size: Int = 10
+
+    fun updateUserId(userId: Long) {
+        _userId.value = userId
+        updateUserFeeds(userId)
     }
 
-//    private fun updateMyActivities() {
-//        viewModelScope.launch {
-//            runCatching {
-//                activityRepository.getUserFeed()
-//            }.mapCatching { activities ->
-//                activities.toUi().take(5)
-//            }.onSuccess { mappedActivities ->
-//                _userActivity.value = mappedActivities
-//            }.onFailure { exception ->
-//
-//            }
-//        }
-//    }
+    private fun updateUserFeeds(userId: Long) {
+        viewModelScope.launch {
+            runCatching {
+                userRepository.fetchUserFeeds(
+                    userId = userId,
+                    lastFeedId = lastFeedId.value ?: 0L,
+                    size = size,
+                )
+            }.onSuccess { response ->
+                _userActivity.value = response.feeds.map { it.toUi() }
+                _lastFeedId.value = response.feeds.lastOrNull()?.feedId?.toLong() ?: 0L
+            }.onFailure {
+            }
+        }
+    }
 }
