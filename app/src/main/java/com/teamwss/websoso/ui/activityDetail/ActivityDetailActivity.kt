7d5fb072bf7d8119c8c56a82b538b53691c3448a
yeonjeen <jeendev2019@gmail.com>
2024-10-04 19:35:16 +0900

    feat: 활동디테일 분기처리 구현

--- app/src/main/java/com/teamwss/websoso/ui/activityDetail/ActivityDetailActivity.kt
+++ app/src/main/java/com/teamwss/websoso/ui/activityDetail/ActivityDetailActivity.kt
@@ -8,8 +8,10 @@ import com.teamwss.websoso.R
 import com.teamwss.websoso.common.ui.base.BaseActivity
 import com.teamwss.websoso.databinding.ActivityActivityDetailBinding
 import com.teamwss.websoso.ui.activityDetail.adapter.ActivityDetailAdapter
+import com.teamwss.websoso.ui.main.myPage.MyPageViewModel
 import com.teamwss.websoso.ui.main.myPage.myActivity.MyActivityFragment
-import com.teamwss.websoso.ui.otherUserPage.otherUserActivity.OtherUserActivityFragment
+import com.teamwss.websoso.ui.mapper.toUserProfileModel
+import com.teamwss.websoso.ui.otherUserPage.OtherUserPageViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -19,13 +21,30 @@ class ActivityDetailActivity :
     private val activityDetailAdapter: ActivityDetailAdapter by lazy {
         ActivityDetailAdapter()
     }
+    private val myPageViewModel: MyPageViewModel by viewModels()
+    private val otherUserPageViewModel: OtherUserPageViewModel by viewModels()
+
+    private lateinit var source: String
+    private var userId: Long = -1L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setupUserAndSource()
         setActivityTitle()
         setupMyActivitiesDetailAdapter()
-        setUpObserve()
+        setUpObserve(source)
         onBackButtonClick()
+    }
+
+    private fun setupUserAndSource() {
+        userId = intent.getLongExtra(EXTRA_USER_ID, -1L)
+        source = intent.getStringExtra(MyActivityFragment.EXTRA_SOURCE) ?: ""
+
+        if (source == SOURCE_OTHER_USER_ACTIVITY) {
+            otherUserPageViewModel.updateUserId(userId)
+        }
+
+        activityDetailViewModel.updateUserId(userId)
     }
 
     private fun setActivityTitle() {
@@ -34,8 +53,8 @@ class ActivityDetailActivity :
         var title: String = ""
 
         when (source) {
-            MyActivityFragment.SOURCE_MY_ACTIVITY -> title = getString(R.string.my_activity_detail_title)
-            OtherUserActivityFragment.SOURCE_OTHER_USER_ACTIVITY -> title = getString(R.string.other_user_page_activity)
+            SOURCE_MY_ACTIVITY -> title = getString(R.string.my_activity_detail_title)
+            SOURCE_OTHER_USER_ACTIVITY -> title = getString(R.string.other_user_page_activity)
         }
         binding.tvActivityDetailTitle.text = title
     }
@@ -46,9 +65,27 @@ class ActivityDetailActivity :
         }
     }
 
-    private fun setUpObserve() {
+    private fun setUpObserve(source: String?) {
         activityDetailViewModel.userActivity.observe(this) { activities ->
             activityDetailAdapter.submitList(activities)
+        }
+
+        when (source) {
+            SOURCE_MY_ACTIVITY -> {
+                myPageViewModel.myPageUiState.observe(this) { uiState ->
+                    uiState.myProfile?.let { myProfile ->
+                        activityDetailAdapter.setUserProfile(myProfile.toUserProfileModel())
+                    }
+                }
+            }
+
+            SOURCE_OTHER_USER_ACTIVITY -> {
+                otherUserPageViewModel.otherUserProfile.observe(this) { otherUserProfile ->
+                    otherUserProfile?.let {
+                        activityDetailAdapter.setUserProfile(otherUserProfile.toUserProfileModel())
+                    }
+                }
+            }
         }
     }
 
@@ -59,6 +96,10 @@ class ActivityDetailActivity :
     }
 
     companion object {
+        const val EXTRA_USER_ID = "userId"
+        const val SOURCE_MY_ACTIVITY = "myActivity"
+        const val SOURCE_OTHER_USER_ACTIVITY = "otherUserActivity"
+
         fun getIntent(context: Context): Intent {
             return Intent(context, ActivityDetailActivity::class.java)
         }
