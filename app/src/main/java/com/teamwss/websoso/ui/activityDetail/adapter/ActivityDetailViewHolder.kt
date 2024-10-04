c1edd28046a8e5310965649d1329e7c43408be2a
yeonjeen <jeendev2019@gmail.com>
2024-10-04 19:36:26 +0900

    feat: s3이미지 설정

--- app/src/main/java/com/teamwss/websoso/ui/activityDetail/adapter/ActivityDetailViewHolder.kt
+++ app/src/main/java/com/teamwss/websoso/ui/activityDetail/adapter/ActivityDetailViewHolder.kt
@@ -3,15 +3,19 @@ package com.teamwss.websoso.ui.activityDetail.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.teamwss.websoso.common.util.getS3ImageUrl
 import com.teamwss.websoso.databinding.ItemMyActivityBinding
 import com.teamwss.websoso.ui.main.myPage.myActivity.model.ActivitiesModel.ActivityModel
+import com.teamwss.websoso.ui.main.myPage.myActivity.model.UserProfileModel
 
 class ActivityDetailViewHolder(private val binding: ItemMyActivityBinding) :
     RecyclerView.ViewHolder(binding.root) {
 
-    fun bind(activity: ActivityModel) {
+    fun bind(activity: ActivityModel, userProfile: UserProfileModel) {
         binding.activity = activity
-        binding.executePendingBindings()
+        binding.userProfile = userProfile.copy(
+            avatarImage = itemView.getS3ImageUrl(userProfile.avatarImage)
+        )
     }
 
     companion object {
