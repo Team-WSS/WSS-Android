801611b95426378aabfcb66ce468d9d1b650be23
librarywon <librarywon@naver.com>
2024-11-13 20:13:33 +0900

    refactor: 코드 포멧팅 반영

--- app/src/main/java/com/teamwss/websoso/ui/login/model/LoginUiState.kt
+++ app/src/main/java/com/teamwss/websoso/ui/login/model/LoginUiState.kt
@@ -2,7 +2,12 @@ package com.teamwss.websoso.ui.login.model
 
 sealed class LoginUiState {
     data object Idle : LoginUiState()
-    data object Loading: LoginUiState()
-    data class Success(val isRegistered: Boolean, val accessToken: String, val refreshToken: String) : LoginUiState()
+    data object Loading : LoginUiState()
+    data class Success(
+        val isRegistered: Boolean,
+        val accessToken: String,
+        val refreshToken: String,
+    ) : LoginUiState()
+
     data class Failure(val error: Throwable) : LoginUiState()
 }
