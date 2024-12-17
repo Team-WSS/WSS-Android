18db1a279e5241e3c62b800241d94987efa01e16
yeonjeen <jeendev2019@gmail.com>
2024-12-18 03:05:30 +0900

    feat: 최소버전 Entity 매핑

---
+++ app/src/main/java/com/into/websoso/data/mapper/VersionMapper.kt
@@ -0,0 +1,10 @@
+package com.into.websoso.data.mapper
+
+import com.into.websoso.data.model.MinimumVersionEntity
+import com.into.websoso.data.remote.response.MinimumVersionResponseDto
+
+fun MinimumVersionResponseDto.toData(): MinimumVersionEntity =
+    MinimumVersionEntity(
+        minimumVersion = minimumVersion,
+        updateDate = updateDate,
+    )
---
+++ app/src/main/java/com/into/websoso/data/model/MinimumVersionEntity.kt
@@ -0,0 +1,6 @@
+package com.into.websoso.data.model
+
+data class MinimumVersionEntity(
+    val minimumVersion: String,
+    val updateDate: String,
+)
