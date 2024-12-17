dce461b7d31fcdc15a77d96f84dee17ab00d3dae
librarywon <librarywon@naver.com>
2024-12-17 21:41:32 +0900

    feat: firebase,analytics 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("org.jetbrains.kotlin.jvm") version "1.9.0" apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
