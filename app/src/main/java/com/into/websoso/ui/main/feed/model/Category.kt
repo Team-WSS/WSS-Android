8c71fd78ed9de75328f5d785d6b68f0cd43352f2
librarywon <librarywon@naver.com>
2024-12-30 20:30:34 +0900

    feat: Category shortCode 추가

--- app/src/main/java/com/into/websoso/ui/main/feed/model/Category.kt
+++ app/src/main/java/com/into/websoso/ui/main/feed/model/Category.kt
@@ -3,18 +3,19 @@ package com.into.websoso.ui.main.feed.model
 enum class Category(
     val krTitle: String,
     val enTitle: String,
+    val shortCode: String,
 ) {
-    ALL("전체", "all"),
-    ROMANCE("로맨스", "romance"),
-    ROMANCE_FANTASY("로판", "romanceFantasy"),
-    BOYS_LOVE("BL", "BL"),
-    FANTASY("판타지", "fantasy"),
-    MODERN_FANTASY("현판", "modernFantasy"),
-    WUXIA("무협", "wuxia"),
-    DRAMA("드라마", "drama"),
-    MYSTERY("미스터리", "mystery"),
-    LIGHT_NOVEL("라노벨", "lightNovel"),
-    ETC("기타", "etc")
+    ALL("전체", "all","all"),
+    ROMANCE("로맨스", "romance","R"),
+    ROMANCE_FANTASY("로판", "romanceFantasy","RF"),
+    BOYS_LOVE("BL", "BL","BL"),
+    FANTASY("판타지", "fantasy","F"),
+    MODERN_FANTASY("현판", "modernFantasy","HF"),
+    WUXIA("무협", "wuxia","MH"),
+    DRAMA("드라마", "drama","D"),
+    MYSTERY("미스터리", "mystery","M"),
+    LIGHT_NOVEL("라노벨", "lightNovel","LN"),
+    ETC("기타", "etc","etc")
     ;
 
     companion object {
