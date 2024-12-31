ae5d7eed066c2189db61a17076d80ce906f5fc7a
librarywon <librarywon@naver.com>
2024-12-31 15:52:11 +0900

    refactor: 코드 포멧팅 수정

--- app/src/main/java/com/into/websoso/ui/main/feed/model/Category.kt
+++ app/src/main/java/com/into/websoso/ui/main/feed/model/Category.kt
@@ -5,17 +5,17 @@ enum class Category(
     val enTitle: String,
     val shortCode: String,
 ) {
-    ALL("전체", "all","all"),
-    ROMANCE("로맨스", "romance","R"),
-    ROMANCE_FANTASY("로판", "romanceFantasy","RF"),
-    BOYS_LOVE("BL", "BL","BL"),
-    FANTASY("판타지", "fantasy","F"),
-    MODERN_FANTASY("현판", "modernFantasy","HF"),
-    WUXIA("무협", "wuxia","MH"),
-    DRAMA("드라마", "drama","D"),
-    MYSTERY("미스터리", "mystery","M"),
-    LIGHT_NOVEL("라노벨", "lightNovel","LN"),
-    ETC("기타", "etc","etc")
+    ALL("전체", "all", "all"),
+    ROMANCE("로맨스", "romance", "R"),
+    ROMANCE_FANTASY("로판", "romanceFantasy", "RF"),
+    BOYS_LOVE("BL", "BL", "BL"),
+    FANTASY("판타지", "fantasy", "F"),
+    MODERN_FANTASY("현판", "modernFantasy", "HF"),
+    WUXIA("무협", "wuxia", "MH"),
+    DRAMA("드라마", "drama", "D"),
+    MYSTERY("미스터리", "mystery", "M"),
+    LIGHT_NOVEL("라노벨", "lightNovel", "LN"),
+    ETC("기타", "etc", "etc")
     ;
 
     companion object {
