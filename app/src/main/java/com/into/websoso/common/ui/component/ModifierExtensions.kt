49a0a8081d0c517ca24162fa67535e49080d2cc4
junseo511 <prime54@g.skku.edu>
2025-01-14 18:42:22 +0900

    refactor: 불필요한 인자 제거

--- app/src/main/java/com/into/websoso/common/ui/component/ModifierExtensions.kt
+++ app/src/main/java/com/into/websoso/common/ui/component/ModifierExtensions.kt
@@ -3,17 +3,18 @@ package com.into.websoso.common.ui.component
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 
 @Composable
 fun Modifier.clickableWithoutRipple(
     enabled: Boolean = true,
-    interactionSource: MutableInteractionSource = MutableInteractionSource(),
     onClick: () -> Unit,
 ): Modifier {
+    val mutableInteractionSource = remember { MutableInteractionSource() }
     return this.clickable(
         enabled = enabled,
-        interactionSource = interactionSource,
+        interactionSource = mutableInteractionSource,
         indication = null,
         onClick = onClick,
     )
