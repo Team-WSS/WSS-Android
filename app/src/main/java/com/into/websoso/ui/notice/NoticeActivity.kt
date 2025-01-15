4572c8983ec1be814a4595b32704010ad68725a9
junseo511 <prime54@g.skku.edu>
2025-01-15 18:01:57 +0900

    refactor: 기존 공지 뷰 제거

--- app/src/main/java/com/into/websoso/ui/notice/NoticeActivity.kt
+++ app/src/main/java/com/into/websoso/ui/notice/NoticeActivity.kt
@@ -3,65 +3,22 @@ package com.into.websoso.ui.notice
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.viewModels
-import com.into.websoso.R
-import com.into.websoso.common.ui.base.BaseActivity
-import com.into.websoso.databinding.ActivityNoticeBinding
-import com.into.websoso.ui.main.MainActivity
-import com.into.websoso.ui.notice.adapter.NoticeAdapter
-import com.into.websoso.ui.notice.model.NoticeModel
-import com.into.websoso.ui.noticeDetail.NoticeDetailActivity
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class NoticeActivity : BaseActivity<ActivityNoticeBinding>(R.layout.activity_notice) {
-
-    private val noticeViewModel: NoticeViewModel by viewModels()
-
-    private val noticeAdapter: NoticeAdapter by lazy {
-        NoticeAdapter(::navigateToNoticeDetail)
-    }
-
-    private fun navigateToNoticeDetail(notice: NoticeModel) =
-        startActivity(NoticeDetailActivity.getIntent(this, notice))
+class NoticeActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setupClickListeners()
-        setupAdapter()
-        setupObserver()
-    }
+        setContent {
 
-    private fun setupClickListeners() {
-        binding.ivNoticeBack.setOnClickListener {
-            navigateToMainActivity()
-        }
-    }
-
-    private fun navigateToMainActivity() {
-        startActivity(MainActivity.getIntent(context = this, isLogin = true))
-    }
-
-    private fun setupAdapter() {
-        binding.rvNotice.adapter = noticeAdapter
-    }
-
-    private fun setupObserver() {
-        noticeViewModel.noticeUiState.observe(this) { noticeUiState ->
-            when {
-                noticeUiState.loading -> binding.wllNotice.setLoadingLayoutVisibility(true)
-                noticeUiState.error -> binding.wllNotice.setLoadingLayoutVisibility(false)
-                !noticeUiState.loading -> {
-                    binding.wllNotice.setWebsosoLoadingVisibility(false)
-                    noticeAdapter.submitList(noticeUiState.notices)
-                }
-            }
         }
     }
 
     companion object {
-
         fun getIntent(context: Context): Intent {
             return Intent(context, NoticeActivity::class.java)
         }
