dd5f8f6c453109efc29135f065acf9ba8802d158
junseo511 <prime54@g.skku.edu>
2025-01-16 03:15:28 +0900

    refactor: 문자열 리소스 추출

--- app/src/main/java/com/into/websoso/ui/notice/component/NoticeAppBar.kt
+++ app/src/main/java/com/into/websoso/ui/notice/component/NoticeAppBar.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.into.websoso.R
@@ -42,7 +43,7 @@ fun NoticeAppBar() {
                 .size(44.dp),
         )
         Text(
-            text = "알림",
+            text = stringResource(R.string.notice_notification),
             style = WebsosoTheme.typography.title2,
             color = Black,
             textAlign = TextAlign.Center,
