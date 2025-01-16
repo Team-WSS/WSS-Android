0a887545f26390504c0cf2ddad4edf31c6dcf9b2
junseo511 <prime54@g.skku.edu>
2025-01-17 04:43:59 +0900

    feat: 공지 읽음 여부 구현

--- app/src/main/java/com/into/websoso/data/remote/api/NoticeApi.kt
+++ app/src/main/java/com/into/websoso/data/remote/api/NoticeApi.kt
@@ -1,14 +1,17 @@
 package com.into.websoso.data.remote.api
 
+import com.into.websoso.data.remote.response.NotificationUnreadResponseDto
 import com.into.websoso.data.remote.response.NotificationsResponseDto
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface NoticeApi {
-
     @GET("notifications")
     suspend fun getNotices(
         @Query("lastNotificationId") lastNotificationId: Long,
         @Query("size") size: Int,
     ): NotificationsResponseDto
+
+    @GET("notifications/unread")
+    suspend fun getNoticeUnread(): NotificationUnreadResponseDto
 }
