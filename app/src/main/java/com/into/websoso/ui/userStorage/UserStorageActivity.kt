a462c8a246d98fdb0cf0acc73378f3545668baa6
yeonjeen <jeendev2019@gmail.com>
2025-01-24 23:50:59 +0900

    refactor: 린트해결

--- app/src/main/java/com/into/websoso/ui/userStorage/UserStorageActivity.kt
+++ app/src/main/java/com/into/websoso/ui/userStorage/UserStorageActivity.kt
@@ -192,12 +192,10 @@ class UserStorageActivity : BaseActivity<ActivityStorageBinding>(R.layout.activi
             source: String,
             userId: Long,
             readStatus: String = StorageTab.INTEREST.readStatus,
-        ): Intent {
-            return Intent(context, UserStorageActivity::class.java).apply {
-                putExtra(EXTRA_SOURCE, source)
-                putExtra(USER_ID_KEY, userId)
-                putExtra(READ_STATUS, readStatus)
-            }
+        ): Intent = Intent(context, UserStorageActivity::class.java).apply {
+            putExtra(EXTRA_SOURCE, source)
+            putExtra(USER_ID_KEY, userId)
+            putExtra(READ_STATUS, readStatus)
         }
     }
 }
