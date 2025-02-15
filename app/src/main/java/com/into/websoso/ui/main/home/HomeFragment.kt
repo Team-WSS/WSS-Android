cbc7f0dade1babbf34f303cdcb46d461e77e61b4
m6z1 <sonmyungj1zz@gmail.com>
2025-02-15 17:11:31 +0900

    feat: 홈 화면 진입 시 fcm토큰 로컬 토큰 값과 비교하는 로직 추가

--- app/src/main/java/com/into/websoso/ui/main/home/HomeFragment.kt
+++ app/src/main/java/com/into/websoso/ui/main/home/HomeFragment.kt
@@ -74,7 +74,7 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     }
 
     private val requestPermissionLauncher =
-        registerForActivityResult(ActivityResultContracts.RequestPermission()) { updateFCMToken() }
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { updateFCMToken(true) }
 
     override fun onViewCreated(
         view: View,
@@ -152,7 +152,9 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
         homeViewModel.isNotificationPermissionFirstLaunched.observe(viewLifecycleOwner) { isFirstLaunch ->
             if (isFirstLaunch) {
                 showNotificationPermissionDialog()
+                return@observe
             }
+            updateFCMToken(isFirstLaunch = false)
         }
     }
 
@@ -209,15 +211,19 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
             homeViewModel.updateIsNotificationPermissionFirstLaunched(false)
             return
         }
-        updateFCMToken()
+        updateFCMToken(true)
         homeViewModel.updateIsNotificationPermissionFirstLaunched(false)
     }
 
-    private fun updateFCMToken() {
+    private fun updateFCMToken(isFirstLaunch: Boolean) {
         FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
             val token = task.result
 
             if (task.isSuccessful) {
+                if (isFirstLaunch) {
+                    homeViewModel.saveFCMToken(token)
+                    return@addOnCompleteListener
+                }
                 homeViewModel.updateFCMToken(token)
             }
         }
