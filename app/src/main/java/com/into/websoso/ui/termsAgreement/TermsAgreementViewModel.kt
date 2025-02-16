ad8ed4d79aac8782707e678018c8577cb5cbfc4c
yeonjeen <jeendev2019@gmail.com>
2025-02-16 11:52:03 +0900

    refactor: 린트해결

--- app/src/main/java/com/into/websoso/ui/termsAgreement/TermsAgreementViewModel.kt
+++ app/src/main/java/com/into/websoso/ui/termsAgreement/TermsAgreementViewModel.kt
@@ -17,66 +17,68 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class TermsAgreementViewModel @Inject constructor(
-    private val userRepository: UserRepository,
-) : ViewModel() {
-    private val _agreementStatus = MutableStateFlow(
-        mapOf(
-            AgreementType.SERVICE to false,
-            AgreementType.PRIVACY to false,
-            AgreementType.MARKETING to false,
-        ),
-    )
-    val agreementStatus: StateFlow<Map<AgreementType, Boolean>> = _agreementStatus.asStateFlow()
+class TermsAgreementViewModel
+    @Inject
+    constructor(
+        private val userRepository: UserRepository,
+    ) : ViewModel() {
+        private val _agreementStatus = MutableStateFlow(
+            mapOf(
+                AgreementType.SERVICE to false,
+                AgreementType.PRIVACY to false,
+                AgreementType.MARKETING to false,
+            ),
+        )
+        val agreementStatus: StateFlow<Map<AgreementType, Boolean>> = _agreementStatus.asStateFlow()
 
-    private val _isLoading = MutableStateFlow(false)
-    val isLoading: StateFlow<Boolean> = _isLoading.asStateFlow()
+        private val _isLoading = MutableStateFlow(false)
+        val isLoading: StateFlow<Boolean> = _isLoading.asStateFlow()
 
-    private val _saveAgreementResult = MutableStateFlow<Result<Unit>?>(null)
-    val saveAgreementResult: StateFlow<Result<Unit>?> = _saveAgreementResult.asStateFlow()
+        private val _saveAgreementResult = MutableStateFlow<Result<Unit>?>(null)
+        val saveAgreementResult: StateFlow<Result<Unit>?> = _saveAgreementResult.asStateFlow()
 
-    val isRequiredAgreementsChecked: StateFlow<Boolean> = agreementStatus
-        .map { isRequiredAgreementChecked(it) }
-        .stateIn(viewModelScope, SharingStarted.Lazily, false)
+        val isRequiredAgreementsChecked: StateFlow<Boolean> = agreementStatus
+            .map { isRequiredAgreementChecked(it) }
+            .stateIn(viewModelScope, SharingStarted.Lazily, false)
 
-    private fun isRequiredAgreementChecked(status: Map<AgreementType, Boolean>): Boolean {
-        return status[AgreementType.SERVICE] == true && status[AgreementType.PRIVACY] == true
-    }
+        private fun isRequiredAgreementChecked(status: Map<AgreementType, Boolean>): Boolean {
+            return status[AgreementType.SERVICE] == true && status[AgreementType.PRIVACY] == true
+        }
 
-    fun updateTermsAgreementsAll() {
-        val newStatus = _agreementStatus.value.values.any { !it }
-        _agreementStatus.update { it.mapValues { _ -> newStatus } }
-    }
+        fun updateTermsAgreementsAll() {
+            val newStatus = _agreementStatus.value.values.any { !it }
+            _agreementStatus.update { it.mapValues { _ -> newStatus } }
+        }
 
-    fun updateTermsAgreements(agreementType: AgreementType) {
-        _agreementStatus.value[agreementType]?.let { currentValue ->
-            _agreementStatus.value = _agreementStatus.value.toMutableMap().apply {
-                this[agreementType] = !currentValue
+        fun updateTermsAgreements(agreementType: AgreementType) {
+            _agreementStatus.value[agreementType]?.let { currentValue ->
+                _agreementStatus.value = _agreementStatus.value.toMutableMap().apply {
+                    this[agreementType] = !currentValue
+                }
             }
         }
-    }
 
-    fun saveTermsAgreements() {
-        if (!isRequiredAgreementsChecked.value) return
+        fun saveTermsAgreements() {
+            if (!isRequiredAgreementsChecked.value) return
 
-        viewModelScope.launch {
-            _isLoading.value = true
+            viewModelScope.launch {
+                _isLoading.value = true
 
-            val agreementRequest = TermsAgreementRequestDto(
-                serviceAgreed = _agreementStatus.value[AgreementType.SERVICE] == true,
-                privacyAgreed = _agreementStatus.value[AgreementType.PRIVACY] == true,
-                marketingAgreed = _agreementStatus.value[AgreementType.MARKETING] == true,
-            )
-
-            _saveAgreementResult.value = runCatching {
-                userRepository.saveTermsAgreements(
-                    agreementRequest.serviceAgreed,
-                    agreementRequest.privacyAgreed,
-                    agreementRequest.marketingAgreed,
+                val agreementRequest = TermsAgreementRequestDto(
+                    serviceAgreed = _agreementStatus.value[AgreementType.SERVICE] == true,
+                    privacyAgreed = _agreementStatus.value[AgreementType.PRIVACY] == true,
+                    marketingAgreed = _agreementStatus.value[AgreementType.MARKETING] == true,
                 )
-            }
 
-            _isLoading.value = false
+                _saveAgreementResult.value = runCatching {
+                    userRepository.saveTermsAgreements(
+                        agreementRequest.serviceAgreed,
+                        agreementRequest.privacyAgreed,
+                        agreementRequest.marketingAgreed,
+                    )
+                }
+
+                _isLoading.value = false
+            }
         }
     }
-}
