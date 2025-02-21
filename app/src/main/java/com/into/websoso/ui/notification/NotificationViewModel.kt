b84a7e618ebfa9085dd74b71c3ac821482b49b2e
junseo511 <prime54@g.skku.edu>
2025-02-21 23:47:05 +0900

    refactor: 인터페이스 변수 네이밍 롤백

--- app/src/main/java/com/into/websoso/ui/notification/NotificationViewModel.kt
+++ app/src/main/java/com/into/websoso/ui/notification/NotificationViewModel.kt
@@ -6,7 +6,7 @@ import com.into.websoso.domain.model.NotificationInfo
 import com.into.websoso.domain.usecase.GetNotificationListUseCase
 import com.into.websoso.ui.mapper.toUi
 import com.into.websoso.ui.notification.model.NotificationInfoModel
-import com.into.websoso.ui.notification.model.NotificationUIState
+import com.into.websoso.ui.notification.model.NotificationUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -19,8 +19,8 @@ class NotificationViewModel
     constructor(
         private val getNotificationListUseCase: GetNotificationListUseCase,
     ) : ViewModel() {
-        private val _notificationUIState: MutableStateFlow<NotificationUIState> = MutableStateFlow(NotificationUIState())
-        val notificationUiState: StateFlow<NotificationUIState> get() = _notificationUIState
+        private val _notificationUiState: MutableStateFlow<NotificationUiState> = MutableStateFlow(NotificationUiState())
+        val notificationUiState: StateFlow<NotificationUiState> get() = _notificationUiState
 
         init {
             updateNotifications()
@@ -41,7 +41,7 @@ class NotificationViewModel
         }
 
         private fun handleLoadingState() {
-            _notificationUIState.value = notificationUiState.value.copy(
+            _notificationUiState.value = notificationUiState.value.copy(
                 isLoading = true,
                 isError = false,
             )
@@ -49,7 +49,7 @@ class NotificationViewModel
 
         private fun handleSuccessState(notificationInfo: NotificationInfoModel) {
             val currentUiState = notificationUiState.value
-            _notificationUIState.value = currentUiState.copy(
+            _notificationUiState.value = currentUiState.copy(
                 isLoadable = notificationInfo.isLoadable,
                 isLoading = false,
                 isError = false,
@@ -60,14 +60,14 @@ class NotificationViewModel
 
         private fun handleFailureState() {
             val currentUiState = notificationUiState.value
-            _notificationUIState.value = currentUiState.copy(
+            _notificationUiState.value = currentUiState.copy(
                 isLoading = false,
                 isError = true,
             )
         }
 
         fun updateReadNotification(notificationId: Long) {
-            _notificationUIState.value = notificationUiState.value.copy(
+            _notificationUiState.value = notificationUiState.value.copy(
                 notifications = notificationUiState.value.notifications.map {
                     if (it.id == notificationId) it.copy(isRead = true) else it
                 },
