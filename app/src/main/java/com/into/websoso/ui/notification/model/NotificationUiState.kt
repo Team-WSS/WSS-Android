b63ef8fe571954e56e32eed6f728cce966e84245
junseo511 <prime54@g.skku.edu>
2025-02-21 23:47:05 +0900

    refactor: 인터페이스 변수 네이밍 롤백

--- app/src/main/java/com/into/websoso/ui/notification/model/NotificationUiState.kt
+++ app/src/main/java/com/into/websoso/ui/notification/model/NotificationUiState.kt
@@ -1,6 +1,6 @@
 package com.into.websoso.ui.notification.model
 
-data class NotificationUIState(
+data class NotificationUiState(
     val isLoadable: Boolean = true,
     val isLoading: Boolean = false,
     val isError: Boolean = false,
