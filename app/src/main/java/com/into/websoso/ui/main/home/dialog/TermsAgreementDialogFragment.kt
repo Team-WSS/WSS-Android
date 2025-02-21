daf1405cfbcba30f93069ee60ef014bd36c5843d
yeonjeen <jeendev2019@gmail.com>
2025-02-21 14:42:37 +0900

    refactor: 약관 동의 다이얼로그 닫힐 때 상태 초기화 추가

--- app/src/main/java/com/into/websoso/ui/main/home/dialog/TermsAgreementDialogFragment.kt
+++ app/src/main/java/com/into/websoso/ui/main/home/dialog/TermsAgreementDialogFragment.kt
@@ -1,5 +1,6 @@
 package com.into.websoso.ui.main.home.dialog
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.View
 import com.into.websoso.R
@@ -9,6 +10,8 @@ import com.into.websoso.ui.termsAgreement.TermsAgreementDialogBottomSheet
 
 class TermsAgreementDialogFragment :
     BaseDialogFragment<DialogTermsAgreementPopupMenuBinding>(R.layout.dialog_terms_agreement_popup_menu) {
+    private var isBottomSheetShown = false
+
     override fun onViewCreated(
         view: View,
         savedInstanceState: Bundle?,
@@ -26,9 +29,20 @@ class TermsAgreementDialogFragment :
     }
 
     private fun showTermsAgreementBottomSheet() {
-        TermsAgreementDialogBottomSheet
-            .newInstance(isFromHome = true)
-            .show(parentFragmentManager, "TermsAgreementDialogBottomSheet")
+        isBottomSheetShown = true
+
+        val bottomSheet = TermsAgreementDialogBottomSheet.newInstance(isFromHome = true)
+        bottomSheet.show(parentFragmentManager, "TermsAgreementDialogBottomSheet")
+
+        bottomSheet.onDismissListener = {
+            isBottomSheetShown = false
+        }
+        dismiss()
+    }
+
+    override fun onDismiss(dialog: DialogInterface) {
+        super.onDismiss(dialog)
+        isBottomSheetShown = false
     }
 
     companion object {
