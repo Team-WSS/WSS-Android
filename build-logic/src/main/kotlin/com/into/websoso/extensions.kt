f4400bec3b06e29f2fabb323c58c14737609f33b
s9hn <tnghqn@naver.com>
2025-04-02 18:43:02 +0900

    feat: 빌드스크립트 확장 함수 구현

--- build-logic/src/main/kotlin/com/into/websoso/extensions.kt
+++ build-logic/src/main/kotlin/com/into/websoso/extensions.kt
@@ -4,6 +4,7 @@ import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalog
 import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.api.plugins.ExtensionContainer
+import org.gradle.kotlin.dsl.DependencyHandlerScope
 import org.gradle.kotlin.dsl.getByType
 
 internal val ExtensionContainer.libs: VersionCatalog
@@ -11,3 +12,8 @@ internal val ExtensionContainer.libs: VersionCatalog
 
 internal val Project.libs: VersionCatalog
     get() = extensions.getByType<VersionCatalogsExtension>().named("libs")
+
+internal fun DependencyHandlerScope.implementation(dependencyNotation: String) {
+    val dependency = extensions.libs.findLibrary(dependencyNotation).get()
+    add("implementation", dependency)
+}
