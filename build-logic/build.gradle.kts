12bb6fc743eaa46600e0885ebe7488fc0cb99794
s9hn <tnghqn@naver.com>
2025-04-03 20:48:56 +0900

    feat: 빌드로직 모듈 그래들 세팅

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -1,9 +1,8 @@
 plugins {
-    id("java-library")
-    alias(libs.plugins.kotlin.jvm)
+    `kotlin-dsl`
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+dependencies {
+    implementation(libs.android.gradle.plugin)
+    implementation(libs.kotlin.gradle.plugin)
 }
