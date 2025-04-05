d9a0ca0e6ba9cadf378b9cd9b6177b866510d7d0
s9hn <tnghqn@naver.com>
2025-04-05 11:06:19 +0900

    refactor: hilt 모듈 그래들 세팅 수정

--- build-logic/src/main/kotlin/websoso.android.hilt.gradle.kts
+++ build-logic/src/main/kotlin/websoso.android.hilt.gradle.kts
@@ -1,12 +1,14 @@
-import com.into.websoso.libs
+import gradle.kotlin.dsl.accessors._80a2ae57395e1362b61311ead0eb480f.implementation
 
 plugins {
-    kotlin("kapt")
-    id("dagger.hilt.android.plugin")
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 dependencies {
-    "implementation"(libs.findLibrary("hilt.android").get())
-    "kapt"(libs.findLibrary("hilt.android.compiler").get())
-    "kaptAndroidTest"(libs.findLibrary("hilt.android.compiler").get())
+    // 기본 Hilt 라이브러리
+    implementation("com.google.dagger:hilt-android:2.51.1")
+
+    // Hilt 컴파일러 (Dagger 코드 생성)
+    add("kapt","com.google.dagger:hilt-compiler:2.51.1")
 }
