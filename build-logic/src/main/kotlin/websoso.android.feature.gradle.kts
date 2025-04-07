30294b97e8f0d9eb87a1d50621a7e0427b51d79b
s9hn <tnghqn@naver.com>
2025-04-07 19:03:40 +0900

    feat: android.feature 그래들 구현

---
+++ build-logic/src/main/kotlin/websoso.android.feature.gradle.kts
@@ -0,0 +1,10 @@
+import com.into.websoso.websosoDependencies
+
+plugins {
+    id("websoso.android.library")
+    id("websoso.android.compose")
+}
+
+websosoDependencies {
+    implementation(project(":core:resource"))
+}
