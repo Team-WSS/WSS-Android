cd00192f207d922c91a38bfd9f307d4dd043600d
yeonjeen <jeendev2019@gmail.com>
2025-04-13 16:30:06 +0900

    refactor: 불필요한 코드 제거

--- app/src/main/java/com/into/websoso/ui/userStorage/UserStorageActivity.kt
+++ app/src/main/java/com/into/websoso/ui/userStorage/UserStorageActivity.kt
@@ -14,8 +14,6 @@ import com.into.websoso.R
 import com.into.websoso.core.common.ui.base.BaseActivity
 import com.into.websoso.core.common.ui.model.ResultFrom
 import com.into.websoso.databinding.ActivityStorageBinding
-import com.into.websoso.ui.main.MainActivity
-import com.into.websoso.ui.main.myPage.myLibrary.MyLibraryFragment.Companion.EXTRA_SOURCE
 import com.into.websoso.ui.novelDetail.NovelDetailActivity
 import com.into.websoso.ui.userStorage.adapter.UserStorageViewPagerAdapter
 import com.into.websoso.ui.userStorage.model.StorageTab
@@ -48,15 +46,12 @@ class UserStorageActivity : BaseActivity<ActivityStorageBinding>(R.layout.activi
         setupInitialReadStatusTab()
         onBackButtonClick()
         onSortTypeButtonClick()
-        onExploreButton()
     }
 
     private fun setupUserId() {
-        val source = intent.getStringExtra(EXTRA_SOURCE) ?: SOURCE_MY_LIBRARY
-        val userId = intent.getLongExtra(USER_ID_KEY, UserStorageViewModel.DEFAULT_USER_ID)
+        val userId = intent.getLongExtra(USER_ID_KEY, DEFAULT_USER_ID)
         val readStatus = intent.getStringExtra(READ_STATUS) ?: StorageTab.INTEREST.readStatus
-
-        userStorageViewModel.updateUserStorage(source, userId, readStatus)
+        userStorageViewModel.updateUserStorage(userId, readStatus)
     }
 
     private fun bindViewModel() {
@@ -123,24 +118,9 @@ class UserStorageActivity : BaseActivity<ActivityStorageBinding>(R.layout.activi
 
             binding.clStorageNull.visibility =
                 if (uiState.userNovelCount == 0L) View.VISIBLE else View.GONE
+
             binding.vpStorage.visibility =
                 if (uiState.userNovelCount > 0L) View.VISIBLE else View.GONE
-
-            binding.btnStorageGoToSearchNovel.visibility =
-                if (uiState.userNovelCount == 0L && intent.getStringExtra(EXTRA_SOURCE) == SOURCE_MY_LIBRARY) {
-                    View.VISIBLE
-                } else {
-                    View.GONE
-                }
-        }
-
-        userStorageViewModel.isRatingChanged.observe(this) { isChanged ->
-            if (isChanged) {
-                val currentReadStatus =
-                    userStorageViewModel.uiState.value?.readStatus ?: StorageTab.INTEREST.readStatus
-                userStorageViewModel.updateReadStatus(currentReadStatus, forceLoad = true)
-                userStorageViewModel.updateRatingChanged()
-            }
         }
     }
 
@@ -148,16 +128,6 @@ class UserStorageActivity : BaseActivity<ActivityStorageBinding>(R.layout.activi
         if (uiState.storageNovels.isNotEmpty()) {
             userStorageAdapter.updateNovels(uiState.storageNovels)
         }
-    }
-
-    private fun onExploreButton() {
-        binding.btnStorageGoToSearchNovel.setOnClickListener {
-            navigateToExploreFragment()
-        }
-    }
-
-    private fun navigateToExploreFragment() {
-        startActivity(MainActivity.getIntent(this, MainActivity.FragmentType.EXPLORE))
     }
 
     private fun navigateToNovelDetail(novelId: Long) {
@@ -189,20 +159,14 @@ class UserStorageActivity : BaseActivity<ActivityStorageBinding>(R.layout.activi
 
     companion object {
         const val USER_ID_KEY = "userId"
-        const val SOURCE_MY_LIBRARY = "myLibrary"
-        const val SOURCE_OTHER_USER_LIBRARY = "otherUserLibrary"
         const val READ_STATUS = "read_status"
+        const val DEFAULT_USER_ID = 0L
 
         fun getIntent(
             context: Context,
-            source: String,
             userId: Long,
             readStatus: String = StorageTab.INTEREST.readStatus,
-        ) = Intent(
-            context,
-            UserStorageActivity::class.java,
-        ).apply {
-            putExtra(EXTRA_SOURCE, source)
+        ) = Intent(context, UserStorageActivity::class.java).apply {
             putExtra(USER_ID_KEY, userId)
             putExtra(READ_STATUS, readStatus)
         }
