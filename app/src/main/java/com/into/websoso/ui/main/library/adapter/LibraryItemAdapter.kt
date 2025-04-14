56566288fb2305dca80a3e837f22c6cdc4ba6fc2
yeonjeen <jeendev2019@gmail.com>
2025-04-15 01:04:44 +0900

    refactor: 린트 반영

--- app/src/main/java/com/into/websoso/ui/main/library/adapter/LibraryItemAdapter.kt
+++ app/src/main/java/com/into/websoso/ui/main/library/adapter/LibraryItemAdapter.kt
@@ -1,5 +1,6 @@
 package com.into.websoso.ui.main.library.adapter
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -10,23 +11,28 @@ class LibraryItemAdapter(
     private var novels: List<StorageNovelModel>,
     private val novelClickListener: (novelId: Long) -> Unit,
 ) : RecyclerView.Adapter<LibraryItemViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LibraryItemViewHolder {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int,
+    ): LibraryItemViewHolder {
         val binding =
             ItemStorageNovelBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return LibraryItemViewHolder(binding, novelClickListener)
     }
 
-    override fun onBindViewHolder(holder: LibraryItemViewHolder, position: Int) {
+    override fun onBindViewHolder(
+        holder: LibraryItemViewHolder,
+        position: Int,
+    ) {
         val novel = novels[position]
         holder.bind(novel)
     }
 
+    @SuppressLint("NotifyDataSetChanged")
     fun updateNovels(newNovels: List<StorageNovelModel>) {
         this.novels = newNovels
         notifyDataSetChanged()
     }
 
-    override fun getItemCount(): Int {
-        return novels.size
-    }
+    override fun getItemCount(): Int = novels.size
 }
