d1ca5d3a075e9f033b8ea13da14a34c8616f7063
s9hn <tnghqn@naver.com>
2025-04-27 11:21:00 +0000

    feat: collectAsEventWithLifecycle 구현

---
+++ core/common/src/main/java/com/into/websoso/core/common/lifecycle/compose/FlowExtensions.kt
@@ -0,0 +1,31 @@
+package com.into.websoso.core.common.lifecycle.compose
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
+
+@Composable
+fun <T> Flow<T>.collectAsEventWithLifecycle(
+    lifecycle: Lifecycle = LocalLifecycleOwner.current.lifecycle,
+    minActiveState: Lifecycle.State = Lifecycle.State.STARTED,
+    context: CoroutineContext = EmptyCoroutineContext,
+    onEvent: suspend (T) -> Unit,
+) {
+    LaunchedEffect(this, lifecycle, minActiveState, context) {
+        lifecycle.repeatOnLifecycle(minActiveState) {
+            if (context == EmptyCoroutineContext) {
+                this@collectAsEventWithLifecycle.collect { onEvent(it) }
+            } else {
+                withContext(context) {
+                    this@collectAsEventWithLifecycle.collect { onEvent(it) }
+                }
+            }
+        }
+    }
+}
