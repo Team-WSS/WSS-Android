6a9ccb7ede435d0d4767896fe8646c517713c021
s9hn <tnghqn@naver.com>
2025-05-07 15:43:27 +0900

    refactor: authIdentifier 제거 및 AuthPlatform 수정

--- core/auth/src/main/java/com/into/websoso/core/auth/AuthIdentifier.kt
+++
@@ -1,8 +0,0 @@
-package com.into.websoso.core.auth
-
-import dagger.MapKey
-
-@MapKey
-annotation class AuthPlatformKey(
-    val value: AuthPlatform,
-)
--- core/auth/src/main/java/com/into/websoso/core/auth/AuthPlatform.kt
+++ core/auth/src/main/java/com/into/websoso/core/auth/AuthPlatform.kt
@@ -1,5 +1,12 @@
 package com.into.websoso.core.auth
 
+import dagger.MapKey
+
+@MapKey
+annotation class AuthPlatformKey(
+    val value: AuthPlatform,
+)
+
 enum class AuthPlatform {
     KAKAO,
 }
