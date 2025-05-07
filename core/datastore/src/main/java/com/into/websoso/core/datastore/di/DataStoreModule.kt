566199861d53a6b3ebae9f47001c869faf690d1d
s9hn <tnghqn@naver.com>
2025-05-07 17:37:11 +0900

    feat: 데이터스토어 모듈 구현

---
+++ core/datastore/src/main/java/com/into/websoso/core/datastore/di/DataStoreModule.kt
@@ -0,0 +1,25 @@
+package com.into.websoso.core.datastore.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object DataStoreModule {
+    private const val ACCOUNT_DATASTORE = "ACCOUNT_DATASTORE"
+    private val Context.accountDataStore: DataStore<Preferences> by preferencesDataStore(name = ACCOUNT_DATASTORE)
+
+    @Singleton
+    @Provides
+    fun provideAccountPreferencesDataStore(
+        @ApplicationContext context: Context,
+    ): DataStore<Preferences> = context.accountDataStore
+}
