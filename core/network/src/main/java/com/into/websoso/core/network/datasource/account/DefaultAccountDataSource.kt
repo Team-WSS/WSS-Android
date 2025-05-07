8ff0f1e7c75b178dda155dc5d291bbc5884dae1a
s9hn <tnghqn@naver.com>
2025-05-07 16:52:45 +0900

    refactor: DefaultAccountDataSource AccountApi 주입받도록 수정

--- core/network/src/main/java/com/into/websoso/core/network/datasource/account/DefaultAccountDataSource.kt
+++ core/network/src/main/java/com/into/websoso/core/network/datasource/account/DefaultAccountDataSource.kt
@@ -3,19 +3,15 @@ package com.into.websoso.core.network.datasource.account
 import com.into.websoso.core.auth.AuthPlatform
 import com.into.websoso.core.auth.AuthToken
 import com.into.websoso.data.account.AccountRemoteDataSource
-import retrofit2.Retrofit
-import retrofit2.create
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class DefaultAccountDataSource
+internal class DefaultAccountDataSource
     @Inject
     constructor(
-        retrofit: Retrofit,
+        private val accountApi: AccountApi,
     ) : AccountRemoteDataSource {
-        private val accountApi by lazy { retrofit.create<AccountApi>() }
-
         override suspend fun postLogin(
             platform: AuthPlatform,
             authToken: AuthToken,
