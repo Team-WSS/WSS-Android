19a426b9c7e5a2e71d2e420141309bab9280b0b6
s9hn <tnghqn@naver.com>
2025-05-13 00:39:32 +0900

    feat: WebsosoApp subscribeSessionState 함수 구현

--- app/src/main/java/com/into/websoso/WebsosoApp.kt
+++ app/src/main/java/com/into/websoso/WebsosoApp.kt
@@ -2,16 +2,38 @@ package com.into.websoso
 
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.lifecycle.ProcessLifecycleOwner
 import com.into.websoso.BuildConfig.KAKAO_APP_KEY
+import com.into.websoso.core.auth.AuthSessionManager
+import com.into.websoso.core.auth.SessionState
+import com.into.websoso.core.common.navigator.NavigatorProvider
+import com.into.websoso.core.common.util.collectWithLifecycle
 import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
 class WebsosoApp : Application() {
+    @Inject
+    lateinit var sessionManager: AuthSessionManager
+
+    @Inject
+    lateinit var navigatorProvider: NavigatorProvider
+
     override fun onCreate() {
         super.onCreate()
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
+        subscribeSessionState()
         KakaoSdk.init(this, KAKAO_APP_KEY)
+    }
+
+    private fun subscribeSessionState() {
+        sessionManager.sessionState.collectWithLifecycle(ProcessLifecycleOwner.get()) { state ->
+            if (state is SessionState.Expired) {
+                sessionManager.clearSessionState()
+                navigatorProvider.navigateToLoginActivity()
+            }
+        }
     }
 }
