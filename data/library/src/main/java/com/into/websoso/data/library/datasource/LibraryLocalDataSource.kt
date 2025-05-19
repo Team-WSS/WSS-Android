e6c663923edc7ca5a1b1a04648f58177c133f307
s9hn <tnghqn@naver.com>
2025-05-19 21:06:20 +0900

    refactor: LibraryLocalDataSource 명세 재정의

--- data/library/src/main/java/com/into/websoso/data/library/datasource/LibraryLocalDataSource.kt
+++ data/library/src/main/java/com/into/websoso/data/library/datasource/LibraryLocalDataSource.kt
@@ -1,5 +1,22 @@
 package com.into.websoso.data.library.datasource
 
+import com.into.websoso.data.library.model.NovelEntity
+import kotlinx.coroutines.flow.Flow
+
 interface LibraryLocalDataSource {
-    suspend fun selectAllNovels()
+    suspend fun insertNovels(novels: List<NovelEntity>)
+
+    suspend fun insertNovel(novel: NovelEntity)
+
+    fun selectAllNovels(): Flow<List<NovelEntity>>
+
+    suspend fun selectNovel(novelId: Long): NovelEntity
+
+    suspend fun updateNovels(novels: List<NovelEntity>)
+
+    suspend fun updateNovel(novel: NovelEntity)
+
+    suspend fun deleteAllNovels()
+
+    suspend fun deleteNovel(novelId: Long)
 }
