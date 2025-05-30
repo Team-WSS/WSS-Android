1bfdb88a7dac09f534ee605385b570f5c0d02bdd
s9hn <tnghqn@naver.com>
2025-05-30 23:57:26 +0900

    build: 피쳐모듈 페이징 의존성 추가

--- feature/library/src/main/java/com/into/websoso/feature/library/LibraryViewModel.kt
+++ feature/library/src/main/java/com/into/websoso/feature/library/LibraryViewModel.kt
@@ -1,10 +1,17 @@
 package com.into.websoso.feature.library
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
+import com.into.websoso.data.library.LibraryRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class LibraryViewModel
     @Inject
-    constructor() : ViewModel()
+    constructor(
+//        private val libraryRepository: LibraryRepository,
+    ) : ViewModel() {
+//        val novels = libraryRepository.getUserLibrary().cachedIn(viewModelScope)
+    }
