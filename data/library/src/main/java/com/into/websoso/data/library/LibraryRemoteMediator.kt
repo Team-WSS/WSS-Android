c830ff1272c90e260d8d1f672511a0dee2842c70
s9hn <tnghqn@naver.com>
2025-07-02 17:49:27 +0900

    refactor: LibraryRemoteMediator 제거

--- data/library/src/main/java/com/into/websoso/data/library/LibraryRemoteMediator.kt
+++
@@ -1,8 +0,0 @@
-package com.into.websoso.data.library
-
-import com.into.websoso.data.library.model.NovelEntity
-import androidx.paging.RemoteMediator
-
-class LibraryRemoteMediator : RemoteMediator<Int, NovelEntity>() {
-
-}
