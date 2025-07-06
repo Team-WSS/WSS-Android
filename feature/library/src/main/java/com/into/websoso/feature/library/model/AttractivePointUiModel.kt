8f2bc051ea9f941960cb93d97e32ec93d02ae3e5
yeonjeen <jeendev2019@gmail.com>
2025-07-06 11:19:48 +0900

    feat: 매력포인트 ui 모델 구현

---
+++ feature/library/src/main/java/com/into/websoso/feature/library/model/AttractivePointUiModel.kt
@@ -0,0 +1,9 @@
+package com.into.websoso.feature.library.model
+
+import com.into.websoso.domain.library.model.AttractivePoints
+
+data class AttractivePointUiModel(
+    val type: AttractivePoints,
+    val label: String,
+    val key: String,
+)
