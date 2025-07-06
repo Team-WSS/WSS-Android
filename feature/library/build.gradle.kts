c46b8e70e1b65a05f61fd40c5b3f39d1f07f279a
yeonjeen <jeendev2019@gmail.com>
2025-07-06 11:42:10 +0900

    build: 의존성 추가

--- feature/library/build.gradle.kts
+++ feature/library/build.gradle.kts
@@ -9,8 +9,12 @@ android {
 }
 
 dependencies {
+    implementation(projects.data.library)
+    implementation(projects.core.resource)
+    implementation(projects.core.designsystem)
     implementation(projects.domain.library)
     implementation(projects.data.library)
 
     implementation("androidx.paging:paging-runtime:3.3.2")
+    implementation(libs.androidx.paging.compose.android)
 }
