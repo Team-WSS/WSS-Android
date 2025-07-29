aeefb22c045fbef16e714679c17b8f70f07e641e
s9hn <tnghqn@naver.com>
2025-07-29 18:04:38 +0900

    refactor: PagingSource 및 RemoteMediator 리팩터링

---
+++ data/library/src/main/java/com/into/websoso/data/library/paging/LibraryPagingSource.kt
@@ -0,0 +1,58 @@
+package com.into.websoso.data.library.paging
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.into.websoso.core.common.extensions.isCloseTo
+import com.into.websoso.data.filter.model.LibraryFilter
+import com.into.websoso.data.library.datasource.LibraryRemoteDataSource
+import com.into.websoso.data.library.model.NovelEntity
+
+class LibraryPagingSource(
+    private val userId: Long,
+    private val libraryRemoteDataSource: LibraryRemoteDataSource,
+    private val filterParams: LibraryFilter,
+) : PagingSource<Long, NovelEntity>() {
+    override suspend fun load(params: LoadParams<Long>): LoadResult<Long, NovelEntity> {
+        val lastUserNovelId = params.key ?: DEFAULT_LAST_USER_NOVEL_ID
+
+        return try {
+            val response = libraryRemoteDataSource.getUserNovels(
+                userId = userId,
+                lastUserNovelId = lastUserNovelId,
+                size = params.loadSize,
+                sortCriteria = filterParams.sortCriteria,
+                isInterest = if (!filterParams.isInterested) null else true,
+                readStatuses = filterParams.readStatusKeys.ifEmpty { null },
+                attractivePoints = filterParams.attractivePointKeys.ifEmpty { null },
+                novelRating = if (filterParams.novelRating.isCloseTo(0f)) null else filterParams.novelRating,
+                query = null,
+                updatedSince = null,
+            )
+
+            val novels = response.userNovels.map { it }
+
+            val nextKey = if (response.isLoadable && novels.isNotEmpty()) {
+                novels.last().userNovelId
+            } else {
+                null
+            }
+
+            LoadResult.Page(
+                data = novels,
+                prevKey = null,
+                nextKey = nextKey,
+            )
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Long, NovelEntity>): Long? =
+        state.anchorPosition?.let { position ->
+            state.closestItemToPosition(position)?.userNovelId
+        }
+
+    companion object {
+        private const val DEFAULT_LAST_USER_NOVEL_ID = 0L
+    }
+}
