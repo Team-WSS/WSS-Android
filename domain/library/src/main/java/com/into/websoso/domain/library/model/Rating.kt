920fe7588c11cada4df16b25fc6fa1629f8b75d3
s9hn <tnghqn@naver.com>
2025-08-11 18:35:39 +0900

    refactor: 평점 정상 반영

--- domain/library/src/main/java/com/into/websoso/domain/library/model/Rating.kt
+++ domain/library/src/main/java/com/into/websoso/domain/library/model/Rating.kt
@@ -6,6 +6,12 @@ enum class Rating(
     val value: Float,
 ) {
     DEFAULT(0.0f),
+    POINT_FIVE(0.5f),
+    ONE(1.0f),
+    ONE_POINT_FIVE(1.5f),
+    TWO(2.0f),
+    TWO_POINT_FIVE(2.5f),
+    THREE(3.0f),
     THREE_POINT_FIVE(3.5f),
     FOUR(4.0f),
     FOUR_POINT_FIVE(4.5f),
@@ -13,6 +19,9 @@ enum class Rating(
     ;
 
     companion object {
-        fun from(value: Float): Rating = entries.find { it.value.isCloseTo(value) } ?: DEFAULT
+        fun from(value: Float): Rating =
+            entries.find {
+                it.value.isCloseTo(value)
+            } ?: DEFAULT
     }
 }
