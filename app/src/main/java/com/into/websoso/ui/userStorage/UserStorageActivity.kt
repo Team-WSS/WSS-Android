1e6186c46d10aee630348d835238f9cc183e7c0f
s9hn <tnghqn@naver.com>
2025-08-11 19:13:17 +0900

    refactor: enableEdgeToEdge() 추가

--- app/src/main/java/com/into/websoso/ui/userStorage/UserStorageActivity.kt
+++ app/src/main/java/com/into/websoso/ui/userStorage/UserStorageActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.WindowInsets
@@ -29,7 +30,7 @@ class UserStorageActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
         setContent {
             WebsosoTheme {
                 Scaffold(
