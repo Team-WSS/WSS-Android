37cc2d656c961d1b40e40d80375187ac70cedf0d
s9hn <tnghqn@naver.com>
2026-01-10 11:34:46 +0900

    refactor: 디자인시스템 수정

--- core/designsystem/src/main/java/com/into/websoso/core/designsystem/theme/Color.kt
+++ core/designsystem/src/main/java/com/into/websoso/core/designsystem/theme/Color.kt
@@ -25,6 +25,34 @@ val GrayToast = Color(0xCC394258)
 val Black = Color(0xFF111118)
 val Black60 = Color(0x99000000)
 
+// Feed
+val LightGreen = Color(0xFFEFF3D6)
+val Green = Color(0xFFAEC23B)
+
+val LightOrange = Color(0xFFFDF2E3)
+val Orange = Color(0xFFB8896E)
+
+val LightPurple = Color(0xFFF0F1FF)
+val Purple = Color(0xFF6457FC)
+
+val LightPink = Color(0xFFFFF5FC)
+val Pink = Color(0xFFE586CA)
+
+val LightMint = Color(0xFFF0FAFD)
+val Mint = Color(0xFF21A6FF)
+
+val LightRed = Color(0xFFF9EBE9)
+val Red = Color(0xFFB72F58)
+
+val LightBlue = Color(0xFFE9F2FF)
+val Blue = Color(0xFF4767F7)
+
+val LightGray = Color(0xFFEEF0F7)
+val Gray = Color(0xFF5D78A0)
+
+val LightViolet = Color(0xFFF2E6F2)
+val Violet = Color(0xFF9560BF)
+
 // ETC
 val Transparent = Color(0x00000000)
 val HyperlinkBlue = Color(0xFF0645AD)
