b86d57ab2b15dc1b3d5857d925ecbfda939d5106
s9hn <tnghqn@naver.com>
2026-01-17 18:17:41 +0900

    feat: 홈화면 알림 권한 다이얼로그 구현

--- app/src/main/java/com/into/websoso/ui/notification/NotificationActivity.kt
+++ app/src/main/java/com/into/websoso/ui/notification/NotificationActivity.kt
@@ -1,28 +1,34 @@
 package com.into.websoso.ui.notification
 
+import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.into.websoso.core.common.ui.model.ResultFrom
 import com.into.websoso.core.common.util.setupSystemBarIconColor
 import com.into.websoso.core.designsystem.theme.WebsosoTheme
 import com.into.websoso.ui.feedDetail.FeedDetailActivity
 import com.into.websoso.ui.notification.model.NotificationModel
 import com.into.websoso.ui.notificationDetail.NotificationDetailActivity
+import com.into.websoso.ui.setting.dialog.NotificationPermissionDialog
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class NotificationActivity : ComponentActivity() {
+class NotificationActivity : AppCompatActivity() {
     private val notificationViewModel: NotificationViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setupSystemBarIconColor(true)
+        checkNotificationPermission()
         setContent {
             WebsosoTheme {
                 NotificationScreen(
@@ -36,6 +42,33 @@ class NotificationActivity : ComponentActivity() {
                 )
             }
         }
+    }
+
+    private fun checkNotificationPermission() {
+        val isNotificationPermissionGranted =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.POST_NOTIFICATIONS,
+                ) == PackageManager.PERMISSION_GRANTED
+            } else {
+                val notificationManager =
+                    getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                notificationManager.areNotificationsEnabled()
+            }
+
+        if (isNotificationPermissionGranted) return
+
+        showNotificationSettingDialog()
+    }
+
+    private fun showNotificationSettingDialog() {
+        val dialog = NotificationPermissionDialog.newInstance()
+        dialog.isCancelable = false
+        dialog.show(
+            supportFragmentManager,
+            NotificationPermissionDialog.NOTIFICATION_PERMISSION_DIALOG_TAG,
+        )
     }
 
     private fun navigateToNotificationDetail(notification: NotificationModel) {
