97fc602c42651bd26c1ffe5126300c6f5a92a59d
devfeijoa <devfeijoa@gmail.com>
2026-01-21 19:38:45 +0900

    test: SortCriteria 테스트 코드 작성

---
+++ domain/library/src/test/java/com/into/websoso/domain/library/model/SortCriteriaTest.kt
@@ -0,0 +1,20 @@
+package com.into.websoso.domain.library.model
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class SortCriteriaTest {
+
+    @Test
+    fun `정렬 키가 일치하면 해당 정렬 기준을 반환한다`() {
+        assertEquals(SortCriteria.RECENT, SortCriteria.from("RECENT"))
+        assertEquals(SortCriteria.OLD, SortCriteria.from("OLD"))
+    }
+
+    @Test
+    fun `정렬 키가 일치하지 않으면 최신순으로 처리된다`() {
+        assertEquals(SortCriteria.RECENT, SortCriteria.from("UNKNOWN"))
+        assertEquals(SortCriteria.RECENT, SortCriteria.from(""))
+        assertEquals(SortCriteria.RECENT, SortCriteria.from("recent"))
+    }
+}
