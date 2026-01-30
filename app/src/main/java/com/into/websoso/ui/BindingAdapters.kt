640f0e2067d1022ef43e88d80707242fdd317a49
devfeijoa <devfeijoa@gmail.com>
2026-01-30 10:55:46 +0900

    refactor: BindingAdapter 파라미터 포맷팅 수정

--- app/src/main/java/com/into/websoso/ui/BindingAdapters.kt
+++ app/src/main/java/com/into/websoso/ui/BindingAdapters.kt
@@ -8,13 +8,17 @@ import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 
 @BindingAdapter("app:imageTint")
-fun ImageView.setImageTint(@ColorRes colorRes: Int?) {
+fun ImageView.setImageTint(
+    @ColorRes colorRes: Int?,
+) {
     if (colorRes == null) return
     setColorFilter(ContextCompat.getColor(context, colorRes))
 }
 
 @BindingAdapter("app:backgroundTint")
-fun View.setBackgroundTint(@ColorRes colorRes: Int?) {
+fun View.setBackgroundTint(
+    @ColorRes colorRes: Int?,
+) {
     if (colorRes == null) return
     val background = this.background
     if (background is GradientDrawable) {
