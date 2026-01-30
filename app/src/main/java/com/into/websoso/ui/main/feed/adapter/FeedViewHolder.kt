3a69432e483f19d981d5f8cf0052fd904d102887
devfeijoa <devfeijoa@gmail.com>
2026-01-30 13:23:24 +0900

    refactor: 피드 아이템 카테고리 로직 개선
    기존의 복잡한 분기문으로 처리하던 피드 아이템의 장르 및 색상 결정 로직을 `firstOrNull`을 사용하여 간소화했습니다.

--- app/src/main/java/com/into/websoso/ui/main/feed/adapter/FeedViewHolder.kt
+++ app/src/main/java/com/into/websoso/ui/main/feed/adapter/FeedViewHolder.kt
@@ -24,28 +24,11 @@ class FeedViewHolder(
             user = feed.user.copy(avatarImage = itemView.getS3ImageUrl(feed.user.avatarImage)),
         )
 
-        // 1. 첫번쨰 카테고리를 가져온다
-        val firstCategory = feed.relevantCategories.first()
-        // 2. 카테고리가 기타인지 확인한다
-        val genre = if (firstCategory != "기타") {
-            firstCategory
-            // 3. 기타가 아니면 해당하는 장르에 맞는 색상을 반환한다.
-        } else {
-            // 두번째 값이 있는지를 확인
-            if (feed.relevantCategories.size >= 2) {
-                // 두번째 값이 있으면 해당하는 장르에 맞는 색상을 반환한다.
-                feed.relevantCategories[1]
-            } else {
-                "기타"
-            }
-        }
-        // 4. 기타이면 두번째 값을 가져온다
+        val genre = feed.relevantCategories.firstOrNull { it != "기타" } ?: "기타"
         val iconColor = Category.iconColor(genre)
         val backgroundColor = Category.backgroundColor(genre)
         binding.clFeedNovelInfo.setBackgroundTint(backgroundColor)
         binding.ivFeedNovelThumbnail.setImageTint(iconColor)
-        // 6. 두번째 값이 없으면 기타로 간주하고 아무런 색상을 반환하지 않는다.
-        // 7. 두번째 값도 기타이면 마찬가지로 아무런 색상을 반환하지 않는다.
 
         binding.clFeedLike.isSelected = feed.isLiked
     }
