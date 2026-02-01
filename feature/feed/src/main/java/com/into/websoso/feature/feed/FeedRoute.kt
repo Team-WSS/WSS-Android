21f6877c3acf55db1c495f7f7c7ab848cd937f3c
s9hn <tnghqn@naver.com>
2026-02-01 15:53:05 +0900

    fix: 루트 로딩뷰 제거

--- feature/feed/src/main/java/com/into/websoso/feature/feed/FeedRoute.kt
+++ feature/feed/src/main/java/com/into/websoso/feature/feed/FeedRoute.kt
@@ -1,7 +1,5 @@
 package com.into.websoso.feature.feed
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.SheetValue
 import androidx.compose.material3.rememberModalBottomSheetState
@@ -11,8 +9,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.into.websoso.feature.feed.model.FeedTab
@@ -39,44 +35,40 @@ fun FeedRoute(
     )
     var isFilterSheetVisible by remember { mutableStateOf(false) }
 
-    Box {
-        FeedScreen(
-            uiState = uiState,
-            bottomSheetState = bottomSheetState,
-            onTabSelected = viewModel::updateTab,
-            onSortSelected = viewModel::updateMyFeedSort,
-            onSosoTypeSelected = viewModel::updateSosoCategory,
-            onWriteClick = onWriteClick,
-            onProfileClick = { userId, feedTab ->
-                onProfileClick(
-                    userId,
-                    feedTab == FeedTab.MY_FEED,
-                )
-            },
-            onMoreClick = { },
-            onNovelClick = onNovelClick,
-            onLikeClick = viewModel::updateLike,
-            onContentClick = onContentClick,
-            onFilterClick = {
-                scope.launch { bottomSheetState.show() }
-                    .invokeOnCompletion { isFilterSheetVisible = true }
-            },
-            onApplyFilterClick = {
-                scope.launch {
-                    viewModel.applyMyFilter(filter = it)
-                    bottomSheetState.hide()
-                }.invokeOnCompletion { isFilterSheetVisible = false }
-            },
-            isFilterSheetVisible = isFilterSheetVisible,
-            onFilterCloseClick = {
-                scope.launch { bottomSheetState.hide() }
-                    .invokeOnCompletion { isFilterSheetVisible = false }
-            },
-            onFirstItemClick = onFirstItemClick,
-            onSecondItemClick = onSecondItemClick,
-            onRefreshPull = viewModel::refresh,
-        )
-
-        if (uiState.loading) CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
-    }
+    FeedScreen(
+        uiState = uiState,
+        bottomSheetState = bottomSheetState,
+        onTabSelected = viewModel::updateTab,
+        onSortSelected = viewModel::updateMyFeedSort,
+        onSosoTypeSelected = viewModel::updateSosoCategory,
+        onWriteClick = onWriteClick,
+        onProfileClick = { userId, feedTab ->
+            onProfileClick(
+                userId,
+                feedTab == FeedTab.MY_FEED,
+            )
+        },
+        onMoreClick = { },
+        onNovelClick = onNovelClick,
+        onLikeClick = viewModel::updateLike,
+        onContentClick = onContentClick,
+        onFilterClick = {
+            scope.launch { bottomSheetState.show() }
+                .invokeOnCompletion { isFilterSheetVisible = true }
+        },
+        onApplyFilterClick = {
+            scope.launch {
+                viewModel.applyMyFilter(filter = it)
+                bottomSheetState.hide()
+            }.invokeOnCompletion { isFilterSheetVisible = false }
+        },
+        isFilterSheetVisible = isFilterSheetVisible,
+        onFilterCloseClick = {
+            scope.launch { bottomSheetState.hide() }
+                .invokeOnCompletion { isFilterSheetVisible = false }
+        },
+        onFirstItemClick = onFirstItemClick,
+        onSecondItemClick = onSecondItemClick,
+        onRefreshPull = viewModel::refresh,
+    )
 }
