98fedca2f831b4b0b1cde1b917cf3a941f7b4408
m6z1 <sonmyungj1zz@gmail.com>
2026-04-11 23:59:43 +0900

    feat: 폰트 디자인 시스템 자간 추가

--- core/designsystem/src/main/java/com/into/websoso/core/designsystem/theme/WebsosoTypography.kt
+++ core/designsystem/src/main/java/com/into/websoso/core/designsystem/theme/WebsosoTypography.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.text.font.FontWeight.Companion.SemiBold
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
 import com.into.websoso.core.resource.R.font.pretendard_bold
 import com.into.websoso.core.resource.R.font.pretendard_medium
 import com.into.websoso.core.resource.R.font.pretendard_regular
@@ -34,31 +35,35 @@ data class WebsosoTypography(
 )
 
 internal fun WebsosoTypography(density: Density): WebsosoTypography {
-    val textStyle =
-        { fontFamily: FontFamily, fontWeight: FontWeight, fontSizeDp: Dp, lineHeightDp: Dp ->
-            TextStyle(
-                fontFamily = fontFamily,
-                fontWeight = fontWeight,
-                fontSize = with(density) { fontSizeDp.toSp() },
-                lineHeight = with(density) { lineHeightDp.toSp() },
-            )
-        }
+    fun textStyle(
+        fontFamily: FontFamily,
+        fontWeight: FontWeight,
+        fontSizeDp: Dp,
+        lineHeightDp: Dp,
+        letterSpacingEm: Float = 0f,
+    ) = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = fontWeight,
+        fontSize = with(density) { fontSizeDp.toSp() },
+        lineHeight = with(density) { lineHeightDp.toSp() },
+        letterSpacing = letterSpacingEm.em,
+    )
 
     return WebsosoTypography(
-        headline1 = textStyle(PretendardBold, Bold, 20.dp, 28.dp),
-        title1 = textStyle(PretendardBold, Bold, 18.dp, 25.dp),
-        title2 = textStyle(PretendardSemiBold, SemiBold, 16.dp, 22.dp),
-        title3 = textStyle(PretendardMedium, Medium, 14.dp, 14.dp),
+        headline1 = textStyle(PretendardBold, Bold, 20.dp, 28.dp, -0.06f),
+        title1 = textStyle(PretendardBold, Bold, 18.dp, 25.dp, -0.033f),
+        title2 = textStyle(PretendardSemiBold, SemiBold, 16.dp, 22.dp, -0.038f),
+        title3 = textStyle(PretendardMedium, Medium, 14.dp, 14.dp, -0.043f),
         title4 = textStyle(PretendardBold, Bold, 13.dp, 23.dp),
-        body1 = textStyle(PretendardRegular, Normal, 17.dp, 24.dp),
-        body2 = textStyle(PretendardRegular, Normal, 15.dp, 23.dp),
-        body3 = textStyle(PretendardRegular, Normal, 14.dp, 21.dp),
-        body4 = textStyle(PretendardMedium, Medium, 13.dp, 19.dp),
-        body5 = textStyle(PretendardRegular, Normal, 12.dp, 17.dp),
-        body4Secondary = textStyle(PretendardRegular, Normal, 13.dp, 19.dp),
-        body5Secondary = textStyle(PretendardMedium, Medium, 12.dp, 17.dp),
-        label1 = textStyle(PretendardMedium, Medium, 13.dp, 19.dp),
-        label2 = textStyle(PretendardRegular, Normal, 10.dp, 10.dp),
+        body1 = textStyle(PretendardRegular, Normal, 17.dp, 24.dp, -0.035f),
+        body2 = textStyle(PretendardRegular, Normal, 15.dp, 22.5.dp, -0.04f),
+        body3 = textStyle(PretendardRegular, Normal, 14.dp, 21.dp, -0.029f),
+        body4 = textStyle(PretendardMedium, Medium, 13.dp, 19.dp, -0.031f),
+        body5 = textStyle(PretendardRegular, Normal, 12.dp, 17.4.dp),
+        body4Secondary = textStyle(PretendardRegular, Normal, 13.dp, 19.dp, -0.031f),
+        body5Secondary = textStyle(PretendardMedium, Medium, 12.dp, 17.4.dp),
+        label1 = textStyle(PretendardMedium, Medium, 13.dp, 19.dp, -0.031f),
+        label2 = textStyle(PretendardMedium, Medium, 10.dp, 10.dp),
     )
 }
 
