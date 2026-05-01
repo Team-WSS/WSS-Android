eb0bee1624fccf492cdb1be44faf8afe568011a2
m6z1 <dev.moongchi@gmail.com>
2026-05-01 14:33:50 +0900

    feat: 서재에 검색 아이콘 추가

--- feature/library/src/main/java/com/into/websoso/feature/library/component/LibraryTopBar.kt
+++ feature/library/src/main/java/com/into/websoso/feature/library/component/LibraryTopBar.kt
@@ -1,23 +1,30 @@
 package com.into.websoso.feature.library.component
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
+import com.into.websoso.core.common.extensions.debouncedClickable
 import com.into.websoso.core.designsystem.theme.Black
 import com.into.websoso.core.designsystem.theme.WebsosoTheme
+import com.into.websoso.core.resource.R.drawable.ic_plus_novel
 
 @Composable
 internal fun LibraryTopBar(onSearchClick: () -> Unit = {}) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(horizontal = 20.dp),
+            .padding(start = 20.dp),
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically,
     ) {
@@ -26,17 +33,16 @@ internal fun LibraryTopBar(onSearchClick: () -> Unit = {}) {
             style = WebsosoTheme.typography.headline1,
             color = Black,
         )
-        /*
-        TODO: 추후 검색 기능 구현 시 롤백
-                IconButton(onClick = onSearchClick) {
-                    Image(
-                        imageVector = ImageVector.vectorResource(id = ic_common_search),
-                        contentDescription = "검색",
-                        modifier = Modifier.size(24.dp),
-                    )
-                }
-            }
-
-         */
+        Box(
+            modifier = Modifier
+                .debouncedClickable(onClick = onSearchClick)
+                .padding(horizontal = 20.dp, vertical = 8.dp),
+        ) {
+            Image(
+                imageVector = ImageVector.vectorResource(id = ic_plus_novel),
+                contentDescription = null,
+                modifier = Modifier.size(24.dp),
+            )
+        }
     }
 }
