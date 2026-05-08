ac39853f6df6517d2b765f296dfbfcf4f78df3cb
m6z1 <sonmyungj1zz@gmail.com>
2026-05-08 19:16:44 +0900

    feat: 상세 탐색 평점 min/max api 연결

--- app/src/main/java/com/into/websoso/ui/detailExploreResult/model/DetailExploreFilteredModel.kt
+++ app/src/main/java/com/into/websoso/ui/detailExploreResult/model/DetailExploreFilteredModel.kt
@@ -8,6 +8,12 @@ import kotlinx.parcelize.Parcelize
 data class DetailExploreFilteredModel(
     val genres: List<Genre>? = null,
     val isCompleted: Boolean? = null,
-    val novelRating: Float? = null,
+    val novelRatingStart: Float = RATING_MIN_DEFAULT,
+    val novelRatingEnd: Float = RATING_MAX_DEFAULT,
     val keywordIds: List<Int>? = null,
-) : Parcelable
+) : Parcelable {
+    companion object {
+        const val RATING_MIN_DEFAULT = 0.0f
+        const val RATING_MAX_DEFAULT = 5.0f
+    }
+}
