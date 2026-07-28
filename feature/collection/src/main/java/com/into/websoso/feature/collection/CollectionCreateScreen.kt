bccf4efa00df0c7c51d48d077c0734b8ba8235a9
devfeijoa <devfeijoa@gmail.com>
2026-07-29 07:56:00 +0900

    refactor: 컬렉션 생성 화면 내 임시 플레이스홀더 제거

--- feature/collection/src/main/java/com/into/websoso/feature/collection/CollectionCreateScreen.kt
+++ feature/collection/src/main/java/com/into/websoso/feature/collection/CollectionCreateScreen.kt
@@ -1,18 +1,12 @@
 package com.into.websoso.feature.collection
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.into.websoso.core.designsystem.theme.Black
-import com.into.websoso.core.designsystem.theme.Gray200
 import com.into.websoso.core.designsystem.theme.WebsosoTheme
 import com.into.websoso.core.designsystem.theme.White
 import com.into.websoso.feature.collection.component.CollectionCreateAppBar
@@ -29,31 +23,6 @@ internal fun CollectionCreateScreen(
             .statusBarsPadding(),
     ) {
         CollectionCreateAppBar(onNavigateBack = onNavigateBack)
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center,
-        ) {
-            CollectionCreatePlaceholder()
-        }
-    }
-}
-
-@Composable
-private fun CollectionCreatePlaceholder() {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-    ) {
-        Text(
-            text = "컬렉션 만들기",
-            color = Black,
-            style = WebsosoTheme.typography.headline1,
-        )
-        Text(
-            text = "임시화면",
-            color = Gray200,
-            style = WebsosoTheme.typography.body2,
-        )
     }
 }
 
