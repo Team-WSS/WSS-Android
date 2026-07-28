d5613f6951c34ff2a1240332c900e84228b58f8c
devfeijoa <devfeijoa@gmail.com>
2026-07-28 12:25:47 +0900

    feat: 컬렉션 기능 내 탐색을 위한 CollectionNavHost 구현

---
+++ feature/collection/src/main/java/com/into/websoso/feature/collection/CollectionNavHost.kt
@@ -0,0 +1,26 @@
+package com.into.websoso.feature.collection
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+private const val COLLECTION_ROUTE = "collection"
+
+@Composable
+fun CollectionNavHost(
+    modifier: Modifier = Modifier,
+) {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = COLLECTION_ROUTE,
+        modifier = modifier,
+    ) {
+        composable(route = COLLECTION_ROUTE) {
+            CollectionScreen()
+        }
+    }
+}
