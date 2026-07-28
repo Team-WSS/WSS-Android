1d920b38512c835abd345ccf340ed8bd190558b4
devfeijoa <devfeijoa@gmail.com>
2026-07-28 12:18:22 +0900

    feat: 컬렉션 기능 구성을 위한 임시 화면 추가
    - `CollectionScreen.kt`: `CollectionScreen` 컴포저블 구현 및 프리뷰 추가

---
+++ feature/collection/src/main/java/com/into/websoso/feature/collection/CollectionScreen.kt
@@ -0,0 +1,50 @@
+package com.into.websoso.feature.collection
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.into.websoso.core.designsystem.theme.Black
+import com.into.websoso.core.designsystem.theme.Gray200
+import com.into.websoso.core.designsystem.theme.WebsosoTheme
+import com.into.websoso.core.designsystem.theme.White
+
+@Composable
+fun CollectionScreen(
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(White)
+            .padding(horizontal = 20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(
+            text = "컬렉션",
+            color = Black,
+            style = WebsosoTheme.typography.headline1,
+        )
+        Text(
+            text = "임시화면",
+            color = Gray200,
+            style = WebsosoTheme.typography.body2,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun CollectionScreenPreview() {
+    WebsosoTheme {
+        CollectionScreen()
+    }
+}
