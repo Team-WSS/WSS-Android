cb430b8bfa8864585c4205bdc9ed69a4db02bb8c
devfeijoa <devfeijoa@gmail.com>
2026-07-28 21:33:43 +0900

    fix: 컬렉션 화면 내 임시 레이아웃 제거
    - `CollectionScreen.kt`: 화면 중앙에 위치하던 "컬렉션", "임시화면" 안내 문구와 이를 포함하던 `Box` 레이아웃 및 미사용 임포트 삭제

--- feature/collection/src/main/java/com/into/websoso/feature/collection/CollectionScreen.kt
+++ feature/collection/src/main/java/com/into/websoso/feature/collection/CollectionScreen.kt
@@ -1,25 +1,18 @@
 package com.into.websoso.feature.collection
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.into.websoso.core.designsystem.theme.Black
-import com.into.websoso.core.designsystem.theme.Gray200
 import com.into.websoso.core.designsystem.theme.WebsosoTheme
 import com.into.websoso.core.designsystem.theme.White
 import com.into.websoso.feature.collection.component.CollectionAppBar
@@ -51,29 +44,6 @@ fun CollectionScreen(
                 vertical = 16.dp,
             ),
         )
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .weight(1f)
-                .padding(horizontal = 20.dp),
-            contentAlignment = Alignment.Center,
-        ) {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center,
-            ) {
-                Text(
-                    text = "컬렉션",
-                    color = Black,
-                    style = WebsosoTheme.typography.headline1,
-                )
-                Text(
-                    text = "임시화면",
-                    color = Gray200,
-                    style = WebsosoTheme.typography.body2,
-                )
-            }
-        }
     }
 }
 
