ea5a1e6ee156f9d03f7f3e2515c9b10882b891df
devfeijoa <devfeijoa@gmail.com>
2026-07-29 08:16:16 +0900

    feat: 컬렉션 생성 화면 내 공개 여부 설정 컴포넌트 추가
    - `CollectionPrivacySetting.kt`: "나만 보는 컬렉션" 설정을 위한 `CollectionPrivacySetting` 컴포저블 및 커스텀 스위치(`CollectionPrivacySwitch`) 구현
    - `CollectionCreateScreen.kt`: 컬렉션 비공개 여부 상태(`isPrivate`) 추가 및 `CollectionPrivacySetting` 컴포넌트 적용

--- feature/collection/src/main/java/com/into/websoso/feature/collection/CollectionCreateScreen.kt
+++ feature/collection/src/main/java/com/into/websoso/feature/collection/CollectionCreateScreen.kt
@@ -5,17 +5,24 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.into.websoso.core.designsystem.theme.WebsosoTheme
 import com.into.websoso.core.designsystem.theme.White
 import com.into.websoso.feature.collection.component.CollectionCreateAppBar
+import com.into.websoso.feature.collection.component.CollectionPrivacySetting
 
 @Composable
 internal fun CollectionCreateScreen(
     onNavigateBack: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
+    var isPrivate by rememberSaveable { mutableStateOf(false) }
+
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -23,6 +30,10 @@ internal fun CollectionCreateScreen(
             .statusBarsPadding(),
     ) {
         CollectionCreateAppBar(onNavigateBack = onNavigateBack)
+        CollectionPrivacySetting(
+            isPrivate = isPrivate,
+            onPrivateChange = { isPrivate = it },
+        )
     }
 }
 
