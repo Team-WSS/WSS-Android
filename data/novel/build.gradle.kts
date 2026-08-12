8bd7f275e5a60b8ea109f9e518c46526df245b6c
devfeijoa <devfeijoa@gmail.com>
2026-08-12 14:09:01 +0900

    feat: novel 데이터 모듈 추가
    - `data/novel/build.gradle.kts`: Retrofit, Paging, Serialization 등 의존성 설정 및 Android 라이브러리 모듈 환경 구성
    - `settings.gradle.kts`: 전체 프로젝트 구성 내 `:data:novel` 모듈 추가
    - `data/novel/src/main/AndroidManifest.xml`: 라이브러리 구성을 위한 기본 매니페스트 파일 추가

---
+++ data/novel/build.gradle.kts
@@ -0,0 +1,20 @@
+import com.into.websoso.setNamespace
+
+plugins {
+    id("websoso.android.library")
+    alias(libs.plugins.kotlin.serialization)
+}
+
+android {
+    setNamespace("data.novel")
+}
+
+dependencies {
+    implementation(projects.core.network)
+
+    implementation(libs.paging.runtime)
+    implementation(libs.retrofit)
+    implementation(libs.serialization.json)
+
+    testImplementation(libs.junit)
+}
