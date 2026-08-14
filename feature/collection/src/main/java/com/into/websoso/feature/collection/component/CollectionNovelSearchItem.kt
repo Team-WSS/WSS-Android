aa86bc49a872fc549a069d9998342ec1cb25da4e
devfeijoa <devfeijoa@gmail.com>
2026-08-14 17:08:38 +0900

    refactor: CollectionNovelSearchItem 내 import 문 정렬
    - `CollectionNovelSearchItem.kt`: import 구문 순서 정렬 및 최적화

--- feature/collection/src/main/java/com/into/websoso/feature/collection/component/CollectionNovelSearchItem.kt
+++ feature/collection/src/main/java/com/into/websoso/feature/collection/component/CollectionNovelSearchItem.kt
@@ -1,7 +1,9 @@
 package com.into.websoso.feature.collection.component
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,9 +11,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.background
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.clickable
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
